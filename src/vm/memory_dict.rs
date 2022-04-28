42a6b11160f303f2108ed22dec34f2576cca65d2
Juan-M-V <juan.viel@lambdaclass.com>
2022-04-28 11:54:49 -0300

    Add index method declaration

--- src/vm/memory_dict.rs
+++ src/vm/memory_dict.rs
@@ -1,9 +1,17 @@
 mod relocatable;
 
 use num_bigint::BigUint;
-use relocatable::RelocatableValue
-struct MemoryDict {
+use std::collections::HashMap
+
+pub struct MemoryDict {
     data: HashMap,
     frozen: bool,
     relocation_rules: HashMap<BigUint, RelocatableValue>
+};
+
+impl MemoryDict {
+    pub fn index(&self, addr: relocatable::MaybeRelocatable) -> relocatable::MaybeRelocatable {
+        // Error checking
+        // Return
+    }
 }
