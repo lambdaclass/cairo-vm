317314f09eaedee1b138fd89bb011fd139647470
Juan-M-V <juan.viel@lambdaclass.com>
2022-04-29 10:58:50 -0300

    Add get method

--- src/vm/memory.rs
+++ src/vm/memory.rs
@@ -1,1 +1,13 @@
-type Memory = Vec<i64>;
+mod relocatable;
+
+std::collections::HashMap;
+
+struct Memory {
+    data:HashMap<relocatable::MaybeRelocatable, relocatable::MaybeRelocatable>;
+}
+
+impl Memory {
+    fn get(&self, &addr:relocatable::MaybeRelocatable) -> Option<relocatable::MaybeRelocatable> {
+        Some(self.data[addr])
+    }
+}
