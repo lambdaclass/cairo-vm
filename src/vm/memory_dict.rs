5bb574494a9edff41dd0d387b0b8bfd538ccd8fd
Juan-M-V <juan.viel@lambdaclass.com>
2022-04-29 10:31:26 -0300

    simplify memory

--- src/vm/memory_dict.rs
+++ src/vm/memory_dict.rs
@@ -1,17 +1,1 @@
-mod relocatable;
-
-use num_bigint::BigUint;
-use std::collections::HashMap
-
-pub struct MemoryDict {
-    data: HashMap,
-    frozen: bool,
-    relocation_rules: HashMap<BigUint, RelocatableValue>
-};
-
-impl MemoryDict {
-    pub fn index(&self, addr: relocatable::MaybeRelocatable) -> relocatable::MaybeRelocatable {
-        // Error checking
-        // Return
-    }
-}
+type Memory = Vec<i64>;
