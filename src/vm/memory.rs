42122301ecc1bab65a38f8f6f293670d62c551bd
Juan-M-V <juan.viel@lambdaclass.com>
2022-05-03 11:59:30 -0300

    Rollback changes in other files

--- src/vm/memory.rs
+++ src/vm/memory.rs
@@ -1,12 +1,12 @@
-use std::collections::HashMap;
 use crate::vm::relocatable::MaybeRelocatable;
+use std::collections::HashMap;
 
 struct Memory {
-    data:HashMap<MaybeRelocatable, MaybeRelocatable>
+    data: HashMap<MaybeRelocatable, MaybeRelocatable>,
 }
 
 impl Memory {
-    fn get(&self, addr:&MaybeRelocatable) -> Option<&MaybeRelocatable> {
-        return self.data.get(&addr)
+    fn get(&self, addr: &MaybeRelocatable) -> Option<&MaybeRelocatable> {
+        return self.data.get(&addr);
     }
 }
