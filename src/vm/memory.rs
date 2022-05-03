5721bbdbd63e0afc0fb1b11166403b3c996e2a9b
Juan-M-V <juan.viel@lambdaclass.com>
2022-05-03 11:51:21 -0300

    Change get parameters and output

--- src/vm/memory.rs
+++ src/vm/memory.rs
@@ -1,12 +1,12 @@
 use std::collections::HashMap;
-use relocatable::MaybeRelocatable
+use crate::vm::relocatable::MaybeRelocatable;
 
 struct Memory {
     data:HashMap<MaybeRelocatable, MaybeRelocatable>
 }
 
 impl Memory {
-    fn get(&self, addr:&MaybeRelocatable) -> Option<MaybeRelocatable> {
-        Some(self.data.get(addr))
+    fn get(&self, addr:&MaybeRelocatable) -> Option<&MaybeRelocatable> {
+        return self.data.get(&addr)
     }
 }
