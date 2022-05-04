bbfc9d16cc8edb0a9e4b8adaff5baef396b750d6
Juan-M-V <juan.viel@lambdaclass.com>
2022-05-04 17:19:14 -0300

    Fix syntax and rm pub keyword

--- src/vm/memory.rs
+++ src/vm/memory.rs
@@ -12,13 +12,13 @@ impl Memory {
 }
 
 #[cfg(test)]
-pub mod memory_tests {
+mod memory_tests {
     #[test]
-    pub fn get_test () {
-        let key = MaybeRelocatable::Int(BigInt::from(2))
-        let val = MaybeRelocatable::Int(BigInt::from(5))
+    fn get_test () {
+        let key = MaybeRelocatable::Int(BigInt::from(2));
+        let val = MaybeRelocatable::Int(BigInt::from(5));
         let mem:Memory {
-            HashMap::from([(key, val)])
+            data: HashMap::from([(key, val)])
         };
         assert_eq!(mem.get(&key), MaybeRelocatable::Int(BigInt::from(5)));
     }
