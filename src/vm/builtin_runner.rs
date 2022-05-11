aeec529d396dfa7f74a7214d0be9483e000562e7
Federica Moletta <fedemoletta@hotmail.com>
2022-05-11 18:23:38 -0300

    Add definitions for SimpleBuiltinRunner and OutputBuiltinRunner

--- src/vm/builtin_runner.rs
+++ src/vm/builtin_runner.rs
@@ -1,3 +1,46 @@
-pub struct BuiltinRunner {
-    
+use num_bigint::BigInt;
+use crate::vm::relocatable::Relocatable;
+
+pub enum BuiltinRunner {
+    SimpleBuiltinRunner,
+    OutputRunner,
+}
+
+struct SimpleBuiltinRunner {
+    name: String,
+    included: bool,
+    ratio: BigInt,
+    base: Option<Relocatable>,
+    stop_ptr: Option<Relocatable>,
+    cells_per_instance: BigInt,
+    n_input_cells: BigInt,
+}
+struct OutputRunner {
+    included: bool,
+    base: Option<Relocatable>,
+    stop_ptr: Option<Relocatable>,
+}
+
+impl SimpleBuiltinRunner {
+    pub fn new(name: String, included: bool, ratio: BigInt, cells_per_instance: BigInt, n_input_cells: BigInt) -> SimpleBuiltinRunner {
+        SimpleBuiltinRunner {
+            name: name,
+            included: included,
+            ratio: ratio,
+            base: None,
+            stop_ptr: None,
+            cells_per_instance: cells_per_instance,
+            n_input_cells: n_input_cells,
+        }
+    }
+}
+
+impl OutputRunner {
+    pub fn new(included: bool) -> OutputRunner {
+        OutputRunner {
+            included: included,
+            base: None,
+            stop_ptr: None,
+        }
+    }
 }
--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -7,3 +7,4 @@ pub mod run_context;
 pub mod trace_entry;
 pub mod validated_memory_dict;
 pub mod vm_core;
+pub mod builtin_runner;
