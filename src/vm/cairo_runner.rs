9fac8441dba243ed2697b327df0f7c8cc43e21e3
Federica Moletta <fedemoletta@hotmail.com>
2022-05-26 14:21:56 -0300

    Start test for initialize_vm

--- src/vm/cairo_runner.rs
+++ src/vm/cairo_runner.rs
@@ -510,4 +510,17 @@ mod tests {
         let return_pc = cairo_runner.initialize_main_entrypoint();
         assert_eq!(return_pc, relocatable!(1, 0));
     }
+
+    #[test]
+    fn initialize_vm() {
+        //This test works with basic Program definition, will later be updated to use Program::new() when fully defined
+        let program = Program {
+            builtins: Vec::new(),
+            prime: bigint!(17),
+            data: Vec::new(),
+            main: Some(bigint!(1)),
+        };
+        let mut cairo_runner = CairoRunner::new(&program);
+        cairo_runner.initialize_vm()
+    }
 }
