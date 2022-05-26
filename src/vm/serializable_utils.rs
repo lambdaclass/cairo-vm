28bd53d712ceb057e397a473be4427c80e5bb05b
Mariano Nicolini <mariano.nicolini@lambdaclass.com>
2022-05-26 13:56:55 -0300

    Apply dead code directive to the deserialize_bigint_hex function

--- src/vm/serializable_utils.rs
+++ src/vm/serializable_utils.rs
@@ -32,7 +32,10 @@ impl<'de> de::Visitor<'de> for BigIntVisitor {
     }
 }
 
-fn deserialize_bigint_hex<'de, D: Deserializer<'de>>(d: D) -> Result<BigInt, D::Error> {
+// This directive should be removed once the entire Program struct is deserializable and the
+// '#[derive(Deserialize)]' directive is could be applied to it.
+#[allow(dead_code)]
+pub fn deserialize_bigint_hex<'de, D: Deserializer<'de>>(d: D) -> Result<BigInt, D::Error> {
     d.deserialize_str(BigIntVisitor)
 }
 
