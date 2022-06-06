5ae64aeb86e4587c480e0ccc36d2aa2ef5651b2a
Juan-M-V <juan.viel@lambdaclass.com>
2022-06-06 13:07:19 -0300

    Add libiconv dependency

--- shell.nix
+++ shell.nix
@@ -1,5 +1,10 @@
 with import <nixpkgs> {};
 mkShell {
-  nativeBuildInputs = [ rustc cargo ];
+  nativeBuildInputs = [ libiconv rustc cargo ];
   RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+  shellHook =
+  ''
+  cargo build
+  exit
+  '';
 }
