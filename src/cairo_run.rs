0c6b5188176975fe1495b729b13a672563499b80
Federica Moletta <fedemoletta@hotmail.com>
2022-06-07 13:20:48 -0300

    Add cairo_run.rs

---
+++ src/cairo_run.rs
@@ -0,0 +1,12 @@
+use crate::types::program::Program;
+use crate::vm::runners::cairo_runner::CairoRunner;
+
+#[allow(dead_code)]
+pub fn cairo_run(path: &str) {
+    let program = Program::new(path);
+    let mut cairo_runner = CairoRunner::new(&program);
+    cairo_runner.initialize_segments(None);
+    let end = cairo_runner.initialize_main_entrypoint();
+    assert!(cairo_runner.run_until_pc(end) == Ok(()), "Execution failed");
+    cairo_runner.relocate()
+}
