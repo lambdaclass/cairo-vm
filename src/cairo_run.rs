c10d369371973a90f589cb0abecb1c7185babdf0
Federica Moletta <fedemoletta@hotmail.com>
2022-06-07 18:38:27 -0300

    Add print_output to cairo_run

--- src/cairo_run.rs
+++ src/cairo_run.rs
@@ -9,5 +9,6 @@ pub fn cairo_run(path: &str) {
     let end = cairo_runner.initialize_main_entrypoint();
     cairo_runner.initialize_vm();
     assert!(cairo_runner.run_until_pc(end) == Ok(()), "Execution failed");
-    cairo_runner.relocate()
+    cairo_runner.relocate();
+    cairo_runner.print_output();
 }
