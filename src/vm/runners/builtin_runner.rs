76111d980d164cd997d3a75a4a5b1054ea56be37
Federica Moletta <fedemoletta@hotmail.com>
2022-06-08 18:10:17 -0300

    Add test for deduce_memory_cell from preset memory

--- src/vm/runners/builtin_runner.rs
+++ src/vm/runners/builtin_runner.rs
@@ -184,14 +184,14 @@ impl HashBuiltinRunner {
                 //Convert MaybeRelocatable to FieldElement
                 let a_string = num_a.to_str_radix(10);
                 let b_string = num_b.to_str_radix(10);
-                let x = FieldElement::from_dec_str(&a_string).unwrap();
-                let y = FieldElement::from_dec_str(&b_string).unwrap();
+                let y = FieldElement::from_dec_str(&a_string).unwrap();
+                let x = FieldElement::from_dec_str(&b_string).unwrap();
                 //Compute pedersen Hash
                 let fe_result = pedersen_hash(&x, &y);
                 //Convert result from FieldElement to MaybeRelocatable
                 let r_byte_slice = fe_result.to_bytes_be();
                 let result = BigInt::from_bytes_be(Sign::Plus, &r_byte_slice);
-                Some(MaybeRelocatable::from(result));
+                return Some(MaybeRelocatable::from(result));
             }
             None
         } else {
@@ -231,8 +231,8 @@ impl BuiltinRunner for HashBuiltinRunner {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::relocatable;
     use crate::vm::vm_memory::memory::Memory;
+    use crate::{bigint_str, relocatable};
 
     #[test]
     fn initialize_segments_for_output() {
@@ -382,17 +382,23 @@ mod tests {
         let mut builtin = HashBuiltinRunner::new(true, 8);
         memory.data.push(Vec::new());
         memory.insert(
-            &MaybeRelocatable::from((0, 0)),
+            &MaybeRelocatable::from((0, 3)),
             &MaybeRelocatable::Int(bigint!(32)),
         );
         memory.insert(
-            &MaybeRelocatable::from((0, 1)),
+            &MaybeRelocatable::from((0, 4)),
             &MaybeRelocatable::Int(bigint!(72)),
         );
         memory.insert(
-            &MaybeRelocatable::from((0, 2)),
+            &MaybeRelocatable::from((0, 5)),
             &MaybeRelocatable::Int(bigint!(0)),
         );
-        let _result = builtin.deduce_memory_cell(&MaybeRelocatable::from((0, 2)), &memory);
+        let result = builtin.deduce_memory_cell(&MaybeRelocatable::from((0, 5)), &memory);
+        assert_eq!(
+            result,
+            Some(MaybeRelocatable::from(bigint_str!(
+                b"3270867057177188607814717243084834301278723532952411121381966378910183338911"
+            )))
+        );
     }
 }
