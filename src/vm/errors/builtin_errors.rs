15c089b6815e2603e7c5fb33b309777bc587f276
Juan-M-V <juan.viel@lambdaclass.com>
2022-06-09 10:36:04 -0300

    Add out of bounds error

--- src/vm/errors/builtin_errors.rs
+++ src/vm/errors/builtin_errors.rs
@@ -2,12 +2,17 @@ use std::fmt;
 
 pub enum BuiltinError {
     UninitializedBase,
+    NumOutOfBounds,
 }
 
 impl fmt::Display for BuiltinError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             BuiltinError::UninitializedBase => write!(f, "Uninitialized self.base"),
+            BuiltinError::NumOutOfBounds => write!(
+                f,
+                "Range-check validation failed, number is out of valid range"
+            ),
         }
     }
 }
