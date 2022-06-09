220c50ebaac8668b4e6b1ff4c90127ba77ccb9c1
Pedro Fontana <pedro.fontana@lambdaclass.com>
2022-06-09 15:13:40 -0300

    add cairo_run_benchmark.rs, modify Cargo.toml

---
+++ benches/cairo_run_benchmark.rs
@@ -0,0 +1,11 @@
+use cleopatra_cairo::cairo_run;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("cairo-run tests/support/fibonacci_compiled.json", |b| {
+        b.iter(|| cairo_run::cairo_run(black_box("tests/support/fibonacci_compiled.json")))
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
