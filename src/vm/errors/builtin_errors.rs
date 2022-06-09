e404a370d5c29eeb91bf4e72d16e57931e68ff79
Juan-M-V <juan.viel@lambdaclass.com>
2022-06-09 10:31:16 -0300

    Add uninitialized base error

--- src/vm/errors/builtin_errors.rs
+++ src/vm/errors/builtin_errors.rs
@@ -1,11 +1,13 @@
 use std::fmt;
 
 pub enum BuiltinError {
-    
+    UninitializedBase,
 }
 
 impl fmt::Display for BuiltinError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-
+        match self {
+            BuiltinError::UninitializedBase => write!(f, "Uninitialized self.base"),
+        }
     }
 }
