6ef5220dc4fe41703821def9293114e38d014820
Juan-M-V <juan.viel@lambdaclass.com>
2022-06-09 14:58:19 -0300

    Correct vm_errors import

--- src/types/relocatable.rs
+++ src/types/relocatable.rs
@@ -1,4 +1,4 @@
-use crate::{bigint, vm::vm_core::VirtualMachineError};
+use crate::{bigint, vm::errors::vm_errors::VirtualMachineError};
 use num_bigint::BigInt;
 use num_traits::{FromPrimitive, ToPrimitive};
 
