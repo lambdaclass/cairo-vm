e1273ebb42b32481a9496d6fd685e4638ec14f0b
unbalancedparentheses <mail@fcarrone.com>
2022-06-13 17:44:42 -0300

    Add better output to the shellscript

--- benches/run_benchmarks.sh
+++ benches/run_benchmarks.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
 set -e
+
+# Setup
 echo "Cleaning old results"
 rm -f results
 rm -f fibonacci.json
 rm -f factorial.json
 rm -rf oriac
 
-echo "Building cleopatra ..."
+echo "Building Cleopatra"
 cargo build --release
 
+# Fibonacci
 echo "Compiling Fibonacci cairo program"
 cairo-compile fibonacci.cairo --output fibonacci.json
 
+echo "* Fibonacci *\n"
 cleo_fibonacci_time=$( (time ../target/release/cleopatra-run fibonacci.json) 2>&1 &)
-echo "Cleopatra VM Fibonacci time:" >> results
+echo "Rust Cleopatra VM time:" >> results
 echo "$cleo_fibonacci_time" >> results
 
 cairo_fibonacci_time=$( (time cairo-run --program fibonacci.json) 2>&1 &)
-echo -e "\nOriginal Cairo VM Fibonacci time:" >> results
+echo -e "\nPython Original VM time:" >> results
 echo "$cairo_fibonacci_time" >> results
 
-echo "Compiling factorial cairo program"
+# Factorial
+echo "Compiling Factorial cairo program"
 cairo-compile factorial.cairo --output factorial.json
 
+echo "* Factorial *\n"
 cleo_factorial_time=$( (time ../target/release/cleopatra-run factorial.json) 2>&1 &)
-echo -e "\nCleopatra VM factorial time:" >> results
+echo -e "\nRust Cleopatra VM time:" >> results
 echo "$cleo_factorial_time" >> results
 
 cairo_factorial_time=$( (time cairo-run --program factorial.json) 2>&1 &)
-echo -e "\nOriginal Cairo VM factorial time:" >> results
+echo -e "\nOriginal Cairo VM time:" >> results
 echo "$cairo_factorial_time" >> results
 
 cat results
