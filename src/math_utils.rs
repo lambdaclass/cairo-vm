17f2656566b066d4643c61f19260f6df837fe1d0
Federica Moletta <fedemoletta@hotmail.com>
2022-06-14 10:27:20 -0300

    Add function div_mod

--- src/math_utils.rs
+++ src/math_utils.rs
@@ -4,17 +4,15 @@ use num_traits::{abs, FromPrimitive};
 
 use crate::bigint;
 
-fn igcdex(mut a: BigInt, mut b: BigInt) -> (BigInt, BigInt, BigInt) {
+fn igcdex(num_a: BigInt, num_b: BigInt) -> (BigInt, BigInt, BigInt) {
+    let mut a = num_a;
+    let mut b = num_b;
     let x_sign: i32;
     let y_sign: i32;
-    match (a.to_owned(), b.to_owned()) {
-        (a, b) if a == b && a == bigint!(0) => {
-            return (bigint!(0), bigint!(1), bigint!(0));
-        }
-        (a, _) if a == bigint!(0) => {
-            return (bigint!(0), b.div_floor(&abs(b.to_owned())), abs(b));
-        }
-        (_, b) if b == bigint!(0) => return (a.div_floor(&a), bigint!(0), abs(a)),
+    match (a.clone(), b.clone()) {
+        (a, b) if a == b && a == bigint!(0) => (bigint!(0), bigint!(1), bigint!(0)),
+        (a, _) if a == bigint!(0) => (bigint!(0), b.div_floor(&abs(b.clone())), abs(b)),
+        (_, b) if b == bigint!(0) => (a.div_floor(&a), bigint!(0), abs(a)),
         _ => {
             if a < bigint!(0) {
                 a = -a;
@@ -31,19 +29,18 @@ fn igcdex(mut a: BigInt, mut b: BigInt) -> (BigInt, BigInt, BigInt) {
             let (mut x, mut y, mut r, mut s) = (bigint!(1), bigint!(0), bigint!(0), bigint!(1));
             let (mut c, mut q);
             while b != bigint!(0) {
-                (c, q) = (a.to_owned() % b.to_owned(), a.div_floor(&b.to_owned()));
-                (a, b, r, s, x, y) = (
-                    b,
-                    c,
-                    x - q.to_owned() * r.to_owned(),
-                    y - q * s.to_owned(),
-                    r,
-                    s,
-                )
+                (c, q) = (a.clone() % b.clone(), a.div_floor(&b.clone()));
+                (a, b, r, s, x, y) = (b, c, x - q.clone() * r.clone(), y - q * s.clone(), r, s)
             }
-            return (x * x_sign, y * y_sign, a.clone());
+            (x * x_sign, y * y_sign, a)
         }
-    };
+    }
+}
+///Finds a nonnegative integer x < p such that (m * x) % p == n.
+fn div_mod(n: BigInt, m: BigInt, p: BigInt) -> BigInt {
+    let (a, _, c) = igcdex(m, p.clone());
+    assert_eq!(c, bigint!(1));
+    (n * a) % p
 }
 
 /// Gets two points on an elliptic curve mod p and returns their sum.
@@ -63,7 +60,7 @@ pub fn ec_add(
 /// Assumes the points are given in affine form (x, y) and have different x coordinates.
 pub fn line_slope(point_a: (BigInt, BigInt), point_b: (BigInt, BigInt), prime: &BigInt) -> BigInt {
     assert!((point_a.0.clone() - point_b.0.clone()) % prime != bigint!(0));
-    (point_a.1 - point_b.1) / (point_a.0 - point_b.0) % prime
+    div_mod(point_a.1 - point_b.1, point_a.0 - point_b.0, prime.clone())
 }
 
 ///  Doubles a point on an elliptic curve with the equation y^2 = x^3 + alpha*x + beta mod p.
@@ -80,7 +77,11 @@ pub fn ec_double(point: (BigInt, BigInt), alpha: &BigInt, prime: &BigInt) -> (Bi
 /// Assumes the point is given in affine form (x, y) and has y != 0.
 pub fn ec_double_slope(point: (BigInt, BigInt), alpha: &BigInt, prime: &BigInt) -> BigInt {
     assert!(point.1.clone() % prime != bigint!(0));
-    (bigint!(3) * point.0.clone() * point.0.clone() + alpha) / (bigint!(2) * point.1) % prime
+    div_mod(
+        bigint!(3) * point.0.clone() * point.0.clone() + alpha,
+        bigint!(2) * point.1,
+        prime.clone(),
+    )
 }
 
 #[cfg(test)]
@@ -101,7 +102,7 @@ mod tests {
 
     #[test]
     fn compute_line_slope_for_valid_points() {
-        let _point_a = (
+        let point_a = (
             bigint_str!(
                 b"3139037544796708144595053687182055617920475701120786241351436619796497072089"
             ),
@@ -109,7 +110,7 @@ mod tests {
                 b"2119589567875935397690285099786081818522144748339117565577200220779667999801"
             ),
         );
-        let _point_b = (
+        let point_b = (
             bigint_str!(
                 b"3324833730090626974525872402899302150520188025637965566623476530814354734325"
             ),
@@ -117,14 +118,14 @@ mod tests {
                 b"3147007486456030910661996439995670279305852583596209647900952752170983517249"
             ),
         );
-        let _prime = bigint_str!(
+        let prime = bigint_str!(
             b"3618502788666131213697322783095070105623107215331596699973092056135872020481"
         );
-        /*assert_eq!(
+        assert_eq!(
             bigint_str!(
                 b"992545364708437554384321881954558327331693627531977596999212637460266617010"
             ),
             line_slope(point_a, point_b, &prime)
-        );*/
+        );
     }
 }
