d85a3fc5c331cd712a5b635ab2379f422d865b3b
Juan-M-V <juan.viel@lambdaclass.com>
2022-06-16 11:59:33 -0300

    Add error handling

--- src/cairo_run.rs
+++ src/cairo_run.rs
@@ -21,10 +21,10 @@ pub fn cairo_run(path: &str) {
 /// Writes a trace as a binary file. Bincode encodes to little endian by default and each trace
 /// entry is composed of 3 usize values that are padded to always reach 64 bit size.
 fn write_binary_trace(relocated_trace: &Vec<RelocatedTraceEntry>, trace_file: &str) {
-    let mut buffer = File::create(trace_file);
+    let mut buffer = File::create(trace_file).expect("Error while creating trace file");
     for (i, entry) in relocated_trace.iter().enumerate() {
-        match bincode::serialize(entry) {
-            Ok(trace) => {},
+        match bincode::serialize_into(&mut buffer, entry) {
+            Ok(_) => {},
             Err(e) => println!("Failed to dump trace at position {}, serialize error: {}", i, e),
         }
     }
