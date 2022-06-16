30b5aa0554bed5d7a2d2cafa6f6a191772b3b661
Juan-M-V <juan.viel@lambdaclass.com>
2022-06-16 10:07:28 -0300

    Remove Serialize trait

--- src/vm/trace/trace_entry.rs
+++ src/vm/trace/trace_entry.rs
@@ -34,22 +34,6 @@ pub fn relocate_trace_register(
     }
 }
 
-/// Serializes RelocatedTraceEntry by putting ap, fp and pc together, in that order. It will add
-/// the necessary padding so that each register takes 64 bits of space. Encoding is done in little
-/// endian format.
-impl Serialize for RelocatedTraceEntry {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let mut entry = serializer.serialize_struct("RelocatedTraceEntry", 3)?;
-        entry.serialize_field("ap", &self.ap)?;
-        entry.serialize_field("fp", &self.fp)?;
-        entry.serialize_field("pc", &self.pc)?;
-        entry.end()
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
