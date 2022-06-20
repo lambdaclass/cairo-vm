ef4a86d6be4db9e5704d2c443a3a54bae568719e
github-action-benchmark <github@users.noreply.github.com>
2022-06-20 20:32:20 +0000

    add Benchmark (cargo) benchmark result for 60186de59d94831649ae97f81a95fbd904fa079c

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1655757140625,
+  "repoUrl": "https://github.com/lambdaclass/cleopatra_cairo",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "mail@fcarrone.com",
+            "name": "Federico Carrone",
+            "username": "unbalancedparentheses"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "60186de59d94831649ae97f81a95fbd904fa079c",
+          "message": "Update README.md",
+          "timestamp": "2022-06-20T17:28:29-03:00",
+          "tree_id": "fc92e0e6ef1f096d7794794a45a127038b1f309a",
+          "url": "https://github.com/lambdaclass/cleopatra_cairo/commit/60186de59d94831649ae97f81a95fbd904fa079c"
+        },
+        "date": 1655757139508,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "cairo_run(bench/criterion/fibonacci_1000.json",
+            "value": 8398433,
+            "range": "± 39344",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
