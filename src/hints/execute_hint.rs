757621a61b328d876eb4f6c5b788be109b408c5e
Federica Moletta <fedemoletta@hotmail.com>
2022-06-23 18:58:04 -0300

    Add test for execute_hint for alloc with empty memory

--- src/hints/execute_hint.rs
+++ src/hints/execute_hint.rs
@@ -1,12 +1,12 @@
 use crate::types::relocatable::MaybeRelocatable;
-use crate::vm::runners::cairo_runner::CairoRunner;
+use crate::vm::vm_core::VirtualMachine;
 use crate::vm::vm_memory::memory::Memory;
 use crate::vm::vm_memory::memory_segments::MemorySegmentManager;
 use std::sync::{Arc, Mutex};
 use wasmer::{imports, wat2wasm, Function, Instance, Module, Store, WasmerEnv};
 
 pub fn execute_hint(
-    runner: &CairoRunner,
+    vm: &VirtualMachine,
     hint_bytes: &[u8],
 ) -> Result<(), Box<dyn std::error::Error>> {
     let wasm_bytes = wat2wasm(hint_bytes)?;
@@ -15,7 +15,7 @@ pub fn execute_hint(
     let module = Module::new(&store, wasm_bytes)?;
 
     //Shared values
-    let shared_ap = Arc::new(Mutex::new(runner.vm.run_context.ap.clone()));
+    let shared_ap = Arc::new(Mutex::new(vm.run_context.ap.clone()));
 
     #[derive(WasmerEnv, Clone)]
     struct Env {
@@ -38,7 +38,7 @@ pub fn execute_hint(
     // Create an import object.
     let import_object = imports! {
         "env" => {
-            "add_segment" => Function::new_native_with_env(&store, Env { memory: runner.vm.memory.clone(), segments: runner.vm.segments.clone(), ap: shared_ap.clone() }, add_segment),
+            "add_segment" => Function::new_native_with_env(&store, Env { memory: vm.memory.clone(), segments: vm.segments.clone(), ap: shared_ap.clone() }, add_segment),
             //Env Received by function must be static
         }
     };
@@ -57,3 +57,34 @@ Block of wasm code to execute alloc() hint:
         (func $function (export "hint") call $add_segment))
     "#
 */
+
+#[cfg(test)]
+mod tests {
+    use num_bigint::{BigInt, Sign};
+
+    use super::*;
+    #[test]
+    fn run_alloc_hint_empty_memory() {
+        let wasm_bytes = br#"
+        (module
+            (func $add_segment (import "env" "add_segment"))
+            (func $function (export "hint") call $add_segment))
+        "#;
+        let vm = VirtualMachine::new(
+            BigInt::new(Sign::Plus, vec![1, 0, 0, 0, 0, 0, 17, 134217728]),
+            //ap value is (0,0)
+            Vec::new(),
+        );
+        execute_hint(&vm, wasm_bytes).expect("Error while executing hint");
+        //first new segment is added
+        assert_eq!(vm.segments.lock().unwrap().num_segments, 1);
+        //new segment base (0,0) is inserted into ap (0,0)
+        assert_eq!(
+            vm.memory
+                .lock()
+                .unwrap()
+                .get(&MaybeRelocatable::from((0, 0))),
+            Ok(Some(&MaybeRelocatable::from((0, 0))))
+        );
+    }
+}
