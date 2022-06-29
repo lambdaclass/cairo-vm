a5596c0bf59ace5517b69252c2d91ad960a41471
Juan-M-V <juan.viel@lambdaclass.com>
2022-06-29 17:35:26 -0300

    Add pc update error test

--- src/vm/decoding/decoder.rs
+++ src/vm/decoding/decoder.rs
@@ -159,6 +159,13 @@ mod decoder_test {
     }
 
     #[test]
+    fn invalid_pc_update() {
+        let error = decode_instruction(0x29A8800080008000, None);
+        assert_eq!(error, Err(VirtualMachineError::InvalidPcUpdate(3)));
+        assert_eq!(error.unwrap_err().to_string(), "Invalid pc_update value: 3")
+    }
+
+    #[test]
     fn decode_flags_call_add_jmp_add_imm_fp_fp() {
         //  0|  opcode|ap_update|pc_update|res_logic|op1_src|op0_reg|dst_reg
         // 15|14 13 12|    11 10|  9  8  7|     6  5|4  3  2|      1|      0
