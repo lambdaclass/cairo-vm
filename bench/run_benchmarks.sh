a45a7da2803ffe026d96c77430110299ce2b98eb
Pedro Fontana <pedro.fontana@lambdaclass.com>
2022-06-30 16:04:05 -0300

    remove oriac

--- bench/run_benchmarks.sh
+++ bench/run_benchmarks.sh
@@ -6,7 +6,6 @@ rm -f fibonacci.json
 rm -f factorial.json
 rm -f integration_builtins.json
 rm -f compare_arrays.json
-# rm -rf oriac
 
 pyenv global 3.7.12
 
@@ -22,34 +21,9 @@ cleo_fibonacci_time=$( (time ../target/release/cleopatra-run fibonacci.json) 2>&
 echo "Rust Cleopatra VM Fibonacci time:" >> results
 echo "$cleo_fibonacci_time" >> results
 
-# echo "Building oriac ..."
-# git clone https://github.com/xJonathanLEI/oriac.git
-# cargo build --release --manifest-path oriac/Cargo.toml
-
-# oriac_fibonacci_time=$( (time oriac/target/release/oriac-run --program fibonacci.json) 2>&1 &)
-# echo -e "\nOriac VM Fibonacci time:" >> results
-# echo "$oriac_fibonacci_time" >> results
-
 cairo_fibonacci_time=$( (time cairo-run --program fibonacci.json) 2>&1 &)
 echo -e "\nOriginal Cairo VM Fibonacci time:" >> results
 echo "$cairo_fibonacci_time" >> results
-
-# Factorial Benchmarks
-
-echo "Compiling factorial cairo program"
-cairo-compile factorial.cairo --output factorial.json
-
-cleo_factorial_time=$( (time ../target/release/cleopatra-run factorial.json) 2>&1 &)
-echo -e "\nRust Cleopatra VM factorial time:" >> results
-echo "$cleo_factorial_time" >> results
-
-# oriac_factorial_time=$( (time oriac/target/release/oriac-run --program factorial.json) 2>&1 &)
-# echo -e "\nOriac VM factorial time:" >> results
-# echo "$oriac_factorial_time" >> results
-
-cairo_factorial_time=$( (time cairo-run --program factorial.json) 2>&1 &)
-echo -e "\nOriginal Cairo VM factorial time:" >> results
-echo "$cairo_factorial_time" >> results
 
 # Integration Benchamarks
 
@@ -60,15 +34,15 @@ cleo_builtins_time=$( (time ../target/release/cleopatra-run integration_builtins
 echo -e "\nRust Cleopatra VM builtins integration time:" >> results
 echo "$cleo_builtins_time" >> results
 
-cairo_factorial_time=$( (time cairo-run --program factorial.json) 2>&1 &)
-echo -e "\nPython Original Cairo VM time:" >> results
-echo "$cairo_factorial_time" >> results
+cairo_builtins_time=$( (time cairo-run --program integration_builtins.json --layout=all) 2>&1 &)
+echo -e "\nPython Original Cairo VM builtins integration time:" >> results
+echo "$cairo_builtins_time" >> results
 
 pyenv global pypy3.7-7.3.9
 
-cairo_pypy_factorial_time=$( (time cairo-run --program factorial.json) 2>&1 &)
-echo -e "\nPyPy Original Cairo VM factorial time:" >> results
-echo "$cairo_pypy_factorial_time" >> results
+cairo_pypy_builtins_time=$( (time cairo-run --program integration_builtins.json --layout=all) 2>&1 &)
+echo -e "\nPyPy Original Cairo VM  builtins integration time:" >> results
+echo "$cairo_pypy_builtins_time" >> results
 
 # Alloc() Benchamarks
 
@@ -78,17 +52,6 @@ cairo-compile compare_arrays.cairo --output compare_arrays.json
 cleo_compare_arrays_time=$( (time ../target/release/cleopatra-run compare_arrays.json) 2>&1 &)
 echo -e "\nRust Cleopatra VM compare arrays time:" >> results
 echo "$cleo_compare_arrays_time" >> results
-
-# cairo_factorial_time=$( (time cairo-run --program compare_arrays.json) 2>&1 &)
-# echo -e "\nPython Original Cairo VM time:" >> results
-# echo "$cairo_factorial_time" >> results
-
-pyenv global pypy3.7-7.3.9
-
-# oriac_compare_arrays_time=$( (time oriac/target/release/oriac-run --program compare_arrays.json) 2>&1 &)
-# echo -e "\nOriac VM compare arrays time:" >> results
-# echo "$oriac_compare_arrays_time" >> results
-
 
 cairo_pypy_compare_arrays_time=$( (time cairo-run --program compare_arrays.json) 2>&1 &)
 echo -e "\nPyPy Original Cairo VM compare arrays time:" >> results
@@ -102,4 +65,3 @@ rm -f fibonacci.json
 rm -f factorial.json
 rm -f compare_arrays.json
 rm -f integration_builtins.json
-# rm -rf oriac
