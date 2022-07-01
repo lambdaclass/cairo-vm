cea5f5e900a0312aabef234efca4f3e6c2e77948
Juan-M-V <juan.viel@lambdaclass.com>
2022-07-01 15:29:49 -0300

    Modify paths

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rm -f ../cairo_programs/*.json
+rm -f *.json
 
 test_files=($(ls -p ../cairo_programs | grep -v / | sed -E 's/\.cairo//'))
 
 for file in ${test_files[@]}; do
-    cairo-compile "../cairo_programs/$file.cairo" --output "../cairo_programs/$file.json"
+    cairo-compile "../cairo_programs/$file.cairo" --output "$file.json"
 done
