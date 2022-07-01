9f5f1339846a580b9a17f11b77c06e91e6cb55b0
Juan-M-V <juan.viel@lambdaclass.com>
2022-07-01 17:08:10 -0300

    Change pathing

--- bench/run_benchmarks.sh
+++ bench/run_benchmarks.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+tests_path="../cairo_programs"
+
 set -e
 echo "Cleaning old results"
 rm -f results
-rm -f *.json
+rm -f $tests_path/*.json
 
 pyenv global 3.7.12
 
@@ -14,13 +16,13 @@ cargo build --release
 echo -e "\n*** fibonacci.cairo times ***" >> results
 
 echo "Compiling Fibonacci cairo program"
-cairo-compile fibonacci.cairo --output fibonacci.json
+cairo-compile $tests_path/fibonacci_1000_multirun.cairo --output $tests_path/fibonacci_1000_multirun.json
 
-cleo_fibonacci_time=$( (time ../target/release/cleopatra-run fibonacci.json) 2>&1 &)
+cleo_fibonacci_time=$( (time ../target/release/cleopatra-run $tests_path/fibonacci_1000_multirun.json) 2>&1 &)
 echo -e "\nRust Cleopatra VM fibonacci.cairo:" >> results
 echo "$cleo_fibonacci_time" >> results
 
-cairo_fibonacci_time=$( (time cairo-run --program fibonacci.json) 2>&1 &)
+cairo_fibonacci_time=$( (time cairo-run --program $tests_path/fibonacci_1000_multirun.json) 2>&1 &)
 echo -e "\nOriginal Cairo VM fibonacci.cairo:" >> results
 echo "$cairo_fibonacci_time" >> results
 
@@ -29,13 +31,13 @@ echo "$cairo_fibonacci_time" >> results
 echo -e "\n*** factorial.cairo times ***" >> results
 
 echo "Compiling factorial cairo program"
-cairo-compile factorial.cairo --output factorial.json
+cairo-compile $tests_path/factorial.cairo --output $tests_path/factorial.json
 
-cleo_factorial_time=$( (time ../target/release/cleopatra-run factorial.json) 2>&1 &)
+cleo_factorial_time=$( (time ../target/release/cleopatra-run $tests_path/factorial.json) 2>&1 &)
 echo -e "\nRust Cleopatra VM  factorial.cairo time:" >> results
 echo "$cleo_factorial_time" >> results
 
-cairo_factorial_time=$( (time cairo-run --program factorial.json) 2>&1 &)
+cairo_factorial_time=$( (time cairo-run --program $tests_path/factorial.json) 2>&1 &)
 echo -e "\nOriginal Cairo VM  factorial.cairo time:" >> results
 echo "$cairo_factorial_time" >> results
 
@@ -44,19 +46,19 @@ echo "$cairo_factorial_time" >> results
 echo -e "\n*** integration_builtins.cairo times ***" >> results
 
 echo "Compiling builtins integration cairo program"
-cairo-compile integration_builtins.cairo --output integration_builtins.json
+cairo-compile integration_builtins.cairo --output $tests_path/integration_builtins.json
 
-cleo_builtins_time=$( (time ../target/release/cleopatra-run integration_builtins.json) 2>&1 &)
+cleo_builtins_time=$( (time ../target/release/cleopatra-run $tests_path/integration_builtins.json) 2>&1 &)
 echo -e "\nRust Cleopatra VM integration_builtins.cairo time:" >> results
 echo "$cleo_builtins_time" >> results
 
-cairo_builtins_time=$( (time cairo-run --program integration_builtins.json --layout=all) 2>&1 &)
+cairo_builtins_time=$( (time cairo-run --program $tests_path/integration_builtins.json --layout=all) 2>&1 &)
 echo -e "\nOriginal Cairo VM integration_builtins.cairo time:" >> results
 echo "$cairo_builtins_time" >> results
 
 pyenv global pypy3.7-7.3.9
 
-cairo_pypy_builtins_time=$( (time cairo-run --program integration_builtins.json --layout=all) 2>&1 &)
+cairo_pypy_builtins_time=$( (time cairo-run --program $tests_path/integration_builtins.json --layout=all) 2>&1 &)
 echo -e "\nPyPy Original Cairo VM integration_builtins.cairo time:" >> results
 echo "$cairo_pypy_builtins_time" >> results
 
@@ -65,32 +67,32 @@ echo "$cairo_pypy_builtins_time" >> results
 echo -e "\n*** compare_arrays.cairo times ***" >> results
 
 echo "Compiling compare arrays cairo program"
-cairo-compile compare_arrays.cairo --output compare_arrays.json
+cairo-compile $tests_path/compare_arrays.cairo --output $tests_path/compare_arrays.json
 
-cleo_compare_arrays_time=$( (time ../target/release/cleopatra-run compare_arrays.json) 2>&1 &)
+cleo_compare_arrays_time=$( (time ../target/release/cleopatra-run $tests_path/compare_arrays.json) 2>&1 &)
 echo -e "\nRust Cleopatra VM compare_arrays.cairo time:" >> results
 echo "$cleo_compare_arrays_time" >> results
 
-cairo_pypy_compare_arrays_time=$( (time cairo-run --program compare_arrays.json) 2>&1 &)
+cairo_pypy_compare_arrays_time=$( (time cairo-run --program $tests_path/compare_arrays.json) 2>&1 &)
 echo -e "\nPyPy Original Cairo VM compare_arrays.cairo time:" >> results
 echo "$cairo_pypy_compare_arrays_time" >> results
 
 # Search Benchamarks
 
 echo "Compiling linear search cairo program"
-cairo-compile ../cairo_programs/linear-search.cairo --output linear-search.json
+cairo-compile $tests_path/linear-search.cairo --output $tests_path/linear-search.json
 
-cleo_search_time=$( (time ../target/release/cleopatra-run linear-search.json) 2>&1 &)
+cleo_search_time=$( (time ../target/release/cleopatra-run $tests_path/linear-search.json) 2>&1 &)
 echo -e "\nRust Cleopatra VM linear search time:" >> results
 echo "$cleo_search_time" >> results
 
-cairo_search_time=$( (time cairo-run --program linear-search.json) 2>&1 &)
+cairo_search_time=$( (time cairo-run --program $tests_path/linear-search.json) 2>&1 &)
 echo -e "\nPython Original Cairo VM time:" >> results
 echo "$cairo_search_time" >> results
 
 pyenv global pypy3.7-7.3.9
 
-cairo_pypy_search_time=$( (time cairo-run --program linear-search.json) 2>&1 &)
+cairo_pypy_search_time=$( (time cairo-run --program $tests_path/linear-search.json) 2>&1 &)
 echo -e "\nPyPy Original Cairo VM lineal search time:" >> results
 echo "$cairo_pypy_search_time" >> results
 
@@ -98,4 +100,4 @@ cat results
 
 echo "Cleaning results"
 rm results
-rm -f *.json
+rm -f $tests_path/*.json
