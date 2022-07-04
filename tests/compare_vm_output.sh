bb2ab7e51410cc09502dde2b17b0e3643fdd1910
Juan-M-V <juan.viel@lambdaclass.com>
2022-07-04 16:11:15 -0300

    Tidy up file fetching

--- tests/compare_vm_output.sh
+++ tests/compare_vm_output.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env sh
 
 tests_path="../cairo_programs"
+exit_code=0
 
-for file in $(ls $tests_path | grep .trace | sed -E 's/(\.cleopatra)?\.trace//'); do
+for file in $(ls $tests_path | grep .cleopatra.trace | sed -E 's/\.cleopatra\.trace//'); do
     if ! diff -q $tests_path/$file{,.cleopatra}.trace; then
         echo "Traces for $file differ"
-        exit 1
+        exit_code=1
     else
         echo "Traces for $file match"
     fi
 done
 
-for file in $(ls $tests_path | grep .memory | sed -E 's/(\.cleopatra)?\.memory//'); do
+for file in $(ls $tests_path | grep .cleopatra.memory | sed -E 's/\.cleopatra\.memory//'); do
     if ! diff -q $tests_path/$file{,.cleopatra}.memory; then
         echo "Memory differs for $file"
-        exit 1
+        exit_code=1
     else
         echo "Memory mateches for $file"
     fi
 done
+
+exit "${exit_code}"
