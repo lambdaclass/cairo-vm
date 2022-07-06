5ebcbd380437b7596d49b9bd4df72cb5572b4ee7
Juan-M-V <juan.viel@lambdaclass.com>
2022-07-06 17:21:20 -0300

    Change set for counter

--- tests/memory_comparator.py
+++ tests/memory_comparator.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 
 import sys
+from collections import Counter
 
 def main():
     filename1 = sys.argv[1]
     filename2 = sys.argv[2]
-    cairo_mem = set()
-    cleo_mem = set()
+    cairo_mem = Counter()
+    cleo_mem = Counter()
 
     with open(filename1, 'rb') as f:
         byte = f.read(40)
-        cairo_mem.add(byte)
+        cairo_mem.update(byte)
 
     with open(filename2, 'rb') as f:
         byte = f.read(40)
-        cleo_mem.add(byte)
+        cleo_mem.update(byte)
 
     assert(cairo_mem == cleo_mem)
 
