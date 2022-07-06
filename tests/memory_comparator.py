5604c36964fd5c86bfc653ffb615aa17de50d6c2
Juan-M-V <juan.viel@lambdaclass.com>
2022-07-06 14:49:41 -0300

    Add python script to compare memory

---
+++ tests/memory_comparator.py
@@ -0,0 +1,22 @@
+#!python3
+import sys
+
+def main():
+    filename1 = sys.argv[1]
+    filename2 = sys.argv[2]
+    cairo_mem = set()
+    cleo_mem = set()
+
+    with open(filename1, 'rb') as f:
+        byte = f.read(40)
+        cairo_mem.add(byte)
+
+    with open(filename2, 'rb') as f:
+        byte = f.read(40)
+        cleo_mem.add(byte)
+
+    assert(cairo_mem == cleo_mem)
+
+if __name__ == '__main__':
+    main()
+
