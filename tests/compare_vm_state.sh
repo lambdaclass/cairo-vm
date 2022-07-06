4479aa2af5eec96e6a47250689cb7c7169a8254d
Juan-M-V <juan.viel@lambdaclass.com>
2022-07-06 15:42:04 -0300

    Remove match msg; add all passed msg

--- tests/compare_vm_state.sh
+++ tests/compare_vm_state.sh
@@ -4,6 +4,8 @@ tests_path="../cairo_programs"
 exit_code=0
 trace=false
 memory=false
+declare -i passed_tests=0
+declare -i failed_tests=0
 
 for i in $@; do
     case $i in
@@ -21,8 +23,9 @@ if $trace; then
         if ! diff -q $tests_path/$file{,.cleopatra}.trace; then
             echo "Traces for $file differ"
             exit_code=1
+            failed_tests+=1
         else
-            echo "Traces for $file match"
+            passed_tests+=1
         fi
     done
 fi
@@ -32,10 +35,15 @@ if $memory; then
             if ! ./memory_comparator.py $tests_path/$file{,.cleopatra}.memory > /dev/null 2>&1; then
             echo "Memory differs for $file"
             exit_code=1
+            failed_tests+=1
         else
-            echo "Memory matches for $file"
+            passed_tests+=1
         fi
     done
+fi
+
+if (($failed_tests == 0)); then
+    echo "All $passed_tests tests passed; no discrepancies found"
 fi
 
 exit "${exit_code}"
