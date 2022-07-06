6646c8fa58bc1d565ff18ea641586f70e47fc0ff
Pedro Fontana <pedro.fontana@lambdaclass.com>
2022-07-06 10:27:35 -0300

    cargo fmt

--- src/vm/hints/execute_hint.rs
+++ src/vm/hints/execute_hint.rs
@@ -4,7 +4,9 @@ use num_bigint::BigInt;
 
 use crate::types::instruction::Register;
 use crate::vm::errors::vm_errors::VirtualMachineError;
-use crate::vm::hints::hint_utils::{add_segment, assert_le_felt, is_nn, is_nn_out_of_range, assert_not_zero};
+use crate::vm::hints::hint_utils::{
+    add_segment, assert_le_felt, assert_not_zero, is_nn, is_nn_out_of_range,
+};
 use crate::vm::vm_core::VirtualMachine;
 
 #[derive(Debug, PartialEq, Clone)]
