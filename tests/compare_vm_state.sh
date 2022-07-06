bdca28d04867fc11679d5f359a809c0b98c4e431
Juan-M-V <juan.viel@lambdaclass.com>
2022-07-06 15:48:38 -0300

    Clarify failure message

--- tests/compare_vm_state.sh
+++ tests/compare_vm_state.sh
@@ -45,7 +45,7 @@ fi
 if (($failed_tests == 0)); then
     echo "All $passed_tests tests passed; no discrepancies found"
 else
-        echo "Comparisons: $failed_tests failed, $passed_tests passed out of $(($failed_tests + $passed_tests))" 
+        echo "Comparisons: $failed_tests failed, $passed_tests passed, $(($failed_tests + $passed_tests)) total" 
 fi
 
 exit "${exit_code}"
