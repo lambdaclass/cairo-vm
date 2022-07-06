9a1de9b6ae0a68fbee48834a46629ba400bc0439
Juan-M-V <juan.viel@lambdaclass.com>
2022-07-06 15:29:37 -0300

    Add trace comparison argument

--- tests/compare_vm_output.sh
+++ tests/compare_vm_output.sh
@@ -2,15 +2,27 @@
 
 tests_path="../cairo_programs"
 exit_code=0
+trace=true
 
-for file in $(ls $tests_path | grep .cleopatra.trace | sed -E 's/\.cleopatra\.trace//'); do
-    if ! diff -q $tests_path/$file{,.cleopatra}.trace; then
-        echo "Traces for $file differ"
-        exit_code=1
-    else
-        echo "Traces for $file match"
-    fi
+for i in $@; do
+    case $i in
+        "trace") trace=1
+        ;;
+        *)
+        ;;
+    esac
 done
+
+if $trace; then
+    for file in $(ls $tests_path | grep .cleopatra.trace | sed -E 's/\.cleopatra\.trace//'); do
+        if ! diff -q $tests_path/$file{,.cleopatra}.trace; then
+            echo "Traces for $file differ"
+            exit_code=1
+        else
+            echo "Traces for $file match"
+        fi
+    done
+fi
 
 for file in $(ls $tests_path | grep .cleopatra.memory | sed -E 's/\.cleopatra\.memory//'); do
         if ! ./memory_comparator.py $tests_path/$file{,.cleopatra}.memory > /dev/null 2>&1; then
