7cd3aab1223849094bf10b6b1c0fc2c67032bac5
Juan-M-V <juan.viel@lambdaclass.com>
2022-07-07 10:09:17 -0300

    Remove bashisms

--- tests/compare_vm_state.sh
+++ tests/compare_vm_state.sh
@@ -4,8 +4,8 @@ tests_path="../cairo_programs"
 exit_code=0
 trace=false
 memory=false
-declare -i passed_tests=0
-declare -i failed_tests=0
+passed_tests=0
+failed_tests=0
 
 for i in $@; do
     case $i in
@@ -19,33 +19,35 @@ for i in $@; do
 done
 
 if $trace; then
-    for file in $(ls $tests_path | grep .cleopatra.trace | sed -E 's/\.cleopatra\.trace//'); do
-        if ! diff -q $tests_path/$file{,.cleopatra}.trace; then
+    for file in $(ls $tests_path | grep .cairo | sed -E 's/\.cairo//'); do
+        path_file="$tests_path/$file"
+        if ! diff -q $path_file.trace $path_file.cleopatra.trace; then
             echo "Traces for $file differ"
             exit_code=1
-            failed_tests+=1
+            failed_tests=$((failed_tests + 1))
         else
-            passed_tests+=1
+            passed_tests=$((passed_tests + 1))
         fi
     done
 fi
 
 if $memory; then
     for file in $(ls $tests_path | grep .cleopatra.memory | sed -E 's/\.cleopatra\.memory//'); do
-            if ! ./memory_comparator.py $tests_path/$file{,.cleopatra}.memory > /dev/null 2>&1; then
+        path_file="$tests_path/$file"
+        if ! ./memory_comparator.py $path_file.cleopatra.memory $path_file.memory> /dev/null 2>&1; then
             echo "Memory differs for $file"
             exit_code=1
-            failed_tests+=1
+            failed_tests=$((failed_tests + 1))
         else
-            passed_tests+=1
+            passed_tests=$((passed_tests + 1))
         fi
     done
 fi
 
-if (($failed_tests == 0)); then
+if test $failed_tests = 0; then
     echo "All $passed_tests tests passed; no discrepancies found"
 else
-        echo "Comparisons: $failed_tests failed, $passed_tests passed, $(($failed_tests + $passed_tests)) total" 
+        echo "Comparisons: $failed_tests failed, $passed_tests passed, $((failed_tests + passed_tests)) total" 
 fi
 
 exit "${exit_code}"
