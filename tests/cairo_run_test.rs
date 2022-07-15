76393056d160f617b1c880eca73963abf9c24613
Juan-M-V <juan.viel@lambdaclass.com>
2022-07-15 11:20:28 -0300

    Add integration test

--- tests/cairo_run_test.rs
+++ tests/cairo_run_test.rs
@@ -107,3 +107,9 @@ fn cairo_run_unsigned_div_rem() {
     cairo_run::cairo_run(Path::new("cairo_programs/unsigned_div_rem.json"))
         .expect("Couldn't run program");
 }
+
+#[test]
+fn cairo_run_signed_div_rem() {
+    cairo_run::cairo_run(Path::new("cairo_programs/signed_div_rem.json"))
+        .expect("Couldn't run program");
+}
