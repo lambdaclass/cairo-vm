778e76b40f697ecc0780f8e74a6dc01a2615d3ee
Pedro Fontana <pedro.fontana@lambdaclass.com>
2022-07-18 15:53:54 -0300

    avoid serde error when deserializing program

--- src/serde/deserialize_utils.rs
+++ src/serde/deserialize_utils.rs
@@ -68,14 +68,20 @@ pub fn parse_dereference(value: &str) -> Result<ValueAddress, ReferenceParseErro
         // issue: https://github.com/lambdaclass/cleopatra_cairo/issues/280
 
         // _ => Err(ReferenceParseError::InvalidStringError(String::from(value))),
-        _ => Ok(ValueAddress {
-            register: Some(Register::FP),
-            offset1: 0,
-            offset2: 0,
-            immediate: None,
-            dereference: true,
-            inner_dereference: false,
-        }),
+        _ => {
+            println!(
+                "Warning: {:?}: \n Avoid Parse Error creating a default ValueAddres",
+                ReferenceParseError::InvalidStringError(String::from(value))
+            );
+            Ok(ValueAddress {
+                register: Some(Register::FP),
+                offset1: 0,
+                offset2: 0,
+                immediate: None,
+                dereference: true,
+                inner_dereference: false,
+            })
+        }
     }
 }
 // parse string values of format `[cast(reg, *felt)]`
@@ -157,14 +163,20 @@ pub fn parse_reference(value: &str) -> Result<ValueAddress, ReferenceParseError>
         // issue: https://github.com/lambdaclass/cleopatra_cairo/issues/280
 
         // _ => Err(ReferenceParseError::InvalidStringError(String::from(value))),
-        _ => Ok(ValueAddress {
-            register: Some(Register::FP),
-            offset1: 0,
-            offset2: 0,
-            immediate: None,
-            dereference: false,
-            inner_dereference: false,
-        }),
+        _ => {
+            println!(
+                "Warning: {:?}: \n Avoid Parse Error creating a default ValueAddres",
+                ReferenceParseError::InvalidStringError(String::from(value))
+            );
+            Ok(ValueAddress {
+                register: Some(Register::FP),
+                offset1: 0,
+                offset2: 0,
+                immediate: None,
+                dereference: false,
+                inner_dereference: false,
+            })
+        }
     }
 }
 
