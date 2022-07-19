7521e47b99b96de169452bec5b8f03e5fca63855
Pedro Fontana <pedro.fontana@lambdaclass.com>
2022-07-19 18:53:51 -0300

    add draft implementation of memset hints

--- tests/cairo_run_test.rs
+++ tests/cairo_run_test.rs
@@ -153,6 +153,12 @@ fn cairo_run_memcpy() {
 }
 
 #[test]
+fn cairo_run_memset() {
+    cairo_run::cairo_run(Path::new("cairo_programs/memset.json"), false)
+        .expect("Couldn't run program");
+}
+
+#[test]
 fn cairo_run_dict() {
     cairo_run::cairo_run(Path::new("cairo_programs/dict.json"), false)
         .expect("Couldn't run program");
