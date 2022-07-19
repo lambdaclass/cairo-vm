37f78ff97f9c31563a7f7c422fbb331746964d0f
Mario J. Rugiero <mrugiero@gmail.com>
2022-07-19 15:33:48 -0300

    Properly compare runs with Hyperfine

--- bench/run_benchmarks.sh
+++ bench/run_benchmarks.sh
@@ -2,28 +2,14 @@
 tests_path="../cairo_programs/benchmarks"
 
 set -e
-echo "Cleaning old results"
-rm -f results
 
 for file in $(ls $tests_path | grep .cairo | sed -E 's/\.cairo//'); do
     echo "Running $file benchmark"
-    echo "\n*** $file.cairo times ***" >> results
 
     export PATH="$(pyenv root)/shims:$PATH"
 
-    echo "\nOriginal Cairo VM:"
-    pyenv local 3.7.12
-    hyperfine "cairo-run --layout all --program $tests_path/$file.json"
-
-    echo "\nPyPy Cairo VM:"
-    pyenv local pypy3.7-7.3.9
-    hyperfine "cairo-run --layout all --program $tests_path/$file.json"
-
-    echo "\nRust Cleopatra VM:"
-    hyperfine "../target/release/cleopatra-run $tests_path/$file.json"
+    hyperfine \
+	    -n "Cairo VM (CPython)" "PYENV_VERSION=3.7.12 cairo-run --layout all --program $tests_path/$file.json" \
+	    -n "Cairo VM (PyPy)" "PYENV_VERSION=pypy3.7-7.3.9 cairo-run --layout all --program $tests_path/$file.json" \
+	    -n "Cleopatra VM (Rust)" "../target/release/cleopatra-run $tests_path/$file.json"
 done
-
-cat results
-
-echo "Cleaning results"
-rm -f results
