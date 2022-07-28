6f010aa26656e25ffa26bbcba9092e32b97ac715
Pedro Fontana <pedro.fontana@lambdaclass.com>
2022-07-28 18:05:47 -0300

    add verify_zero unit tests

--- src/vm/hints/secp/field_utils.rs
+++ src/vm/hints/secp/field_utils.rs
@@ -12,7 +12,7 @@ use num_integer::Integer;
 use num_traits::Zero;
 use std::collections::HashMap;
 
-use super::secp_utils::pack;
+use crate::vm::hints::secp::secp_utils::pack;
 
 /*
 Implements hint:
@@ -38,7 +38,7 @@ pub fn verify_zero(
 
     let pack = pack(val_d0, val_d1, val_d2, &vm.prime);
 
-    // SECP_P = 2**256 - 2**32 - 2**9 - 2**8 - 2**7 - 2**6 - 2**4 - 1
+    //SECP_P = 2**256 - 2**32 - 2**9 - 2**8 - 2**7 - 2**6 - 2**4 - 1
     let sec_p = bigint_str!(
         b"115792089237316195423570985008687907853269984665640564039457584007908834671663"
     );
@@ -56,4 +56,211 @@ pub fn verify_zero(
     vm.memory
         .insert(&q_address, &MaybeRelocatable::from(q.mod_floor(&vm.prime)))
         .map_err(VirtualMachineError::MemoryError)
+}
+
+#[cfg(test)]
+mod tests {
+    use num_bigint::Sign;
+
+    use super::*;
+    use crate::bigint;
+    use crate::types::instruction::Register;
+    use crate::types::relocatable::MaybeRelocatable;
+    use crate::vm::hints::execute_hint::{execute_hint, HintReference};
+    use crate::vm::runners::builtin_runner::RangeCheckBuiltinRunner;
+    use num_traits::FromPrimitive;
+
+    #[test]
+    fn run_verify_zero_ok() {
+        let hint_code = "from starkware.cairo.common.cairo_secp.secp_utils import SECP_P, pack\n\nq, r = divmod(pack(ids.val, PRIME), SECP_P)\nassert r == 0, f\"verify_zero: Invalid input {ids.val.d0, ids.val.d1, ids.val.d2}.\"\nids.q = q % PRIME".as_bytes();
+        let mut vm = VirtualMachine::new(
+            BigInt::new(Sign::Plus, vec![1, 0, 0, 0, 0, 0, 17, 134217728]),
+            vec![(
+                "range_check".to_string(),
+                Box::new(RangeCheckBuiltinRunner::new(true, bigint!(8), 8)),
+            )],
+            false,
+        );
+        for _ in 0..3 {
+            vm.segments.add(&mut vm.memory, None);
+        }
+
+        //Initialize fp
+        vm.run_context.fp = MaybeRelocatable::from((1, 9));
+
+        //Initialize ap
+        vm.run_context.ap = MaybeRelocatable::from((1, 9));
+
+        //Create ids
+        let mut ids = HashMap::<String, BigInt>::new();
+        ids.insert(String::from("val"), bigint!(0));
+        ids.insert(String::from("q"), bigint!(1));
+
+        //Create references
+        vm.references = HashMap::from([
+            (
+                0,
+                HintReference {
+                    register: Register::FP,
+                    offset1: -5,
+                    offset2: 0,
+                    inner_dereference: false,
+                    ap_tracking_data: Some(ApTracking {
+                        group: 1,
+                        offset: 0,
+                    }),
+                },
+            ),
+            (
+                1,
+                HintReference {
+                    register: Register::AP,
+                    offset1: 0,
+                    offset2: 0,
+                    inner_dereference: false,
+                    ap_tracking_data: Some(ApTracking {
+                        group: 1,
+                        offset: 0,
+                    }),
+                },
+            ),
+        ]);
+
+        //Create AP tracking
+        let ap_tracking = ApTracking {
+            group: 1,
+            offset: 0,
+        };
+
+        //Insert ids.val.d0 into memory
+        vm.memory
+            .insert(
+                &MaybeRelocatable::from((1, 4)),
+                &MaybeRelocatable::from(bigint!(0)),
+            )
+            .unwrap();
+
+        //Insert ids.val.d1 into memory
+        vm.memory
+            .insert(
+                &MaybeRelocatable::from((1, 5)),
+                &MaybeRelocatable::from(bigint!(0)),
+            )
+            .unwrap();
+
+        //Insert ids.val.d2 into memory
+        vm.memory
+            .insert(
+                &MaybeRelocatable::from((1, 6)),
+                &MaybeRelocatable::from(bigint!(0)),
+            )
+            .unwrap();
+
+        //Execute the hint
+        assert_eq!(execute_hint(&mut vm, hint_code, ids, &ap_tracking), Ok(()));
+
+        //Check hint memory inserts
+        //ids.q
+        assert_eq!(
+            vm.memory.get(&MaybeRelocatable::from((1, 9))),
+            Ok(Some(&MaybeRelocatable::from(bigint!(0))))
+        );
+    }
+
+    #[test]
+    fn run_verify_zero_error() {
+        let hint_code = "from starkware.cairo.common.cairo_secp.secp_utils import SECP_P, pack\n\nq, r = divmod(pack(ids.val, PRIME), SECP_P)\nassert r == 0, f\"verify_zero: Invalid input {ids.val.d0, ids.val.d1, ids.val.d2}.\"\nids.q = q % PRIME".as_bytes();
+        let mut vm = VirtualMachine::new(
+            BigInt::new(Sign::Plus, vec![1, 0, 0, 0, 0, 0, 17, 134217728]),
+            vec![(
+                "range_check".to_string(),
+                Box::new(RangeCheckBuiltinRunner::new(true, bigint!(8), 8)),
+            )],
+            false,
+        );
+        for _ in 0..3 {
+            vm.segments.add(&mut vm.memory, None);
+        }
+
+        //Initialize fp
+        vm.run_context.fp = MaybeRelocatable::from((1, 9));
+
+        //Initialize ap
+        vm.run_context.ap = MaybeRelocatable::from((1, 9));
+
+        //Create ids
+        let mut ids = HashMap::<String, BigInt>::new();
+        ids.insert(String::from("val"), bigint!(0));
+        ids.insert(String::from("q"), bigint!(1));
+
+        //Create references
+        vm.references = HashMap::from([
+            (
+                0,
+                HintReference {
+                    register: Register::FP,
+                    offset1: -5,
+                    offset2: 0,
+                    inner_dereference: false,
+                    ap_tracking_data: Some(ApTracking {
+                        group: 1,
+                        offset: 0,
+                    }),
+                },
+            ),
+            (
+                1,
+                HintReference {
+                    register: Register::AP,
+                    offset1: 0,
+                    offset2: 0,
+                    inner_dereference: false,
+                    ap_tracking_data: Some(ApTracking {
+                        group: 1,
+                        offset: 0,
+                    }),
+                },
+            ),
+        ]);
+
+        //Create AP tracking
+        let ap_tracking = ApTracking {
+            group: 1,
+            offset: 0,
+        };
+
+        //Insert ids.val.d0 into memory
+        vm.memory
+            .insert(
+                &MaybeRelocatable::from((1, 4)),
+                &MaybeRelocatable::from(bigint!(0)),
+            )
+            .unwrap();
+
+        //Insert ids.val.d1 into memory
+        vm.memory
+            .insert(
+                &MaybeRelocatable::from((1, 5)),
+                &MaybeRelocatable::from(bigint!(0)),
+            )
+            .unwrap();
+
+        //Insert ids.val.d2 into memory
+        vm.memory
+            .insert(
+                &MaybeRelocatable::from((1, 6)),
+                &MaybeRelocatable::from(bigint!(150)),
+            )
+            .unwrap();
+
+        //Execute the hint
+        assert_eq!(
+            execute_hint(&mut vm, hint_code, ids, &ap_tracking),
+            Err(VirtualMachineError::SecpVerifyZero(
+                bigint!(0),
+                bigint!(0),
+                bigint!(150)
+            ))
+        );
+    }
 }
