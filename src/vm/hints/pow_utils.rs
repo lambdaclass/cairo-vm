cb585a0b274a7198db5e1054713a15a9a3a9fc2e
Federica Moletta <fedemoletta@hotmail.com>
2022-08-01 17:19:15 -0300

    Refactor pow hint

--- src/vm/hints/pow_utils.rs
+++ src/vm/hints/pow_utils.rs
@@ -1,13 +1,14 @@
-use crate::bigint;
 use crate::serde::deserialize_program::ApTracking;
-use crate::types::relocatable::MaybeRelocatable;
+use crate::types::relocatable::Relocatable;
 use crate::vm::errors::vm_errors::VirtualMachineError;
-use crate::vm::hints::hint_utils::get_address_from_reference;
 use crate::vm::vm_core::VirtualMachine;
+use crate::{bigint, relocatable};
 use num_bigint::BigInt;
 use num_integer::Integer;
 use num_traits::FromPrimitive;
 use std::collections::HashMap;
+
+use super::hint_utils::{get_relocatable_from_var_name, insert_integer_from_var_name};
 
 /*
 Implements hint:
@@ -18,62 +19,12 @@ pub fn pow(
     ids: HashMap<String, BigInt>,
     hint_ap_tracking: Option<&ApTracking>,
 ) -> Result<(), VirtualMachineError> {
-    //Check that ids contains the reference id for the variables used by the hint
-    let (prev_locs_ref, locs_ref) = if let (Some(prev_locs_ref), Some(locs_ref)) = (
-        ids.get(&String::from("prev_locs")),
-        ids.get(&String::from("locs")),
-    ) {
-        (prev_locs_ref, locs_ref)
-    } else {
-        return Err(VirtualMachineError::IncorrectIds(
-            vec![String::from("prev_locs"), String::from("locs")],
-            ids.into_keys().collect(),
-        ));
-    };
-
-    // Get the addresses of the variables used in the hints
-    let (prev_locs_addr, locs_addr) = if let (
-        Ok(Some(MaybeRelocatable::RelocatableValue(prev_locs_addr))),
-        Ok(Some(MaybeRelocatable::RelocatableValue(locs_addr))),
-    ) = (
-        get_address_from_reference(
-            prev_locs_ref,
-            &vm.references,
-            &vm.run_context,
-            vm,
-            hint_ap_tracking,
-        ),
-        get_address_from_reference(
-            locs_ref,
-            &vm.references,
-            &vm.run_context,
-            vm,
-            hint_ap_tracking,
-        ),
-    ) {
-        (prev_locs_addr, locs_addr)
-    } else {
-        return Err(VirtualMachineError::FailedToGetIds);
-    };
-
-    let prev_locs_exp_addr =
-        MaybeRelocatable::from((prev_locs_addr.segment_index, prev_locs_addr.offset + 4));
-
-    match vm.memory.get(&prev_locs_exp_addr) {
-        Ok(Some(MaybeRelocatable::Int(prev_locs_exp))) => {
-            let locs_bit = prev_locs_exp.mod_floor(&vm.prime) & bigint!(1);
-
-            vm.memory
-                .insert(
-                    &MaybeRelocatable::RelocatableValue(locs_addr),
-                    &MaybeRelocatable::Int(locs_bit),
-                )
-                .map_err(VirtualMachineError::MemoryError)?;
-            Ok(())
-        }
-        Ok(_) => Err(VirtualMachineError::ExpectedInteger(prev_locs_exp_addr)),
-        Err(memory_error) => Err(VirtualMachineError::MemoryError(memory_error)),
-    }
+    let prev_locs_addr = get_relocatable_from_var_name("prev_locs", &ids, vm, hint_ap_tracking)?;
+    let prev_locs_exp_addr = relocatable!(prev_locs_addr.segment_index, prev_locs_addr.offset + 4);
+    let prev_locs_exp = vm.memory.get_integer(&prev_locs_exp_addr)?;
+    let locs_bit = prev_locs_exp.mod_floor(&vm.prime) & bigint!(1);
+    insert_integer_from_var_name("locs", locs_bit, &ids, vm, hint_ap_tracking)?;
+    Ok(())
 }
 
 #[cfg(test)]
@@ -194,10 +145,7 @@ mod tests {
         //Execute the hint
         assert_eq!(
             execute_hint(&mut vm, hint_code, ids, &ap_tracking),
-            Err(VirtualMachineError::IncorrectIds(
-                vec![String::from("prev_locs"), String::from("locs")],
-                vec![String::from("locs")]
-            ))
+            Err(VirtualMachineError::FailedToGetIds)
         );
     }
 
@@ -256,7 +204,9 @@ mod tests {
         //Execute the hint
         assert_eq!(
             execute_hint(&mut vm, hint_code, ids, &ap_tracking),
-            Err(VirtualMachineError::FailedToGetIds)
+            Err(VirtualMachineError::ExpectedInteger(
+                MaybeRelocatable::from((1, 10))
+            ))
         );
     }
 
