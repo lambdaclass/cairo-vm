013309489b14161dadc14751be271b753d6ef8c7
Pedro Fontana <pedro.fontana@lambdaclass.com>
2022-08-02 18:34:20 -0300

    add unit tests

--- src/vm/hints/secp/ec_utils.rs
+++ src/vm/hints/secp/ec_utils.rs
@@ -95,3 +95,153 @@ pub fn compute_doubling_slope(
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::bigint_str;
+    use crate::types::instruction::Register;
+    use crate::types::relocatable::MaybeRelocatable;
+    use crate::utils::test_utils::*;
+    use crate::vm::errors::memory_errors::MemoryError;
+    use crate::vm::hints::execute_hint::{execute_hint, HintReference};
+    use crate::vm::runners::builtin_runner::RangeCheckBuiltinRunner;
+    use crate::vm::vm_memory::memory::Memory;
+
+    #[test]
+    fn run_ec_negative_ok() {
+        let hint_code = "from starkware.cairo.common.cairo_secp.secp_utils import SECP_P, pack\n\ny = pack(ids.point.y, PRIME) % SECP_P\n# The modulo operation in python always returns a nonnegative number.\nvalue = (-y) % SECP_P".as_bytes();
+        let mut vm = VirtualMachine::new(
+            VM_PRIME.clone(),
+            vec![(
+                "range_check".to_string(),
+                Box::new(RangeCheckBuiltinRunner::new(true, bigint!(8), 8)),
+            )],
+            false,
+        );
+
+        vm.memory = memory![((1, 3), 2645), ((1, 4), 454), ((1, 5), 206)];
+
+        //Initialize fp
+        vm.run_context.fp = MaybeRelocatable::from((1, 8));
+
+        //Create ids
+        let mut ids = HashMap::<String, BigInt>::new();
+        ids.insert(String::from("point"), bigint!(0));
+
+        //Create references
+        vm.references = HashMap::from([(
+            0,
+            HintReference {
+                register: Register::FP,
+                offset1: -8,
+                offset2: 0,
+                inner_dereference: false,
+                immediate: None,
+                ap_tracking_data: Some(ApTracking {
+                    group: 1,
+                    offset: 0,
+                }),
+            },
+        )]);
+
+        //Check 'value' is not defined in the vm scope
+        assert_eq!(
+            vm.exec_scopes.get_local_variables().unwrap().get("value"),
+            None
+        );
+
+        //Execute the hint
+        assert_eq!(
+            execute_hint(&mut vm, hint_code, ids, &ApTracking::new()),
+            Ok(())
+        );
+
+        //Check 'value' is defined in the vm scope
+        assert_eq!(
+            vm.exec_scopes.get_local_variables().unwrap().get("value"),
+            Some(&PyValueType::BigInt(bigint_str!(
+                b"115792089237316195423569751828682367333329274433232027476421668138471189901786"
+            )))
+        );
+    }
+
+    #[test]
+    fn run_compute_doubling_slope_ok() {
+        let hint_code = "from starkware.cairo.common.cairo_secp.secp_utils import SECP_P, pack\nfrom starkware.python.math_utils import ec_double_slope\n\n# Compute the slope.\nx = pack(ids.point.x, PRIME)\ny = pack(ids.point.y, PRIME)\nvalue = slope = ec_double_slope(point=(x, y), alpha=0, p=SECP_P)".as_bytes();
+        let mut vm = VirtualMachine::new(
+            VM_PRIME.clone(),
+            vec![(
+                "range_check".to_string(),
+                Box::new(RangeCheckBuiltinRunner::new(true, bigint!(8), 8)),
+            )],
+            false,
+        );
+
+        vm.memory = memory![
+            ((1, 0), 614323),
+            ((1, 1), 5456867),
+            ((1, 2), 101208),
+            ((1, 3), 773712524),
+            ((1, 4), 77371252),
+            ((1, 5), 5298795)
+        ];
+
+        //Initialize fp
+        vm.run_context.fp = MaybeRelocatable::from((1, 8));
+
+        //Create ids
+        let mut ids = HashMap::<String, BigInt>::new();
+        ids.insert(String::from("point"), bigint!(0));
+
+        //Create references
+        vm.references = HashMap::from([(
+            0,
+            HintReference {
+                register: Register::FP,
+                offset1: -8,
+                offset2: 0,
+                inner_dereference: false,
+                immediate: None,
+                ap_tracking_data: Some(ApTracking {
+                    group: 1,
+                    offset: 0,
+                }),
+            },
+        )]);
+
+        //Check 'value' is not defined in the vm scope
+        assert_eq!(
+            vm.exec_scopes.get_local_variables().unwrap().get("value"),
+            None
+        );
+
+        //Check 'slope' is not defined in the vm scope
+        assert_eq!(
+            vm.exec_scopes.get_local_variables().unwrap().get("slope"),
+            None
+        );
+
+        //Execute the hint
+        assert_eq!(
+            execute_hint(&mut vm, hint_code, ids, &ApTracking::new()),
+            Ok(())
+        );
+
+        //Check 'value' is defined in the vm scope
+        assert_eq!(
+            vm.exec_scopes.get_local_variables().unwrap().get("value"),
+            Some(&PyValueType::BigInt(bigint_str!(
+                b"40442433062102151071094722250325492738932110061897694430475034100717288403728"
+            )))
+        );
+
+        //Check 'slope' is defined in the vm scope
+        assert_eq!(
+            vm.exec_scopes.get_local_variables().unwrap().get("slope"),
+            Some(&PyValueType::BigInt(bigint_str!(
+                b"40442433062102151071094722250325492738932110061897694430475034100717288403728"
+            )))
+        );
+    }
+}
