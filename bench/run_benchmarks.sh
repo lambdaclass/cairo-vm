21e89d61353f7c09f0e9ffaa071dd638caf14d65
Federica Moletta <fedemoletta@hotmail.com>
2022-08-23 13:54:42 -0300

    Change name in run_benchmark.sh

--- bench/run_benchmarks.sh
+++ bench/run_benchmarks.sh
@@ -11,5 +11,5 @@ for file in $(ls $tests_path | grep .cairo | sed -E 's/\.cairo//'); do
     hyperfine \
 	    -n "Cairo VM (CPython)" "PYENV_VERSION=3.7.12 cairo-run --layout all --program $tests_path/$file.json" \
 	    -n "Cairo VM (PyPy)" "PYENV_VERSION=pypy3.7-7.3.9 cairo-run --layout all --program $tests_path/$file.json" \
-	    -n "Cleopatra VM (Rust)" "../target/release/cairo-rs-run $tests_path/$file.json"
+	    -n "cairo-rs (Rust)" "../target/release/cairo-rs-run $tests_path/$file.json"
 done
