2cdcae7b2ef38863c744a86f090c7b4edd296ed0
Juan-M-V <juan.viel@lambdaclass.com>
2022-08-31 10:26:55 -0300

    Use already implemented 8 byte from

--- src/utils.rs
+++ src/utils.rs
@@ -519,11 +519,10 @@ mod test {
 
     #[test]
     fn data_vec_test() {
-        from_bigint_str![3];
-        let data = vec_data!((1), ((2, 2)), ((b"499", 10)), ((b"a9c", 16)));
+        let data = vec_data!((1), ((2, 2)), ((b"49128305", 10)), ((b"3b6f00a9", 16)));
         assert_eq!(data[0], mayberelocatable!(1));
         assert_eq!(data[1], mayberelocatable!(2, 2));
-        assert_eq!(data[2], mayberelocatable!(499));
-        assert_eq!(data[3], mayberelocatable!(2716));
+        assert_eq!(data[2], mayberelocatable!(49128305));
+        assert_eq!(data[3], mayberelocatable!(997130409));
     }
 }
