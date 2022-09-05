099dac826b1cc9a33d4230371f051d3236dc355e
Pedro Fontana <pedro.fontana@lambdaclass.com>
2022-09-05 17:42:21 -0300

    serde memory

--- unix_socket_example/ext_process.py
+++ unix_socket_example/ext_process.py
@@ -1,7 +1,9 @@
 import socket
 import os
 import sys
+from typing import Dict
 import utils
+import time
 import json
 
 socket_path = "ipc.sock"
@@ -18,19 +20,41 @@ s.listen()
 
 while 1:
     conn, addr = s.accept()
+    xs = bytearray()
 
+    memory = dict()
+
+    t0 = time.time()
     try:
         while 1:
             data = conn.recv(8192)
             if data:
-                print("type", type(data))
-                print("received ", data)
+                xs.extend(data)
+                # final =+ data
+                # print("received ", data)
 
                 # x = utils.Memory
                 # x = json.loads(data)
                 # print("x ", x.data)
                 # print("type x", type(x))
+            # print(xs)
+            else:
+                break
+        t1 = time.time()
 
+        x = json.loads(xs)
+        for i, value in enumerate(x['data']):
+            if value.__contains__('Int'):
+                memory[(0, i)] = value['Int']
+            else:
+                memory[(0, i)] = (value['RelocatableValue']['segment_index'], value['RelocatableValue']['offset'])
+        t2 = time.time()
+
+        conn.close()
+        # print(memory)
+        print("load time", t1- t0)
+        print("execution time", t2- t1)
+        break
 
     finally:
         conn.__exit__
