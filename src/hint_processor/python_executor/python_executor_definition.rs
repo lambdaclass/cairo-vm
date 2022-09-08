3b94712e41d13d36d6cfaea6c250059ff53b331e
Federica Moletta <fedemoletta@hotmail.com>
2022-09-08 16:38:28 -0300

    Modularize

--- src/hint_processor/python_executor/python_executor_definition.rs
+++ src/hint_processor/python_executor/python_executor_definition.rs
@@ -60,87 +60,103 @@ impl PythonExecutor {
             VirtualMachineError::PythonHint("Failed to establish connection".to_string())
         })?;
         //Send initial python data
-        let ids = get_python_compatible_ids(vm, &hint_data.ids_data, &hint_data.ap_tracking)?;
-        let python_data = PythonData {
-            code: hint_data.code.clone(),
-            ap: (1, vm.run_context.ap),
-            fp: (1, vm.run_context.fp),
-            ids,
-        };
-        let serialized_data = serde_json::to_string(&python_data)
-            .map_err(|_| VirtualMachineError::PythonHint("Failed to serielize data".to_string()))?;
+        let serialized_data = get_serialized_python_data(hint_data, vm)?;
         stream.write_all(serialized_data.as_bytes()).unwrap();
-        //Start operation loop
-        let mut finished_hint = false;
-        let mut counter = 3; //Counter is a temporary measure to prevent infinite looping
-        while !finished_hint && counter != 0 {
-            //Read requests from python process
-            let mut response = [0; 1024];
-            stream.read(&mut response).unwrap();
-            let json_data = std::str::from_utf8(&response)
-                .unwrap()
-                .trim_end_matches('\0');
-            let python_operation: PythonOperation = serde_json::from_str(json_data).unwrap();
-            //Execute operations
-            match python_operation.operation {
-                "Ok" => finished_hint = true,
-                "ADD_SEGMENT" => {
-                    let base = vm.segments.add(&mut vm.memory);
-                    stream
-                        .write_all(
-                            serde_json::to_string(&(base.segment_index, base.offset))
-                                .unwrap()
-                                .as_bytes(),
-                        )
-                        .unwrap();
-                }
-                "MEMORY_INSERT" => {
-                    //Yes this code is ugly
-                    //Fix to accept BigInt too
-                    //Parse arguments & carry out operation
-                    let parse_result: Result<((usize, usize), usize), _> =
-                        serde_json::from_str(&python_operation.args.clone().unwrap());
-                    match parse_result {
-                        Err(_) => {
-                            let parse_result: Result<((usize, usize), (usize, usize)), _> =
-                                serde_json::from_str(&python_operation.args.unwrap());
-                            let parse_result = parse_result.expect("Failed argument parse");
-                            vm.memory.insert(
-                                &(Relocatable::from(parse_result.0)),
-                                &(Relocatable::from(parse_result.1)),
-                            )?;
-                        }
-                        Ok(parse_result) => {
-                            vm.memory.insert(
-                                &(Relocatable::from(parse_result.0)),
-                                &bigint!(parse_result.1),
-                            )?;
-                        }
-                    }
-                    //Inform that the operation was succesful
-                    stream.write_all(b"Ok").unwrap();
-                }
-                "UPDATE_DATA" => {
-                    let update_data: PythonUpdate =
-                        serde_json::from_str(&python_operation.args.unwrap()).unwrap();
-                    //Perform update
-                    vm.run_context.ap = update_data.ap;
-                    vm.run_context.fp = update_data.fp;
-                    update_ids(
-                        vm,
-                        &hint_data.ids_data,
-                        &hint_data.ap_tracking,
-                        &update_data.ids,
-                    )?;
-                    //Inform that the operation was succesful
-                    stream.write_all(b"Ok").unwrap();
-                }
-                _ => (),
-            }
-            counter -= 1;
-        }
+        //Process operations
+        process_python_operations(vm, &mut stream, hint_data)?;
         Ok(())
     }
+}
+
+pub fn process_python_operations(
+    vm: &mut VirtualMachine,
+    stream: &mut TcpStream,
+    hint_data: &HintProcessorData,
+) -> Result<(), VirtualMachineError> {
+    let mut finished_hint = false;
+    let mut counter = 3; //Counter is a temporary measure to prevent infinite looping
+    while !finished_hint && counter != 0 {
+        //Read requests from python process
+        let mut response = [0; 1024];
+        stream.read(&mut response).unwrap();
+        let json_data = std::str::from_utf8(&response)
+            .unwrap()
+            .trim_end_matches('\0');
+        let python_operation: PythonOperation = serde_json::from_str(json_data).unwrap();
+        //Execute operations
+        match python_operation.operation {
+            "Ok" => finished_hint = true,
+            "ADD_SEGMENT" => {
+                let base = vm.segments.add(&mut vm.memory);
+                stream
+                    .write_all(
+                        serde_json::to_string(&(base.segment_index, base.offset))
+                            .unwrap()
+                            .as_bytes(),
+                    )
+                    .unwrap();
+            }
+            "MEMORY_INSERT" => {
+                //Yes this code is ugly
+                //Fix to accept BigInt too
+                //Parse arguments & carry out operation
+                let parse_result: Result<((usize, usize), usize), _> =
+                    serde_json::from_str(&python_operation.args.clone().unwrap());
+                match parse_result {
+                    Err(_) => {
+                        let parse_result: Result<((usize, usize), (usize, usize)), _> =
+                            serde_json::from_str(&python_operation.args.unwrap());
+                        let parse_result = parse_result.expect("Failed argument parse");
+                        vm.memory.insert(
+                            &(Relocatable::from(parse_result.0)),
+                            &(Relocatable::from(parse_result.1)),
+                        )?;
+                    }
+                    Ok(parse_result) => {
+                        vm.memory.insert(
+                            &(Relocatable::from(parse_result.0)),
+                            &bigint!(parse_result.1),
+                        )?;
+                    }
+                }
+                //Inform that the operation was succesful
+                stream.write_all(b"Ok").unwrap();
+            }
+            "UPDATE_DATA" => {
+                let update_data: PythonUpdate =
+                    serde_json::from_str(&python_operation.args.unwrap()).unwrap();
+                //Perform update
+                vm.run_context.ap = update_data.ap;
+                vm.run_context.fp = update_data.fp;
+                update_ids(
+                    vm,
+                    &hint_data.ids_data,
+                    &hint_data.ap_tracking,
+                    &update_data.ids,
+                )?;
+                //Inform that the operation was succesful
+                stream.write_all(b"Ok").unwrap();
+            }
+            _ => (),
+        }
+        counter -= 1;
+    }
+    Ok(())
+}
+
+pub fn get_serialized_python_data(
+    hint_data: &HintProcessorData,
+    vm: &VirtualMachine,
+) -> Result<String, VirtualMachineError> {
+    let ids = get_python_compatible_ids(vm, &hint_data.ids_data, &hint_data.ap_tracking)?;
+    let python_data = PythonData {
+        code: hint_data.code.clone(),
+        ap: (1, vm.run_context.ap),
+        fp: (1, vm.run_context.fp),
+        ids,
+    };
+    Ok(serde_json::to_string(&python_data)
+        .map_err(|_| VirtualMachineError::PythonHint("Failed to serialize data".to_string()))?)
 }
 
 pub fn update_ids(
