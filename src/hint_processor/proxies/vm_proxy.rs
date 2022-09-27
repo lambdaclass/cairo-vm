6357b78b278df35282d281f2704175efb7fc8e57
Pedro Fontana <pedro.fontana@lambdaclass.com>
2022-09-27 14:02:58 -0300

    add vm.add_memory_segment method

--- src/hint_processor/proxies/vm_proxy.rs
+++ src/hint_processor/proxies/vm_proxy.rs
@@ -1,8 +1,11 @@
 use num_bigint::BigInt;
 
-use crate::vm::{
-    context::run_context::RunContext, runners::builtin_runner::BuiltinRunner,
-    vm_core::VirtualMachine, vm_memory::memory_segments::MemorySegmentManager,
+use crate::{
+    types::relocatable::Relocatable,
+    vm::{
+        context::run_context::RunContext, runners::builtin_runner::BuiltinRunner,
+        vm_core::VirtualMachine, vm_memory::memory_segments::MemorySegmentManager,
+    },
 };
 
 use super::memory_proxy::{get_memory_proxy, MemoryProxy};
@@ -24,5 +27,11 @@ pub fn get_vm_proxy(vm: &mut VirtualMachine) -> VMProxy {
         run_context: &mut vm.run_context,
         builtin_runners: &vm.builtin_runners,
         prime: &vm.prime,
+    }
+}
+
+impl VMProxy<'_> {
+    pub fn add_memory_segment(&mut self) -> Relocatable {
+        self.memory.add_segment(self.segments)
     }
 }
