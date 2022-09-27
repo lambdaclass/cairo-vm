720f1462e671a628c8e1e8bff277e19ca9784485
github-action-benchmark <github@users.noreply.github.com>
2022-09-27 17:17:25 +0000

    add Benchmark (cargo) benchmark result for 6357b78b278df35282d281f2704175efb7fc8e57

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1663957240533,
+  "lastUpdate": 1664299045777,
   "repoUrl": "https://github.com/lambdaclass/cairo-rs",
   "entries": {
     "Benchmark": [
@@ -36711,6 +36711,56 @@ window.BENCHMARK_DATA = {
             "name": "cairo_run(cairo_programs/benchmarks/linear_search.json",
             "value": 200321000,
             "range": "± 442740",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "lambdaclass",
+            "username": "lambdaclass"
+          },
+          "committer": {
+            "name": "lambdaclass",
+            "username": "lambdaclass"
+          },
+          "id": "6357b78b278df35282d281f2704175efb7fc8e57",
+          "message": "[Remove VMProxy -1] add Segments",
+          "timestamp": "2022-09-23T08:24:48Z",
+          "url": "https://github.com/lambdaclass/cairo-rs/pull/442/commits/6357b78b278df35282d281f2704175efb7fc8e57"
+        },
+        "date": 1664299042775,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "cairo_run(cairo_programs/benchmarks/compare_arrays_200000.json",
+            "value": 1530991059,
+            "range": "± 4975623",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cairo_run(cairo_programs/benchmarks/factorial_multirun.json",
+            "value": 565808825,
+            "range": "± 1229413",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cairo_run(cairo_programs/benchmarks/fibonacci_1000_multirun.json",
+            "value": 283274343,
+            "range": "± 594892",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cairo_run(cairo_programs/benchmarks/integration_builtins.json",
+            "value": 1410651388,
+            "range": "± 3640619",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cairo_run(cairo_programs/benchmarks/linear_search.json",
+            "value": 199763387,
+            "range": "± 2813412",
             "unit": "ns/iter"
           }
         ]
