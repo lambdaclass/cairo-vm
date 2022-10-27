4382d65f4e84b661ea4ab3b54d1eb5903e254ebb
Esteve Soler Arderiu <esteve.soler@fratilia.com>
2022-10-27 19:59:15 +0200

    Split tests in two.

--- src/vm/runners/builtin_runner/hash.rs
+++ src/vm/runners/builtin_runner/hash.rs
@@ -163,14 +163,20 @@ mod tests {
     }
 
     #[test]
-    fn get_memory_accesses() {
+    fn get_memory_accesses_missing_segment_used_sizes() {
         let builtin = HashBuiltinRunner::new(256);
-        let mut vm = vm!();
+        let vm = vm!();
 
         assert_eq!(
             builtin.get_memory_accesses(&vm),
             Err(MemoryError::MissingSegmentUsedSizes),
         );
+    }
+
+    #[test]
+    fn get_memory_accesses() {
+        let builtin = HashBuiltinRunner::new(256);
+        let mut vm = vm!();
 
         vm.segments.segment_used_sizes = Some(vec![0]);
         assert_eq!(builtin.get_memory_accesses(&vm), Ok(vec![]));
