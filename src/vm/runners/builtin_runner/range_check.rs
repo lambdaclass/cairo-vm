ec488c6e947588d2d5af357176154a2cbb71394d
Esteve Soler Arderiu <esteve.soler@fratilia.com>
2022-10-27 17:55:22 +0200

    Implement `BuiltinRunner::get_memory_accesses()`.

--- src/vm/runners/builtin_runner/range_check.rs
+++ src/vm/runners/builtin_runner/range_check.rs
@@ -10,7 +10,7 @@ use crate::types::relocatable::{MaybeRelocatable, Relocatable};
 use crate::vm::errors::memory_errors::MemoryError;
 use crate::vm::errors::runner_errors::RunnerError;
 use crate::vm::runners::builtin_runner::BuiltinRunner;
-use crate::vm::runners::cairo_runner::CairoRunner;
+use crate::vm::vm_core::VirtualMachine;
 use crate::vm::vm_memory::memory::{Memory, ValidationRule};
 use crate::vm::vm_memory::memory_segments::MemorySegmentManager;
 
@@ -94,8 +94,13 @@ impl BuiltinRunner for RangeCheckBuiltinRunner {
         self
     }
 
-    fn get_memory_accesses(&self, _runner: &CairoRunner) -> std::collections::HashSet<Relocatable> {
-        todo!()
+    fn get_memory_accesses(&self, vm: &VirtualMachine) -> Result<Vec<Relocatable>, MemoryError> {
+        let segment_size = vm
+            .segments
+            .get_segment_size(self.base as _)
+            .ok_or(MemoryError::MissingSegmentUsedSizes)?;
+
+        Ok((0..segment_size).map(|i| (self.base, i).into()).collect())
     }
 }
 
