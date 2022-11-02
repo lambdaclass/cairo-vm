6c82eaabb8019fc4073c22dd1b4cfbc77d3041c0
Esteve Soler Arderiu <esteve.soler@fratilia.com>
2022-11-02 11:21:12 +0100

    Fix stuff and implement testing.

--- src/vm/security.rs
+++ src/vm/security.rs
@@ -6,8 +6,8 @@ use super::{
 use crate::types::relocatable::Relocatable;
 use std::collections::HashMap;
 
-/// Verify that the complete run in a runner is safe to relocate and be ran by
-/// another Cairo program.
+/// Verify that the completed run in a runner is safe to be relocated and be
+/// used by other Cairo programs.
 ///
 /// Checks include:
 ///   - There mustn't be memory accesses to any temporary segment.
@@ -19,11 +19,9 @@ use std::collections::HashMap;
 /// Note: Each builtin is responsible for checking its own segments' data.
 pub fn verify_secure_runner(
     runner: &CairoRunner,
-    verify_builtins: Option<bool>,
+    verify_builtins: bool,
     vm: &VirtualMachine,
 ) -> Result<(), RunnerError> {
-    let verify_builtins = verify_builtins.unwrap_or(true);
-
     let program_base = runner
         .program_base
         .as_ref()
@@ -79,5 +77,139 @@ pub fn verify_secure_runner(
         }
     }
 
+    // for (_, builtin_runner) in &vm.builtin_runners {
+    //     builtin_runner.run_security_checks(vm)?;
+    // }
+
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::{
+        bigint, bigint_str, relocatable, serde::deserialize_program::ReferenceManager,
+        types::program::Program, utils::test_utils::vm,
+    };
+    use num_bigint::{BigInt, Sign};
+
+    #[test]
+    fn verify_secure_runner_without_program_base() {
+        let program = Program {
+            builtins: Vec::new(),
+            prime: bigint_str!(
+                b"3618502788666131213697322783095070105623107215331596699973092056135872020481"
+            ),
+            data: Vec::new(),
+            constants: HashMap::new(),
+            main: None,
+            hints: HashMap::new(),
+            reference_manager: ReferenceManager {
+                references: Vec::new(),
+            },
+            identifiers: HashMap::new(),
+        };
+
+        let runner = CairoRunner::new(&program).unwrap();
+        let vm = vm!();
+
+        assert_eq!(
+            verify_secure_runner(&runner, true, &vm),
+            Err(RunnerError::NoProgBase),
+        );
+    }
+
+    #[test]
+    fn verify_secure_runner_empty_memory() {
+        let program = Program {
+            builtins: Vec::new(),
+            prime: bigint_str!(
+                b"3618502788666131213697322783095070105623107215331596699973092056135872020481"
+            ),
+            data: Vec::new(),
+            constants: HashMap::new(),
+            main: Some(0),
+            hints: HashMap::new(),
+            reference_manager: ReferenceManager {
+                references: Vec::new(),
+            },
+            identifiers: HashMap::new(),
+        };
+
+        let mut runner = CairoRunner::new(&program).unwrap();
+        let mut vm = vm!();
+
+        runner.initialize(&mut vm).unwrap();
+        vm.segments.compute_effective_sizes(&vm.memory);
+        assert_eq!(verify_secure_runner(&runner, true, &vm), Ok(()));
+    }
+
+    #[test]
+    fn verify_secure_runner_program_out_of_bounds() {
+        let program = Program {
+            builtins: Vec::new(),
+            prime: bigint_str!(
+                b"3618502788666131213697322783095070105623107215331596699973092056135872020481"
+            ),
+            data: Vec::new(),
+            constants: HashMap::new(),
+            main: Some(0),
+            hints: HashMap::new(),
+            reference_manager: ReferenceManager {
+                references: Vec::new(),
+            },
+            identifiers: HashMap::new(),
+        };
+
+        let mut runner = CairoRunner::new(&program).unwrap();
+        let mut vm = vm!();
+
+        runner.initialize(&mut vm).unwrap();
+
+        vm.memory.data = vec![vec![Some(relocatable!(0, 1000).into())]];
+        vm.segments.segment_used_sizes = Some(vec![0, 0, 0, 0]);
+
+        assert_eq!(
+            verify_secure_runner(&runner, true, &vm),
+            Err(RunnerError::FailedMemoryGet(MemoryError::NumOutOfBounds)),
+        );
+    }
+
+    #[test]
+    fn verify_secure_runner_success() {
+        let program = Program {
+            builtins: Vec::new(),
+            prime: bigint_str!(
+                b"3618502788666131213697322783095070105623107215331596699973092056135872020481"
+            ),
+            data: vec![
+                bigint!(0).into(),
+                bigint!(0).into(),
+                bigint!(0).into(),
+                bigint!(0).into(),
+            ],
+            constants: HashMap::new(),
+            main: Some(0),
+            hints: HashMap::new(),
+            reference_manager: ReferenceManager {
+                references: Vec::new(),
+            },
+            identifiers: HashMap::new(),
+        };
+
+        let mut runner = CairoRunner::new(&program).unwrap();
+        let mut vm = vm!();
+
+        runner.initialize(&mut vm).unwrap();
+
+        vm.memory.data = vec![vec![
+            Some(relocatable!(1, 0).into()),
+            Some(relocatable!(2, 1).into()),
+            Some(relocatable!(3, 2).into()),
+            Some(relocatable!(4, 3).into()),
+        ]];
+        vm.segments.segment_used_sizes = Some(vec![5, 1, 2, 3, 4]);
+
+        assert_eq!(verify_secure_runner(&runner, true, &vm), Ok(()));
+    }
 }
