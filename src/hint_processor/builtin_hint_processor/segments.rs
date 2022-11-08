9236ab1f8daa9d6fe28e6872197b451467114bef
Pedro Fontana <pedro.fontana@lamdaclass.com>
2022-11-08 14:08:34 -0300

    add unit test

--- src/hint_processor/builtin_hint_processor/segments.rs
+++ src/hint_processor/builtin_hint_processor/segments.rs
@@ -24,3 +24,46 @@ pub fn relocate_segment(
     vm.memory.add_relocation_rule(src_ptr, dest_ptr)?;
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::any_box;
+    use crate::hint_processor::builtin_hint_processor::builtin_hint_processor_definition::BuiltinHintProcessor;
+    use crate::hint_processor::builtin_hint_processor::builtin_hint_processor_definition::HintProcessorData;
+    use crate::hint_processor::builtin_hint_processor::hint_code;
+    use crate::hint_processor::hint_processor_definition::HintProcessor;
+    use crate::relocatable;
+    use crate::types::exec_scope::ExecutionScopes;
+    use crate::types::relocatable::MaybeRelocatable;
+    use crate::types::relocatable::Relocatable;
+    use crate::utils::test_utils::*;
+    use crate::vm::errors::memory_errors::MemoryError;
+    use crate::vm::vm_core::VirtualMachine;
+    use crate::vm::vm_memory::memory::Memory;
+    use num_bigint::BigInt;
+    use num_bigint::Sign;
+    use std::any::Any;
+
+    #[test]
+    fn run_relocate_segment() {
+        let hint_code = hint_code::RELOCATE_SEGMENT;
+        //Initialize vm
+        let mut vm = vm!();
+        //Initialize fp
+        vm.run_context.fp = 2;
+        //Insert ids into memory
+        vm.memory = memory![((1, 0), (-2, 0)), ((1, 1), (3, 0))];
+
+        //Create ids_data & hint_data
+        let ids_data = ids_data!["src_ptr", "dest_ptr"];
+
+        //Execute the hint
+        assert_eq!(run_hint!(vm, ids_data, hint_code), Ok(()));
+
+        assert_eq!(
+            vm.memory.relocation_rules.get(&2),
+            Some(&relocatable!(3, 0))
+        );
+    }
+}
