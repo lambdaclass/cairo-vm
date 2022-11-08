fa0ce837c52026bb9597f416a0aa5d4064627515
Pedro Fontana <pedro.fontana@lamdaclass.com>
2022-11-08 14:08:17 -0300

    fix hint

--- src/hint_processor/builtin_hint_processor/segments.rs
+++ src/hint_processor/builtin_hint_processor/segments.rs
@@ -1,5 +1,3 @@
-use crate::hint_processor::builtin_hint_processor::hint_utils::get_relocatable_from_var_name;
-
 use crate::hint_processor::hint_processor_definition::HintReference;
 
 use crate::serde::deserialize_program::ApTracking;
@@ -8,6 +6,8 @@ use crate::vm::errors::vm_errors::VirtualMachineError;
 use crate::vm::vm_core::VirtualMachine;
 
 use std::collections::HashMap;
+
+use crate::hint_processor::builtin_hint_processor::hint_utils::get_ptr_from_var_name;
 
 /*
 Implements hint:
@@ -18,8 +18,8 @@ pub fn relocate_segment(
     ids_data: &HashMap<String, HintReference>,
     ap_tracking: &ApTracking,
 ) -> Result<(), VirtualMachineError> {
-    let src_ptr = get_relocatable_from_var_name("src_ptr", vm, ids_data, ap_tracking)?;
-    let dest_ptr = get_relocatable_from_var_name("dest_ptr", vm, ids_data, ap_tracking)?;
+    let src_ptr = get_ptr_from_var_name("src_ptr", vm, ids_data, ap_tracking)?;
+    let dest_ptr = get_ptr_from_var_name("dest_ptr", vm, ids_data, ap_tracking)?;
 
     vm.memory.add_relocation_rule(src_ptr, dest_ptr)?;
     Ok(())
