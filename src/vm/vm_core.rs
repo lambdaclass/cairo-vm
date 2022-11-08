ba04aa30694272e9e1bc006ab740405c9f89fa73
Pedro Fontana <pedro.fontana@lamdaclass.com>
2022-11-08 19:21:13 -0300

    expose add_relocation_rule method

--- src/vm/vm_core.rs
+++ src/vm/vm_core.rs
@@ -804,6 +804,20 @@ impl VirtualMachine {
     pub fn add_temporary_segment(&mut self) -> Relocatable {
         self.segments.add_temporary_segment(&mut self.memory)
     }
+
+    /// Add a new relocation rule.
+    ///
+    /// Will return an error if any of the following conditions are not met:
+    ///   - Source address's segment must be negative (temporary).
+    ///   - Source address's offset must be zero.
+    ///   - There shouldn't already be relocation at the source segment.
+    pub fn add_relocation_rule(
+        &mut self,
+        src_ptr: Relocatable,
+        dst_ptr: Relocatable,
+    ) -> Result<(), MemoryError> {
+        self.memory.add_relocation_rule(src_ptr, dst_ptr)
+    }
 }
 
 #[cfg(test)]
