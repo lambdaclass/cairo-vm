d74987b676921daa091b696c869687141b3c6a4b
Juanma <juan.viel@lambdaclass.com>
2022-11-24 16:36:22 -0300

    Add felt and field types

--- src/types/felt.rs
+++ src/types/felt.rs
@@ -1,12 +1,11 @@
 use crate::{
     bigint,
-    types::field::{Field, PRIME_HIGH, PRIME_LOW}
+    types::field::{Field, PRIME_HIGH, PRIME_LOW},
 };
-use std::{marker::PhantomData, ops::Add};
+use lazy_static::lazy_static;
 use num_bigint::BigInt;
 use num_integer::Integer;
-use lazy_static::lazy_static;
-
+use std::{marker::PhantomData, ops::Add};
 
 pub type Felt = FeltBigInt<Field>;
 
@@ -16,18 +15,24 @@ lazy_static! {
 
 pub struct FeltBigInt<Field> {
     value: BigInt,
-    phantom: PhantomData<Field>
+    phantom: PhantomData<Field>,
 }
 
 impl<Field> FeltBigInt<Field> {
     pub fn new(value: BigInt) -> Self {
-        FeltBigInt { value , phantom: PhantomData}
+        FeltBigInt {
+            value,
+            phantom: PhantomData,
+        }
     }
 }
 
 impl<Field> Add for FeltBigInt<Field> {
-    type Output = Self; 
+    type Output = Self;
     fn add(self, rhs: Self) -> Self {
-        FeltBigInt {value : (self.value + rhs.value).mod_floor(&CAIRO_PRIME), phantom: PhantomData}
+        FeltBigInt {
+            value: (self.value + rhs.value).mod_floor(&CAIRO_PRIME),
+            phantom: PhantomData,
+        }
     }
 }
--- src/types/field.rs
+++ src/types/field.rs
@@ -11,7 +11,11 @@ impl<const HIGH: u128, const LOW: u128> FieldStruct<HIGH, LOW> {
     pub fn new(input_prime: &str) -> Result<Self, VirtualMachineError> {
         let assembled_prime = format!("{:x}{:032x}", PRIME_HIGH, PRIME_LOW);
         if assembled_prime != input_prime {
-            Err(VirtualMachineError::PrimeDiffers(input_prime.to_string(), PRIME_HIGH, PRIME_HIGH))
+            Err(VirtualMachineError::PrimeDiffers(
+                input_prime.to_string(),
+                PRIME_HIGH,
+                PRIME_HIGH,
+            ))
         } else {
             Ok(FieldStruct {})
         }
