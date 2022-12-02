0490e1423d17eba8103a20134ae5416c2d421397
Nabih <nabih@fuzzinglabs.com>
2022-12-02 14:45:12 +0100

    add deserialize program from string

--- src/types/program.rs
+++ src/types/program.rs
@@ -1,5 +1,5 @@
 use crate::serde::deserialize_program::{
-    deserialize_program, Attribute, HintParams, Identifier, ReferenceManager,
+    deserialize_program, deserialize_program_from_string, Attribute, HintParams, Identifier, ReferenceManager,
 };
 use crate::types::errors::program_errors::ProgramError;
 use crate::types::relocatable::MaybeRelocatable;
@@ -30,6 +30,10 @@ impl Program {
         let reader = BufReader::new(file);
 
         deserialize_program(reader, entrypoint)
+    }
+
+    pub fn from_string(json: &String, entrypoint: Option<&str>) -> Result<Program, ProgramError> {
+        deserialize_program_from_string(json, entrypoint)
     }
 
     pub fn from_reader(
@@ -65,6 +69,308 @@ mod tests {
     use crate::{bigint, bigint_str};
     use num_traits::FromPrimitive;
 
+    #[test]
+
+    fn deserialize_program_from_str_test() {
+        let json_artifact = r#"
+        {
+            "attributes": [
+                {
+                    "accessible_scopes": [
+                        "openzeppelin.security.safemath.library",
+                        "openzeppelin.security.safemath.library.SafeUint256",
+                        "openzeppelin.security.safemath.library.SafeUint256.add"
+                    ],
+                    "end_pc": 381,
+                    "flow_tracking_data": {
+                        "ap_tracking": {
+                            "group": 14,
+                            "offset": 35
+                        },
+                        "reference_ids": {}
+                    },
+                    "name": "error_message",
+                    "start_pc": 379,
+                    "value": "SafeUint256: addition overflow"
+                },
+                {
+                    "accessible_scopes": [
+                        "openzeppelin.security.safemath.library",
+                        "openzeppelin.security.safemath.library.SafeUint256",
+                        "openzeppelin.security.safemath.library.SafeUint256.sub_le"
+                    ],
+                    "end_pc": 404,
+                    "flow_tracking_data": {
+                        "ap_tracking": {
+                            "group": 15,
+                            "offset": 60
+                        },
+                        "reference_ids": {}
+                    },
+                    "name": "unknow",
+                    "start_pc": 402,
+                    "value": "unknow"
+                }
+            ],
+            "builtins": [],
+            "data": [
+                "0x480680017fff8000",
+                "0x3e8",
+                "0x480680017fff8000",
+                "0x7d0",
+                "0x48307fff7ffe8000",
+                "0x208b7fff7fff7ffe"
+            ],
+            "debug_info": {
+                "file_contents": {},
+                "instruction_locations": {
+                    "0": {
+                        "accessible_scopes": [
+                            "__main__",
+                            "__main__.main"
+                        ],
+                        "flow_tracking_data": {
+                            "ap_tracking": {
+                                "group": 0,
+                                "offset": 0
+                            },
+                            "reference_ids": {}
+                        },
+                        "hints": [],
+                        "inst": {
+                            "end_col": 22,
+                            "end_line": 2,
+                            "input_file": {
+                                "filename": "test.cairo"
+                            },
+                            "start_col": 5,
+                            "start_line": 2
+                        }
+                    },
+                    "2": {
+                        "accessible_scopes": [
+                            "__main__",
+                            "__main__.main"
+                        ],
+                        "flow_tracking_data": {
+                            "ap_tracking": {
+                                "group": 0,
+                                "offset": 1
+                            },
+                            "reference_ids": {}
+                        },
+                        "hints": [],
+                        "inst": {
+                            "end_col": 22,
+                            "end_line": 3,
+                            "input_file": {
+                                "filename": "test.cairo"
+                            },
+                            "start_col": 5,
+                            "start_line": 3
+                        }
+                    },
+                    "4": {
+                        "accessible_scopes": [
+                            "__main__",
+                            "__main__.main"
+                        ],
+                        "flow_tracking_data": {
+                            "ap_tracking": {
+                                "group": 0,
+                                "offset": 2
+                            },
+                            "reference_ids": {}
+                        },
+                        "hints": [],
+                        "inst": {
+                            "end_col": 37,
+                            "end_line": 4,
+                            "input_file": {
+                                "filename": "test.cairo"
+                            },
+                            "start_col": 5,
+                            "start_line": 4
+                        }
+                    },
+                    "5": {
+                        "accessible_scopes": [
+                            "__main__",
+                            "__main__.main"
+                        ],
+                        "flow_tracking_data": {
+                            "ap_tracking": {
+                                "group": 0,
+                                "offset": 3
+                            },
+                            "reference_ids": {}
+                        },
+                        "hints": [],
+                        "inst": {
+                            "end_col": 8,
+                            "end_line": 5,
+                            "input_file": {
+                                "filename": "test.cairo"
+                            },
+                            "start_col": 5,
+                            "start_line": 5
+                        }
+                    }
+                }
+            },
+            "hints": {
+                "0": [
+                    {
+                        "accessible_scopes": [
+                            "starkware.cairo.common.alloc",
+                            "starkware.cairo.common.alloc.alloc"
+                        ],
+                        "code": "memory[ap] = segments.add()",
+                        "flow_tracking_data": {
+                            "ap_tracking": {
+                                "group": 0,
+                                "offset": 0
+                            },
+                            "reference_ids": {}
+                        }
+                    }
+                ],
+                "46": [
+                    {
+                        "accessible_scopes": [
+                            "__main__",
+                            "__main__.main"
+                        ],
+                        "code": "import math",
+                        "flow_tracking_data": {
+                            "ap_tracking": {
+                                "group": 5,
+                                "offset": 0
+                            },
+                            "reference_ids": {}
+                        }
+                    }
+                ]
+            },
+            "identifiers": {
+                "__main__.main": {
+                    "decorators": [],
+                    "pc": 0,
+                    "type": "function"
+                },
+                "__main__.main.Args": {
+                    "full_name": "__main__.main.Args",
+                    "members": {},
+                    "size": 0,
+                    "type": "struct"
+                },
+                "__main__.main.ImplicitArgs": {
+                    "full_name": "__main__.main.ImplicitArgs",
+                    "members": {},
+                    "size": 0,
+                    "type": "struct"
+                },
+                "__main__.main.Return": {
+                    "full_name": "__main__.main.Return",
+                    "members": {},
+                    "size": 0,
+                    "type": "struct"
+                },
+                "__main__.main.SIZEOF_LOCALS": {
+                    "type": "const",
+                    "value": 0
+                }
+            },
+            "main_scope": "__main__",
+            "prime": "0x800000000000011000000000000000000000000000000000000000000000001",
+            "reference_manager": {
+                "references": []
+            }
+        }        
+        "#;
+        let program: Program = Program::from_string(
+            &json_artifact.to_string(),
+            Some("main"),
+        )
+        .expect("Failed to deserialize program");
+
+        let builtins: Vec<String> = Vec::new();
+        let data: Vec<MaybeRelocatable> = vec![
+            MaybeRelocatable::Int(BigInt::from_i64(5189976364521848832).unwrap()),
+            MaybeRelocatable::Int(BigInt::from_i64(1000).unwrap()),
+            MaybeRelocatable::Int(BigInt::from_i64(5189976364521848832).unwrap()),
+            MaybeRelocatable::Int(BigInt::from_i64(2000).unwrap()),
+            MaybeRelocatable::Int(BigInt::from_i64(5201798304953696256).unwrap()),
+            MaybeRelocatable::Int(BigInt::from_i64(2345108766317314046).unwrap()),
+        ];
+
+        let mut identifiers: HashMap<String, Identifier> = HashMap::new();
+
+        identifiers.insert(
+            String::from("__main__.main"),
+            Identifier {
+                pc: Some(0),
+                type_: Some(String::from("function")),
+                value: None,
+                full_name: None,
+                members: None,
+            },
+        );
+        identifiers.insert(
+            String::from("__main__.main.Args"),
+            Identifier {
+                pc: None,
+                type_: Some(String::from("struct")),
+                value: None,
+                full_name: Some("__main__.main.Args".to_string()),
+                members: Some(HashMap::new()),
+            },
+        );
+        identifiers.insert(
+            String::from("__main__.main.ImplicitArgs"),
+            Identifier {
+                pc: None,
+                type_: Some(String::from("struct")),
+                value: None,
+                full_name: Some("__main__.main.ImplicitArgs".to_string()),
+                members: Some(HashMap::new()),
+            },
+        );
+        identifiers.insert(
+            String::from("__main__.main.Return"),
+            Identifier {
+                pc: None,
+                type_: Some(String::from("struct")),
+                value: None,
+                full_name: Some("__main__.main.Return".to_string()),
+                members: Some(HashMap::new()),
+            },
+        );
+        identifiers.insert(
+            String::from("__main__.main.SIZEOF_LOCALS"),
+            Identifier {
+                pc: None,
+                type_: Some(String::from("const")),
+                value: Some(bigint!(0)),
+                full_name: None,
+                members: None,
+            },
+        );
+
+        assert_eq!(
+            program.prime,
+            BigInt::parse_bytes(
+                b"3618502788666131213697322783095070105623107215331596699973092056135872020481",
+                10
+            )
+            .unwrap()
+        );
+        assert_eq!(program.builtins, builtins);
+        assert_eq!(program.data, data);
+        assert_eq!(program.main, Some(0));
+        assert_eq!(program.identifiers, identifiers);
+    }
+    
     #[test]
     fn deserialize_program_test() {
         let program: Program = Program::from_file(
