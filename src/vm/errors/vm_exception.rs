6ab520d7cd6f1c7f25ac23ccf64b976b4c904416
Federica <federica@MacBook-Air-de-Federica.local>
2022-12-14 17:29:27 -0300

    Add VmException to CairoRunError enum

--- src/vm/errors/vm_exception.rs
+++ src/vm/errors/vm_exception.rs
@@ -1,19 +1,22 @@
+use thiserror::Error;
+
 use crate::{
     serde::deserialize_program::{Attribute, Location},
     vm::runners::cairo_runner::CairoRunner,
 };
 
-use super::runner_errors::RunnerError;
-
+use super::vm_errors::VirtualMachineError;
+#[derive(Debug, PartialEq, Error)]
+#[error("Error at pc={pc}.\n{inner_exc}")] //Temporary, should impelment Display manually
 pub struct VmException {
     pc: usize,
     inst_location: Option<Location>,
-    inner_exc: RunnerError,
+    inner_exc: VirtualMachineError,
     error_attr_value: Option<String>,
 }
 
 impl VmException {
-    fn from_runner_error(runner: CairoRunner, error: RunnerError, pc: usize) -> Self {
+    pub fn from_vm_error(runner: &CairoRunner, error: VirtualMachineError, pc: usize) -> Self {
         let error_attr_value = get_error_attr_value(pc, &runner.program.error_message_attributes);
         VmException {
             pc,
