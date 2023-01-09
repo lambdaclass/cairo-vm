d0828b99eaaef50b29717d695856cfb335e431a9
Mario J. Rugiero <mrugiero@gmail.com>
2023-01-09 18:07:16 -0300

    Silence valgrind, use only the script

--- scripts/stable_bench.sh
+++ scripts/stable_bench.sh
@@ -13,5 +13,5 @@ find $BENCHMARK_DIR -name '*.json' -execdir basename -s .json '{}' '+' | \
 	xargs -I '{input}' -P $(nproc) -n 1 \
 	valgrind --tool=cachegrind --cachegrind-out-file=$TARGET_DIR/'cachegrind.out.{input}'\
 	--D1=$((32*1024)),8,64 --I1=$((32*1024)),8,64 --LL=$((8*1024*1024)),16,64 \
-	./target/release/cairo-rs-run --layout all --memory_file /dev/null --trace_file /dev/null $BENCHMARK_DIR/'{input}'.json
+	./target/release/cairo-rs-run --layout all --memory_file /dev/null --trace_file /dev/null $BENCHMARK_DIR/'{input}'.json > /dev/null
 find $TARGET_DIR -name 'cachegrind.out.*' ! -name '*.old' -exec ${ROOT_DIR}/scripts/regression_check.py '{}' '{}'.old ';'
