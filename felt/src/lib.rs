db3203f8046a4430f403f0dd4e14151cc856abd3
Martina <martina@martina>
2023-01-09 19:03:01 -0300

    add property testing for left bit shift

--- felt/src/lib.rs
+++ felt/src/lib.rs
@@ -189,5 +189,14 @@ mod test {
             prop_assert!(as_uint < p, "{}", as_uint);
             prop_assert_eq!(&(q * y), x);
         }
+
+        #[test]
+        fn shift_left_in_range(ref value in "(0|[1-9][0-9]*)", ref shift_amount in "[1-9][0-9]*"){
+            let value = Felt::parse_bytes(value.as_bytes(), 10).unwrap();
+            let p = &BigUint::parse_bytes(PRIME_STR[2..].as_bytes(), 16).unwrap();
+            let shift_amount:u32 = shift_amount.parse::<u32>().unwrap();
+            let result = (value << shift_amount).to_biguint();
+            prop_assert!(&result < p);
+        }
     }
 }
