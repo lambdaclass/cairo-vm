110ca1a4e3fd62c4990aa94a2a66e64dd3ac4a15
Martina <martina@martina>
2023-01-12 14:25:23 -0300

    implemented change suggested by another dev to fix this

--- felt/src/bigint_felt.rs
+++ felt/src/bigint_felt.rs
@@ -415,8 +415,9 @@ impl Pow<u32> for FeltBigInt {
 
 impl<'a> Pow<u32> for &'a FeltBigInt {
     type Output = FeltBigInt;
+    let x = &self.0;
     fn pow(self, rhs: u32) -> Self::Output {
-        FeltBigInt((&self.0).pow(rhs).mod_floor(&CAIRO_PRIME))
+        FeltBigInt(x.pow(rhs).mod_floor(&CAIRO_PRIME))
     }
 }
 
