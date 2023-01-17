32ebe5f04f23650fb397a96717b0c23b6ccb0a7b
Juan-M-V <102986292+Juan-M-V@users.noreply.github.com>
2023-01-17 10:29:02 -0300

    verify signature hint (#702)
    * Add common signature test
    
    * Add hint to hint code
    
    * Add signature hint
    
    * Add signature test
    
    * Add test to signature-hint
    
    * Happy path test
    
    Co-authored-by: Juanma <juanma@Juanmas-MacBook-Air.local>

---
+++ cairo_programs/common_signature.cairo
@@ -0,0 +1,9 @@
+%builtins ecdsa
+from starkware.cairo.common.serialize import serialize_word
+from starkware.cairo.common.cairo_builtins import SignatureBuiltin
+from starkware.cairo.common.signature import verify_ecdsa_signature
+
+func main{ecdsa_ptr: SignatureBuiltin*}() {
+    verify_ecdsa_signature(2718, 1735102664668487605176656616876767369909409133946409161569774794110049207117, 3086480810278599376317923499561306189851900463386393948998357832163236918254, 598673427589502599949712887611119751108407514580626464031881322743364689811);
+    return();
+}
--- src/hint_processor/builtin_hint_processor/builtin_hint_processor_definition.rs
+++ src/hint_processor/builtin_hint_processor/builtin_hint_processor_definition.rs
@@ -37,6 +37,7 @@ use crate::{
             segments::{relocate_segment, temporary_array},
             set::set_add,
             sha256_utils::{sha256_finalize, sha256_input, sha256_main},
+            signature::verify_ecdsa_signature,
             squash_dict_utils::{
                 squash_dict, squash_dict_inner_assert_len_keys,
                 squash_dict_inner_check_access_index, squash_dict_inner_continue_loop,
@@ -432,6 +433,9 @@ impl HintProcessor for BuiltinHintProcessor {
             }
             hint_code::TEMPORARY_ARRAY => {
                 temporary_array(vm, &hint_data.ids_data, &hint_data.ap_tracking)
+            }
+            hint_code::VERIFY_ECDSA_SIGNATURE => {
+                verify_ecdsa_signature(vm, &hint_data.ids_data, &hint_data.ap_tracking)
             }
             #[cfg(feature = "skip_next_instruction_hint")]
             hint_code::SKIP_NEXT_INSTRUCTION => skip_next_instruction(vm),
