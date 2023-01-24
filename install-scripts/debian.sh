817d0cdd6590e40ff1ceb224d694cdc30a16f140
Klaus Lungwitz <18153834+klaus993@users.noreply.github.com>
2023-01-24 14:54:10 -0300

    Add apt update

--- install-scripts/debian.sh
+++ install-scripts/debian.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
 
+apt update -y
+
 apt install -y make
 
 lsb_release -a
--- install-scripts/ubuntu.sh
+++ install-scripts/ubuntu.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
 
+sudo apt update -y
+
 sudo apt install -y make
 
 lsb_release -a
