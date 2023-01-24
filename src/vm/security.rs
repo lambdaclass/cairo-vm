77c66049ec4e6f2a56c8efb690df179cf5b3fa97
Federica <fedemoletta@hotmail.com>
2023-01-24 13:35:35 -0300

    Remove previous implementation

--- src/vm/security.rs
+++ src/vm/security.rs
@@ -1,14 +1,12 @@
 use num_traits::ToPrimitive;
 
 use super::{
-    errors::{
-        memory_errors::MemoryError, runner_errors::RunnerError, vm_errors::VirtualMachineError,
-    },
+    errors::{runner_errors::RunnerError, vm_errors::VirtualMachineError},
     runners::cairo_runner::CairoRunner,
     vm_core::VirtualMachine,
 };
-use crate::types::relocatable::{MaybeRelocatable, Relocatable};
-use std::{collections::HashMap, mem::swap};
+use crate::types::relocatable::MaybeRelocatable;
+use std::collections::HashMap;
 
 /// Verify that the completed run in a runner is safe to be relocated and be
 /// used by other Cairo programs.
@@ -81,93 +79,12 @@ pub fn verify_secure_runner(
     Ok(())
 }
 
-/// Verify that the completed run in a runner is safe to be relocated and be
-/// used by other Cairo programs.
-///
-/// Checks include:
-///   - All accesses to the builtin segments must be within the range defined by
-///     the builtins themselves.
-///   - There mustn't be accesses to the program segment outside the program
-///     data range.
-///
-/// Note: Each builtin is responsible for checking its own segments' data.
-pub fn verify_secure_runner_2(
-    runner: &CairoRunner,
-    verify_builtins: bool,
-    vm: &mut VirtualMachine,
-) -> Result<(), VirtualMachineError> {
-    let program_base = runner
-        .program_base
-        .as_ref()
-        .ok_or(RunnerError::NoProgBase)?;
-
-    let builtin_segments = match verify_builtins {
-        true => runner.get_builtin_segments_info(vm)?,
-        false => HashMap::new(),
-    };
-
-    let builtin_segment_by_index = builtin_segments
-        .iter()
-        .map(|(seg_name, seg_info)| (seg_info.index, (seg_name, seg_info)))
-        .collect::<HashMap<_, _>>();
-
-    let memory_iter = vm
-        .memory
-        .data
-        .iter()
-        .enumerate()
-        .flat_map(|(idx, segment)| {
-            segment.iter().enumerate().filter_map(move |(off, value)| {
-                value
-                    .as_ref()
-                    .map(|val| (Relocatable::from((idx as _, off)), val))
-            })
-        });
-    for (addr, value) in memory_iter {
-        // Check builtin segment bounds.
-        if let Some((_, seg_info)) = builtin_segment_by_index.get(&addr.segment_index) {
-            if addr.offset >= seg_info.size {
-                return Err(RunnerError::FailedMemoryGet(MemoryError::NumOutOfBounds).into());
-            }
-        }
-
-        // Check program segment bounds.
-        if addr.segment_index == program_base.segment_index
-            && addr.offset >= runner.program.data.len()
-        {
-            return Err(RunnerError::FailedMemoryGet(MemoryError::NumOutOfBounds).into());
-        }
-
-        // Check value validity (when relocatable, that the segment exists and
-        // is not temporary).
-        if !vm
-            .segments
-            .is_valid_memory_value(value)
-            .map_err(RunnerError::FailedMemoryGet)?
-        {
-            return Err(
-                RunnerError::FailedMemoryGet(MemoryError::InvalidMemoryValue(addr, value.clone()))
-                    .into(),
-            );
-        }
-    }
-
-    // This swap is needed to avoid double mutable borrows.
-    let mut tmp = Vec::new();
-    swap(&mut tmp, &mut vm.builtin_runners);
-    for (_, builtin_runner) in &tmp {
-        builtin_runner.run_security_checks(vm)?;
-    }
-    swap(&mut tmp, &mut vm.builtin_runners);
-
-    Ok(())
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
     use crate::hint_processor::builtin_hint_processor::builtin_hint_processor_definition::BuiltinHintProcessor;
     use crate::types::relocatable::MaybeRelocatable;
+    use crate::types::relocatable::Relocatable;
     use crate::vm::vm_memory::memory::Memory;
     use crate::{relocatable, types::program::Program, utils::test_utils::*};
     use felt::Felt;
