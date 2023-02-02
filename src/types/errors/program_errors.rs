4227485229fa835e7e1f9d5d5bf18da78481447c
Florian Pellissier <111426680+flopell@users.noreply.github.com>
2023-02-02 12:08:20 +0200

    feat: Add no_std support for program_errors

--- src/types/errors/program_errors.rs
+++ src/types/errors/program_errors.rs
@@ -2,20 +2,49 @@ use std::prelude::v1::*;
 
 use felt::PRIME_STR;
 use std::io;
-use thiserror::Error;
 
-#[derive(Debug, Error)]
+#[derive(Debug)]
 pub enum ProgramError {
-    #[error(transparent)]
-    IO(#[from] io::Error),
-    #[error(transparent)]
-    Parse(#[from] serde_json::Error),
-    #[error("Entrypoint {0} not found")]
+    IO(io::Error),
+    Parse(serde_json::Error),
     EntrypointNotFound(String),
-    #[error("Constant {0} has no value")]
     ConstWithoutValue(String),
-    #[error("Expected prime {PRIME_STR}, got {0}")]
     PrimeDiffers(String),
+}
+
+impl std::fmt::Display for ProgramError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ProgramError::IO(e) => e.fmt(f),
+            ProgramError::Parse(e) => e.fmt(f),
+            ProgramError::EntrypointNotFound(v) => format!("Entrypoint {v} not found").fmt(f),
+            ProgramError::ConstWithoutValue(v) => format!("Constant {v} has no value").fmt(f),
+            ProgramError::PrimeDiffers(v) => format!("Expected prime {PRIME_STR}, got {v}").fmt(f),
+        }
+    }
+}
+
+impl From<io::Error> for ProgramError {
+    fn from(value: io::Error) -> Self {
+        Self::IO(value)
+    }
+}
+
+impl From<serde_json::Error> for ProgramError {
+    fn from(value: serde_json::Error) -> Self {
+        Self::Parse(value)
+    }
+}
+
+#[cfg(feature = "std")]
+impl std::error::Error for ProgramError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            ProgramError::IO(e) => Some(e),
+            ProgramError::Parse(e) => Some(e),
+            _ => None,
+        }
+    }
 }
 
 #[cfg(test)]
