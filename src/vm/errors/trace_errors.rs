65ba97f0140a3a0bc58b1ac6e38b21cc8fa511a1
Alex Sumner <alex.sumner@gmail.com>
2023-02-02 11:09:38 +0200

    implemented error trace_errors.rs

--- src/vm/errors/trace_errors.rs
+++ src/vm/errors/trace_errors.rs
@@ -1,16 +1,38 @@
 use crate::vm::errors::memory_errors::MemoryError;
-use thiserror::Error;
 
-#[derive(Debug, PartialEq, Eq, Error)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum TraceError {
-    #[error("Trace is not enabled for this run")]
     TraceNotEnabled,
-    #[error("Trace is already relocated")]
     AlreadyRelocated,
-    #[error("Trace register must be relocatable")]
     RegNotRelocatable,
-    #[error("No relocation found for this segment")]
     NoRelocationFound,
-    #[error(transparent)]
-    MemoryError(#[from] MemoryError),
+    MemoryError(MemoryError),
+}
+
+impl std::fmt::Display for TraceError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TraceError::TraceNotEnabled => "Trace is not enabled for this run".fmt(f),
+            TraceError::AlreadyRelocated => "Trace is already relocated".fmt(f),
+            TraceError::RegNotRelocatable => "Trace register must be relocatable".fmt(f),
+            TraceError::NoRelocationFound => "No relocation found for this segment".fmt(f),
+            TraceError::MemoryError(e) => e.fmt(f),
+        }
+    }
+}
+
+impl From<MemoryError> for TraceError {
+    fn from(value: MemoryError) -> Self {
+        Self::MemoryError(value)
+    }
+}
+
+#[cfg(feature = "std")]
+impl std::error::Error for TraceError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            TraceError::MemoryError(e) => Some(e),
+            _ => None,
+        }
+    }
 }
