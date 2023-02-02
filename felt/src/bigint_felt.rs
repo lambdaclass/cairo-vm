df567e1408dd058bec488494915283975827805c
Martina <martina@martina>
2023-02-02 20:33:20 -0300

    add unit test for bitwise And operation with zeros. Add proptest for bitwise And operation with large numbers

--- felt/src/bigint_felt.rs
+++ felt/src/bigint_felt.rs
@@ -834,7 +834,6 @@ mod tests {
 
     #[test]
     // Tests that the result of performing add asign with two zeros results in zero.
-
     fn add_assign_zeros() {
         let mut a = FeltBigInt::<FIELD_HIGH, FIELD_LOW>::new(0);
         let b = FeltBigInt::new(0);
@@ -842,6 +841,15 @@ mod tests {
         let c = FeltBigInt::new(0);
 
         assert_eq!(a, c);
+    }
+    #[test]
+    // Tests that the result of performing a bitwise "and" operation with two zeros results in zero.
+    fn bit_and_zeros() {
+        let a = FeltBigInt::<FIELD_HIGH, FIELD_LOW>::new(0);
+        let b = FeltBigInt::new(0);
+        let c = FeltBigInt::new(0);
+
+        assert_eq!(&a & &b, c);
     }
 
     #[test]
@@ -933,6 +941,17 @@ mod tests {
             x += y;
             let as_uint = &x.to_biguint();
             prop_assert!(as_uint < &p, "{}", as_uint);
+        }
+
+        #[test]
+        // Tests that the result of performing the bitwise "and" operation on two random large bigint felts falls within the range [0, p]. This test is performed 100 times each run.
+        fn bitand_bigint_felts_within_field(ref x in "([1-9][0-9]*)", ref y in "([1-9][0-9]*)") {
+            let x = FeltBigInt::<FIELD_HIGH, FIELD_LOW>::parse_bytes(x.as_bytes(), 10).unwrap();
+            let y = FeltBigInt::<FIELD_HIGH, FIELD_LOW>::parse_bytes(y.as_bytes(), 10).unwrap();
+            let p:BigUint = BigUint::parse_bytes(CAIRO_PRIME.to_string().as_bytes(), 16).unwrap();
+            let result = &x & &y;
+            let as_uint = result.to_biguint();
+            prop_assert!(as_uint < p, "{}", as_uint);
         }
     }
 }
