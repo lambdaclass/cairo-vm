d1d329ffa5f3b3c7f7a25fa4e44d092e656d6e5c
Aurelou <aurelien.isak@epita.fr>
2023-02-02 08:41:32 +0100

    feat: manual implem ExecScopeError

--- src/vm/errors/exec_scope_errors.rs
+++ src/vm/errors/exec_scope_errors.rs
@@ -1,9 +1,21 @@
-use thiserror::Error;
+use std::prelude::v1::*;
 
-#[derive(Eq, Hash, PartialEq, Debug, Error)]
+#[derive(Eq, Hash, PartialEq, Debug)]
 pub enum ExecScopeError {
-    #[error("Cannot exit main scope.")]
     ExitMainScopeError,
-    #[error("Every enter_scope() requires a corresponding exit_scope().")]
     NoScopeError,
 }
+
+impl std::fmt::Display for ExecScopeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ExecScopeError::ExitMainScopeError => "Cannot exit main scope.".fmt(f),
+            ExecScopeError::NoScopeError => {
+                "Every enter_scope() requires a corresponding exit_scope().".fmt(f)
+            }
+        }
+    }
+}
+
+#[cfg(feature = "std")]
+impl std::error::Error for ExecScopeError {}
