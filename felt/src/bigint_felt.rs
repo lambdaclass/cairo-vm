2f8f7f2bca4e44eb540bf8a0474f93e0a6d3c241
Jonathan LEI <me@xjonathan.dev>
2023-02-09 18:10:24 +0200

    Fix new clippy warnings in 1.67.0 (#825)

--- felt/src/bigint_felt.rs
+++ felt/src/bigint_felt.rs
@@ -813,7 +813,7 @@ impl<const PH: u128, const PL: u128> fmt::Debug for FeltBigInt<PH, PL> {
 
 impl fmt::Display for ParseFeltError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", ParseFeltError)
+        write!(f, "{ParseFeltError:?}")
     }
 }
 
