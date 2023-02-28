d540d37b5bdf4805af4e09f8ed0f91bf193cc71d
Federica <fedemoletta@hotmail.com>
2023-02-28 13:17:05 -0300

    Fix add

--- felt/src/u64_felt.rs
+++ felt/src/u64_felt.rs
@@ -229,7 +229,7 @@ impl Add<usize> for FeltU64 {
 impl Add<usize> for &FeltU64 {
     type Output = FeltU64;
     fn add(self, rhs: usize) -> Self::Output {
-        let mut sum = match rhs.to_u64() {
+        let mut sum = self.val + match rhs.to_u64() {
             Some(rhs) => rhs,
             None => rhs.mod_floor(&(OXFOI_PRIME as usize)) as u64,
         };
