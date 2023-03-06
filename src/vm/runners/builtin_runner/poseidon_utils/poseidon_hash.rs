feee06c391748f9b6b00854a134c1e7c01a5f764
Federica <fedemoletta@hotmail.com>
2023-03-06 18:51:05 -0300

    Add tests

--- src/vm/runners/builtin_runner/poseidon_utils/poseidon_hash.rs
+++ src/vm/runners/builtin_runner/poseidon_utils/poseidon_hash.rs
@@ -52,3 +52,67 @@ pub fn permute_comp(state: &mut [FieldElement; 3]) {
         idx += 3;
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_permute_a() {
+        let mut poseidon_state = [FieldElement::THREE, FieldElement::ZERO, FieldElement::TWO];
+        permute_comp(&mut poseidon_state);
+        assert_eq!(
+            poseidon_state,
+            [
+                FieldElement::from_hex_be(
+                    "0x268c44203f1c763bca21beb5aec78b9063cdcdd0fdf6b598bb8e1e8f2b6253f"
+                )
+                .unwrap(),
+                FieldElement::from_hex_be(
+                    "0x2b85c9f686f5d3036db55b2ca58a763a3065bc1bc8efbe0e70f3a7171f6cad3"
+                )
+                .unwrap(),
+                FieldElement::from_hex_be(
+                    "0x61df3789eef0e1ee0dbe010582a00dd099191e6395dfb976e7be3be2fa9d54b"
+                )
+                .unwrap()
+            ]
+        )
+    }
+
+    #[test]
+    fn test_permute_b() {
+        let mut poseidon_state = [
+            FieldElement::from_hex_be(
+                "0x268c44203f1c763bca21beb5aec78b9063cdcdd0fdf6b598bb8e1e8f2b6253f",
+            )
+            .unwrap(),
+            FieldElement::from_hex_be(
+                "0x2b85c9f686f5d3036db55b2ca58a763a3065bc1bc8efbe0e70f3a7171f6cad3",
+            )
+            .unwrap(),
+            FieldElement::from_hex_be(
+                "0x61df3789eef0e1ee0dbe010582a00dd099191e6395dfb976e7be3be2fa9d54b",
+            )
+            .unwrap(),
+        ];
+        permute_comp(&mut poseidon_state);
+        assert_eq!(
+            poseidon_state,
+            [
+                FieldElement::from_hex_be(
+                    "0x4ec565b1b01606b5222602b20f8ddc4a8a7c75b559b852ab183a0daf5930b5c"
+                )
+                .unwrap(),
+                FieldElement::from_hex_be(
+                    "0x4d3c32c3c7cd39b6444db42e2437eeda12e459d28ce49a0f761a23d64c29e4c"
+                )
+                .unwrap(),
+                FieldElement::from_hex_be(
+                    "0x749d4d0ddf41548e039f183b745a08b80fad54e9ac389021148350bdda70a92"
+                )
+                .unwrap()
+            ]
+        )
+    }
+}
