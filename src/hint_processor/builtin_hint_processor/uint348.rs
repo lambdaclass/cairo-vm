c409d015c6e504ef2247ed83c7eb8c1d1880c419
Federica <fedemoletta@hotmail.com>
2023-04-12 11:11:44 -0300

    Add pack & split for uint348

--- src/hint_processor/builtin_hint_processor/uint348.rs
+++ src/hint_processor/builtin_hint_processor/uint348.rs
@@ -1,9 +1,36 @@
+use core::ops::Shl;
+
+use felt::Felt252;
+use num_bigint::BigUint;
+use num_traits::One;
+
 use crate::stdlib::{collections::HashMap, prelude::*};
 use crate::{
     hint_processor::hint_processor_definition::HintReference,
     serde::deserialize_program::ApTracking,
     vm::{errors::hint_errors::HintError, vm_core::VirtualMachine},
 };
+
+use super::secp::bigint_utils::BigInt3;
+
+fn split<const T: usize>(num: &Felt252, num_bits_shift: u32) -> [Felt252; T] {
+    let mut num = num.clone();
+    [0; T].map(|_| {
+        let a = &num & &((Felt252::one() << num_bits_shift) - 1_u32);
+        num = &num >> num_bits_shift;
+        a
+    })
+}
+
+fn pack(num: BigInt3, num_bits_shift: usize) -> BigUint {
+    let limbs = vec![num.d0, num.d1, num.d2];
+    #[allow(deprecated)]
+    limbs
+        .into_iter()
+        .enumerate()
+        .map(|(idx, value)| value.to_biguint().shl(idx * num_bits_shift))
+        .sum()
+}
 
 // Notes: Hints in this lib use the type Uint348, which is equal to common lib's BigInt3
 /* Implements Hint:
