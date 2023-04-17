376cd3d915ff7be3dc9c5ad6ef2a0720e4265dc9
Federica <fedemoletta@hotmail.com>
2023-04-17 13:24:49 -0300

    Add unit test

--- src/hint_processor/builtin_hint_processor/field_arithmetic.rs
+++ src/hint_processor/builtin_hint_processor/field_arithmetic.rs
@@ -73,7 +73,7 @@ pub fn get_square_root(
     let x = pack(BigInt3::from_var_name("x", vm, ids_data, ap_tracking)?, 128);
     let p = pack(BigInt3::from_var_name("p", vm, ids_data, ap_tracking)?, 128);
     let success_x = is_quad_residue(&x, &p)?;
-    //TODO use sqrt algorithm from sympy
+
     let root_x = if success_x {
         sqrt_prime_power(&x, &p).unwrap_or_default()
     } else {
@@ -82,7 +82,7 @@ pub fn get_square_root(
 
     let gx = generator * &x;
     let success_gx = is_quad_residue(&gx, &p)?;
-    //TODO use sqrt algorithm from sympy
+
     let root_gx = if success_gx {
         sqrt_prime_power(&gx, &p).unwrap_or_default()
     } else {
@@ -113,4 +113,77 @@ pub fn get_square_root(
     }
 
     Ok(())
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::hint_processor::builtin_hint_processor::hint_code;
+    use crate::vm::vm_memory::memory_segments::MemorySegmentManager;
+    use crate::{
+        any_box,
+        hint_processor::{
+            builtin_hint_processor::builtin_hint_processor_definition::{
+                BuiltinHintProcessor, HintProcessorData,
+            },
+            hint_processor_definition::HintProcessor,
+        },
+        types::{exec_scope::ExecutionScopes, relocatable::MaybeRelocatable},
+        utils::test_utils::*,
+        vm::{
+            errors::memory_errors::MemoryError, runners::builtin_runner::RangeCheckBuiltinRunner,
+            vm_core::VirtualMachine, vm_memory::memory::Memory,
+        },
+    };
+    use assert_matches::assert_matches;
+
+    #[cfg(target_arch = "wasm32")]
+    use wasm_bindgen_test::*;
+
+    #[test]
+    #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+    fn run_get_square_ok_goldilocks_prime() {
+        let mut vm = vm_with_range_check!();
+        //Initialize fp
+        vm.run_context.fp = 14;
+        //Create hint_data
+        let ids_data = non_continuous_ids_data![
+            ("p", -14),
+            ("x", -11),
+            ("generator", -8),
+            ("sqrt_x", -5),
+            ("sqrt_gx", -2),
+            ("success_x", 1)
+        ];
+        //Insert ids into memory
+        vm.segments = segments![
+            //p
+            ((1, 0), 18446744069414584321),
+            ((1, 1), 0),
+            ((1, 2), 0),
+            //x
+            ((1, 3), 25),
+            ((1, 4), 0),
+            ((1, 5), 0),
+            //generator
+            ((1, 6), 7),
+            ((1, 7), 0),
+            ((1, 8), 0)
+        ];
+        //Execute the hint
+        assert_matches!(run_hint!(vm, ids_data, hint_code::GET_SQUARE_ROOT), Ok(()));
+        //Check hint memory inserts
+        check_memory![
+            vm.segments.memory,
+            // sqrt_x
+            ((1, 9), 5),
+            ((1, 10), 0),
+            ((1, 11), 0),
+            // sqrt_gx
+            ((1, 12), 0),
+            ((1, 13), 0),
+            ((1, 14), 0),
+            // success_x
+            ((1, 15), 1)
+        ];
+    }
 }
