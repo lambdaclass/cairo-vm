7675a93e89096ccc5dc82c8b66ad6863b5fd66d0
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2023-04-18 15:36:03 -0300

    Add a unit test for bit lenght hint

--- src/hint_processor/builtin_hint_processor/garaga.rs
+++ src/hint_processor/builtin_hint_processor/garaga.rs
@@ -23,3 +23,45 @@ pub fn get_felt_bitlenght(
     let bit_length = x.bits() as usize;
     insert_value_from_var_name("bit_length", bit_length, vm, ids_data, ap_tracking)
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::any_box;
+    use crate::hint_processor::builtin_hint_processor::builtin_hint_processor_definition::BuiltinHintProcessor;
+    use crate::hint_processor::builtin_hint_processor::builtin_hint_processor_definition::HintProcessorData;
+    use crate::hint_processor::hint_processor_definition::HintProcessor;
+    use crate::hint_processor::hint_processor_utils::felt_to_u32;
+    use crate::types::exec_scope::ExecutionScopes;
+    use crate::types::relocatable::MaybeRelocatable;
+    use crate::vm::errors::memory_errors::MemoryError;
+    use crate::vm::vm_memory::memory::Memory;
+    use crate::vm::vm_memory::memory_segments::MemorySegmentManager;
+    use crate::{hint_processor::builtin_hint_processor::hint_code, utils::test_utils::*};
+    use assert_matches::assert_matches;
+
+    use super::*;
+
+    #[test]
+    #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+    fn test_7_bit_length_is_3() {
+        let ids_data = non_continuous_ids_data![
+            ("x", 0),          // located at `fp + 0`.
+            ("bit_length", 1)  // located at `fp + 1`.
+        ];
+
+        let mut vm = vm!();
+        vm.run_context.fp = 0;
+        vm.segments = segments![
+            ((1, 0), 7) // Inits `x` to `7`.
+                        // Don't initialize `fp + 1` for `bit_length`!
+        ];
+
+        assert_matches!(
+            run_hint!(vm, ids_data, hint_code::GET_FELT_BITLENGTH),
+            Ok(())
+        );
+
+        let bit_length = felt_to_u32(&vm.get_integer((1, 1).into()).unwrap()).unwrap();
+        assert_eq!(bit_length, 3);
+    }
+}
