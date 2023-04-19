a19958960e65f8098ff3592253ffd8610ee469a0
Tomás <47506558+MegaRedHand@users.noreply.github.com>
2023-04-19 15:44:01 -0300

    feat(hints): add NewHint#22 (#1003)
    * Add NewHint#22
    
    * Update changelog
    
    * Remove unused import
    
    * Use constant and add NOTE comment

--- src/tests/cairo_run_test.rs
+++ src/tests/cairo_run_test.rs
@@ -1343,3 +1343,10 @@ fn cairo_run_is_zero() {
     let program_data = include_bytes!("../../cairo_programs/is_zero.json");
     run_program_simple(program_data.as_slice());
 }
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn cairo_run_is_zero_pack() {
+    let program_data = include_bytes!("../../cairo_programs/is_zero_pack.json");
+    run_program_simple(program_data.as_slice());
+}
