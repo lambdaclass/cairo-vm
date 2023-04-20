a0793e2cdbbd3e511a17d335a8f2d31fc2402ca5
Pedro Fontana <fontana.pedro93@gmail.com>
2023-04-20 16:36:56 -0300

    Add unit test

--- src/hint_processor/builtin_hint_processor/vrf/inv_mod_p_uint512.rs
+++ src/hint_processor/builtin_hint_processor/vrf/inv_mod_p_uint512.rs
@@ -90,9 +90,28 @@ pub fn inv_mod_p_uint512(
 
 #[cfg(test)]
 mod tests {
-    use num_traits::FromPrimitive;
-
     use super::*;
+    use crate::any_box;
+    use crate::hint_processor::builtin_hint_processor::builtin_hint_processor_definition::BuiltinHintProcessor;
+    use crate::hint_processor::builtin_hint_processor::builtin_hint_processor_definition::HintProcessorData;
+    use crate::hint_processor::hint_processor_definition::HintProcessor;
+    use crate::types::relocatable::{MaybeRelocatable, Relocatable};
+    use crate::utils::test_utils::mayberelocatable;
+    use crate::utils::test_utils::memory;
+    use crate::utils::test_utils::memory_from_memory;
+    use crate::utils::test_utils::memory_inner;
+    use crate::vm::errors::memory_errors::MemoryError;
+    use crate::vm::runners::builtin_runner::RangeCheckBuiltinRunner;
+    use crate::vm::vm_memory::memory::Memory;
+    use crate::vm::vm_memory::memory_segments::MemorySegmentManager;
+    use crate::{
+        hint_processor::builtin_hint_processor::hint_code::INV_MOD_P_UINT512,
+        types::exec_scope::ExecutionScopes,
+        utils::test_utils::{
+            add_segments, non_continuous_ids_data, run_hint, segments, vm_with_range_check,
+        },
+    };
+    use num_traits::FromPrimitive;
 
     #[test]
     fn test_pack_512() {
@@ -136,6 +155,55 @@ mod tests {
                 761
             ),
             BigUint::from_str_radix("80853029148137605102740201774483901385926652025450340798711030404174727480763870493377667725625759764292622444803788021444434452626041518098606806141685367065099387655302625873713592439838446220691925786159227082298892378981461987274693629088875674987359669209043388107114325450518636532594445145924759095125734364345163525655691027843325303271775064263282011908012871334532482494107608759994020937000541268185418760956243245766874157401648637158526410360988956699864519559367805347900540475245570833510432301935056255005826223734865268553682118180231081037207280009003811438596531432027766301678781550463988061852846171462460595592799020846810683500364584025173048032553173114469560143047387885550", 10).unwrap()
+        );
+    }
+
+    #[test]
+    #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+    fn run_inv_mod_p_uint512_ok() {
+        let mut vm = vm_with_range_check!();
+        add_segments!(vm, 3);
+
+        //Initialize fp
+        vm.run_context.fp = 25;
+
+        //Create hint data
+        let ids_data = non_continuous_ids_data![("x", -5), ("p", -10), ("x_inverse_mod_p", -20)];
+        vm.segments = segments![
+            ((1, 20), 101), //ids.x.d0
+            ((1, 21), 2),   // ids.x.d1
+            ((1, 22), 15),  // ids.x.d2
+            ((1, 23), 61)   // ids.x.d3
+                            // ((1, 15), 201385395114098847380338600778089168199),   // ids.p.low
+                            // ((1, 16), 64323764613183177041862057485226039389)   // ids.p.high
+        ];
+        vm.insert_value(
+            Relocatable::from((1, 15)),
+            Felt252::from_str_radix("201385395114098847380338600778089168199", 10).unwrap(),
+        )
+        .expect("error setting ids.p");
+        vm.insert_value(
+            Relocatable::from((1, 16)),
+            Felt252::from_str_radix("64323764613183177041862057485226039389", 10).unwrap(),
+        )
+        .expect("error setting ids.p");
+
+        let mut exec_scopes = ExecutionScopes::new();
+        //Execute the hint
+        assert!(run_hint!(vm, ids_data, INV_MOD_P_UINT512, &mut exec_scopes).is_ok());
+
+        // Check VM inserts
+        assert_eq!(
+            vm.get_integer(Relocatable::from((1, 5)))
+                .unwrap()
+                .into_owned(),
+            Felt252::from_str_radix("80275402838848031859800366538378848249", 10).unwrap()
+        );
+        assert_eq!(
+            vm.get_integer(Relocatable::from((1, 6)))
+                .unwrap()
+                .into_owned(),
+            Felt252::from_str_radix("5810892639608724280512701676461676039", 10).unwrap()
         );
     }
 }
