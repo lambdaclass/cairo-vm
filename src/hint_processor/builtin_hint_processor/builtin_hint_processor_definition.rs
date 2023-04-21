b4340525744e74179cf97f177bc6bf964ca0fcda
Federica <fedemoletta@hotmail.com>
2023-04-21 17:03:26 -0300

    Add hint code + hint impl + unit test

--- src/hint_processor/builtin_hint_processor/builtin_hint_processor_definition.rs
+++ src/hint_processor/builtin_hint_processor/builtin_hint_processor_definition.rs
@@ -83,7 +83,7 @@ use felt::Felt252;
 #[cfg(feature = "skip_next_instruction_hint")]
 use crate::hint_processor::builtin_hint_processor::skip_next_instruction::skip_next_instruction;
 
-use super::ec_recover::ec_recover_divmod_n_packed;
+use super::ec_recover::{ec_recover_divmod_n_packed, ec_recover_sub_a_b};
 
 pub struct HintProcessorData {
     pub code: String,
@@ -566,6 +566,9 @@ impl HintProcessor for BuiltinHintProcessor {
                 &hint_data.ids_data,
                 &hint_data.ap_tracking,
             ),
+            hint_code::EC_RECOVER_SUB_A_B => {
+                ec_recover_sub_a_b(vm, exec_scopes, &hint_data.ids_data, &hint_data.ap_tracking)
+            }
             #[cfg(feature = "skip_next_instruction_hint")]
             hint_code::SKIP_NEXT_INSTRUCTION => skip_next_instruction(vm),
             code => Err(HintError::UnknownHint(code.to_string())),
--- src/hint_processor/builtin_hint_processor/hint_code.rs
+++ src/hint_processor/builtin_hint_processor/hint_code.rs
@@ -921,5 +921,11 @@ pub const EC_RECOVER_DIV_MOD_N_PACKED: &str = r#"from starkware.cairo.common.cai
     x = pack(ids.x, PRIME) % N
     s = pack(ids.s, PRIME) % N
     value = res = div_mod(x, s, N)"#;
+pub const EC_RECOVER_SUB_A_B: &str = r#"from starkware.cairo.common.cairo_secp.secp_utils import pack
+from starkware.python.math_utils import div_mod, safe_div
+
+a = pack(ids.x, PRIME)
+b = pack(ids.s, PRIME)
+value = res = a - b"#;
 #[cfg(feature = "skip_next_instruction_hint")]
 pub const SKIP_NEXT_INSTRUCTION: &str = "skip_next_instruction()";
