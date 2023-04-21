7a1bea7ed66eaf5181249518d1719115649d92b4
Tomás <tomas.gruner@lambdaclass.com>
2023-04-21 15:27:17 -0300

    Change bitmask to `u128::MAX`

--- src/hint_processor/builtin_hint_processor/uint_utils.rs
+++ src/hint_processor/builtin_hint_processor/uint_utils.rs
@@ -1,10 +1,9 @@
 use felt::Felt252;
 use num_bigint::BigUint;
-use num_traits::One;
 
 pub(crate) fn split<const T: usize>(num: &BigUint, num_bits_shift: u32) -> [BigUint; T] {
     let mut num = num.clone();
-    let bitmask = &((BigUint::one() << num_bits_shift) - 1_u32);
+    let bitmask = &BigUint::from(u128::MAX);
     [0; T].map(|_| {
         let a = &num & bitmask;
         num >>= num_bits_shift;
