fb731257da6fc2842abff103fccab964ae87bb38
fmoletta <99273364+fmoletta@users.noreply.github.com>
2023-04-24 23:02:57 +0300

    feat(hints): Implement NewHint#38 (#1036)
    * Add test
    
    * Ignore builtin segments in get_memory_holes
    
    * Improve test
    
    * Add memory holes check for cairo_run_test tests
    
    * Fix broken condition
    
    * Make memory_holes check optional & customizable
    
    * Fix bounds
    
    * Add a test with deliberately created memory holes
    
    * Fix test value
    
    * Remove duplicated tests + add memory hole value to some tests
    
    * Add memory holes value + remove duplicated tests + fix typo
    
    * Fix test values
    
    * Add changelog entry
    
    * Link PR in Changelog
    
    * Mark breaking change in changelog
    
    * fmt
    
    * Fix test value
    
    * Fix codecov-patch diff
    
    * Add hint code
    
    * Add integration test
    
    * Add untracked file
    
    * Add changelog entry
    
    * Rename hint
    
    * Fix test
    
    * Fix test
    
    * Add hint code + hint impl + unit test
    
    * fmt
    
    * Add integration test
    
    * Add changelog entry
    
    * Remove indent from hint string
    
    * Add wasm import

--- src/hint_processor/builtin_hint_processor/hint_code.rs
+++ src/hint_processor/builtin_hint_processor/hint_code.rs
@@ -987,5 +987,13 @@ N = pack(ids.n, PRIME)
 x = pack(ids.x, PRIME) % N
 s = pack(ids.s, PRIME) % N
 value = res = div_mod(x, s, N)"#;
+
+pub const EC_RECOVER_SUB_A_B: &str = r#"from starkware.cairo.common.cairo_secp.secp_utils import pack
+from starkware.python.math_utils import div_mod, safe_div
+
+a = pack(ids.a, PRIME)
+b = pack(ids.b, PRIME)
+value = res = a - b"#;
+
 #[cfg(feature = "skip_next_instruction_hint")]
 pub const SKIP_NEXT_INSTRUCTION: &str = "skip_next_instruction()";
