327ee7d6778bdab9573573e6af008fcc6041f9ad
fmoletta <99273364+fmoletta@users.noreply.github.com>
2023-04-26 19:59:59 +0300

    feat(hints): NewHint#60 (#1052)
    * Add hint code
    
    * Add cairo_programs/compute_slope_v2.cairo
    
    * add hints code
    
    * implement compute_slope_v2
    
    * Implement VERIFY_ZERO_V3
    
    * Add verify_zero_v3 unit tests
    
    * Add COMPUTE_SLOPE_V2 unit test
    
    * Update CHANGELOG.md
    
    * cargo clippy
    
    * Implement hint
    
    * fmt
    
    * Add changelog entry
    
    * Add integration test
    
    * cargo clippy
    
    * Update cairo_run_test.rs
    
    * Fix changelog
    
    * Remove duplicated test
    
    * fixes
    
    * Fix fast_ec_add_assign_new_y
    
    * Fix tests
    
    * Clippy
    
    ---------
    
    Co-authored-by: Pedro Fontana <fontana.pedro93@gmail.com>
    Co-authored-by: Mario Rugiero <mario.rugiero@lambdaclass.com>

--- src/tests/cairo_run_test.rs
+++ src/tests/cairo_run_test.rs
@@ -886,6 +886,13 @@ fn cairo_run_compute_doubling_slope_v2_test() {
 
 #[test]
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn fast_ec_add_v2() {
+    let program_data = include_bytes!("../../cairo_programs/fast_ec_add_v2.json");
+    run_program_simple(program_data.as_slice());
+}
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
 fn ec_double_assign_new_x_v3() {
     let program_data = include_bytes!("../../cairo_programs/ec_double_assign_new_x_v3.json");
     run_program_simple(program_data.as_slice());
