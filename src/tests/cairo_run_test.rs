4b7fd8ef2be85d7aa1151fc60cc561a78ae4e588
Mario Rugiero <mario.rugiero@lambdaclass.com>
2023-04-26 05:52:36 -0300

    Typo

--- src/tests/cairo_run_test.rs
+++ src/tests/cairo_run_test.rs
@@ -887,6 +887,6 @@ fn cairo_run_compute_doubling_slope_v2_test() {
 #[test]
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
 fn ec_double_assing_new_x_v3() {
-    let program_data = include_bytes!("../../cairo_programs/ec_double_assing_new_x_v3.json");
+    let program_data = include_bytes!("../../cairo_programs/ec_double_assign_new_x_v3.json");
     run_program_simple(program_data.as_slice());
 }
