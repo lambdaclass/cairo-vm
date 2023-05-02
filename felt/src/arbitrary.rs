d39d10ed4a1d95872cc812470d3baba2782ff658
Tomás <tomas.gruner@lambdaclass.com>
2023-05-02 10:58:23 -0300

    Impl `Arbitrary` for `FeltBigInt` and `Felt252`

--- felt/src/arbitrary.rs
+++ felt/src/arbitrary.rs
@@ -7,17 +7,16 @@ use crate::{
     Felt252,
 };
 
-pub(crate) fn any_felt_big_int<const PH: u128, const PL: u128>(
-) -> impl Strategy<Value = FeltBigInt<PH, PL>> {
+fn any_felt_big_int<const PH: u128, const PL: u128>() -> impl Strategy<Value = FeltBigInt<PH, PL>> {
     (0..=PH)
         // turn range into `impl Strategy`
         .prop_map(|x| x)
         // choose second 128-bit limb capped by first one
         .prop_flat_map(|high| {
             let low = if high == PH {
-                Strategy::sboxed((0..PL).prop_map(|x| x))
+                (0..PL).prop_map(|x| x).sboxed()
             } else {
-                Strategy::sboxed(any::<u128>())
+                any::<u128>().sboxed()
             };
             (Just(high), low)
         })
@@ -29,11 +28,31 @@ pub(crate) fn any_felt_big_int<const PH: u128, const PL: u128>(
 }
 
 /// Returns a [`Strategy`] that generates any valid Felt252
-pub fn any_felt252() -> impl Strategy<Value = Felt252> {
+fn any_felt252() -> impl Strategy<Value = Felt252> {
     any_felt_big_int::<FIELD_HIGH, FIELD_LOW>().prop_map(|value| Felt252 { value })
 }
 
 /// Returns a [`Strategy`] that generates any nonzero Felt252
 pub fn nonzero_felt252() -> impl Strategy<Value = Felt252> {
     any_felt252().prop_filter("is zero", |x| !x.is_zero())
+}
+
+impl<const PH: u128, const PL: u128> Arbitrary for FeltBigInt<PH, PL> {
+    type Parameters = ();
+
+    fn arbitrary_with(_args: Self::Parameters) -> Self::Strategy {
+        any_felt_big_int().sboxed()
+    }
+
+    type Strategy = SBoxedStrategy<Self>;
+}
+
+impl Arbitrary for Felt252 {
+    type Parameters = ();
+
+    fn arbitrary_with(_args: Self::Parameters) -> Self::Strategy {
+        any_felt252().sboxed()
+    }
+
+    type Strategy = SBoxedStrategy<Self>;
 }
