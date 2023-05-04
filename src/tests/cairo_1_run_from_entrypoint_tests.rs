7ab23aefd779e3c13bf480a987d85b064f306c58
Roberto Catalan <rcatalan@itba.edu.ar>
2023-05-04 16:01:44 -0300

    Add square root test for u8, u16 and u32

--- src/tests/cairo_1_run_from_entrypoint_tests.rs
+++ src/tests/cairo_1_run_from_entrypoint_tests.rs
@@ -23,3 +23,147 @@ fn fibonacci_3() {
         &vec![9_usize.into()],
     );
 }
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn u8_sqrt_9() {
+    let program_data = include_bytes!("../../cairo_programs/cairo-1-contracts/u8_sqrt.casm");
+    run_cairo_1_entrypoint(
+        program_data.as_slice(),
+        0,
+        &vec![9_usize.into()],
+        &vec![3_usize.into()],
+    );
+}
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn u8_sqrt_10() {
+    let program_data = include_bytes!("../../cairo_programs/cairo-1-contracts/u8_sqrt.casm");
+    run_cairo_1_entrypoint(
+        program_data.as_slice(),
+        0,
+        &vec![10_usize.into()],
+        &vec![3_usize.into()],
+    );
+}
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn u8_sqrt_one() {
+    let program_data = include_bytes!("../../cairo_programs/cairo-1-contracts/u8_sqrt.casm");
+    run_cairo_1_entrypoint(
+        program_data.as_slice(),
+        0,
+        &vec![1_usize.into()],
+        &vec![1_usize.into()],
+    );
+}
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn u8_sqrt_zero() {
+    let program_data = include_bytes!("../../cairo_programs/cairo-1-contracts/u8_sqrt.casm");
+    run_cairo_1_entrypoint(
+        program_data.as_slice(),
+        0,
+        &vec![0_usize.into()],
+        &vec![0_usize.into()],
+    );
+}
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn u16_sqrt_9() {
+    let program_data = include_bytes!("../../cairo_programs/cairo-1-contracts/u16_sqrt.casm");
+    run_cairo_1_entrypoint(
+        program_data.as_slice(),
+        0,
+        &vec![9_usize.into()],
+        &vec![3_usize.into()],
+    );
+}
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn u16_sqrt_10() {
+    let program_data = include_bytes!("../../cairo_programs/cairo-1-contracts/u16_sqrt.casm");
+    run_cairo_1_entrypoint(
+        program_data.as_slice(),
+        0,
+        &vec![10_usize.into()],
+        &vec![3_usize.into()],
+    );
+}
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn u16_sqrt_one() {
+    let program_data = include_bytes!("../../cairo_programs/cairo-1-contracts/u16_sqrt.casm");
+    run_cairo_1_entrypoint(
+        program_data.as_slice(),
+        0,
+        &vec![1_usize.into()],
+        &vec![1_usize.into()],
+    );
+}
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn u16_sqrt_zero() {
+    let program_data = include_bytes!("../../cairo_programs/cairo-1-contracts/u16_sqrt.casm");
+    run_cairo_1_entrypoint(
+        program_data.as_slice(),
+        0,
+        &vec![0_usize.into()],
+        &vec![0_usize.into()],
+    );
+}
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn u32_sqrt_9() {
+    let program_data = include_bytes!("../../cairo_programs/cairo-1-contracts/u32_sqrt.casm");
+    run_cairo_1_entrypoint(
+        program_data.as_slice(),
+        0,
+        &vec![9_usize.into()],
+        &vec![3_usize.into()],
+    );
+}
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn u32_sqrt_10() {
+    let program_data = include_bytes!("../../cairo_programs/cairo-1-contracts/u32_sqrt.casm");
+    run_cairo_1_entrypoint(
+        program_data.as_slice(),
+        0,
+        &vec![10_usize.into()],
+        &vec![3_usize.into()],
+    );
+}
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn u32_sqrt_one() {
+    let program_data = include_bytes!("../../cairo_programs/cairo-1-contracts/u32_sqrt.casm");
+    run_cairo_1_entrypoint(
+        program_data.as_slice(),
+        0,
+        &vec![1_usize.into()],
+        &vec![1_usize.into()],
+    );
+}
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn u32_sqrt_zero() {
+    let program_data = include_bytes!("../../cairo_programs/cairo-1-contracts/u32_sqrt.casm");
+    run_cairo_1_entrypoint(
+        program_data.as_slice(),
+        0,
+        &vec![0_usize.into()],
+        &vec![0_usize.into()],
+    );
+}
