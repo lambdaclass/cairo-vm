c4fbe31034b2e5136f4a7784a8f3e291e33e3693
Federica <fedemoletta@hotmail.com>
2023-05-04 11:35:30 -0300

    Add another fibonacci case

--- src/tests/cairo_1_run_from_entrypoint_tests.rs
+++ src/tests/cairo_1_run_from_entrypoint_tests.rs
@@ -12,3 +12,16 @@ fn fibonacci_1() {
         true,
     );
 }
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn fibonacci_3() {
+    let program_data = include_bytes!("../../cairo_programs/cairo-1-contracts/fib.casm");
+    run_cairo_1_entrypoint(
+        program_data.as_slice(),
+        0,
+        &vec![3_usize.into(), 3_usize.into(), 3_usize.into()],
+        &vec![9_usize.into()],
+        true,
+    );
+}
