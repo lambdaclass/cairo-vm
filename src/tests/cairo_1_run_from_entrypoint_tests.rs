09c3a13a01a369e336ea9a157c17de8388ab6db9
Roberto Catalan <rcatalan@itba.edu.ar>
2023-05-05 12:34:18 -0300

    Add u256 sqrt test

--- src/tests/cairo_1_run_from_entrypoint_tests.rs
+++ src/tests/cairo_1_run_from_entrypoint_tests.rs
@@ -263,3 +263,75 @@ fn u128_sqrt_zero() {
         &vec![0_usize.into()],
     );
 }
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn u256_sqrt_9() {
+    let program_data = include_bytes!("../../cairo_programs/cairo-1-contracts/u256_sqrt.casm");
+    run_cairo_1_entrypoint(
+        program_data.as_slice(),
+        0,
+        &vec![9_usize.into()],
+        &vec![3_usize.into()],
+    );
+}
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn u256_sqrt_10() {
+    let program_data = include_bytes!("../../cairo_programs/cairo-1-contracts/u256_sqrt.casm");
+    run_cairo_1_entrypoint(
+        program_data.as_slice(),
+        0,
+        &vec![10_usize.into()],
+        &vec![3_usize.into()],
+    );
+}
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn u256_sqrt_one() {
+    let program_data = include_bytes!("../../cairo_programs/cairo-1-contracts/u256_sqrt.casm");
+    run_cairo_1_entrypoint(
+        program_data.as_slice(),
+        0,
+        &vec![1_usize.into()],
+        &vec![1_usize.into()],
+    );
+}
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn u256_sqrt_zero() {
+    let program_data = include_bytes!("../../cairo_programs/cairo-1-contracts/u256_sqrt.casm");
+    run_cairo_1_entrypoint(
+        program_data.as_slice(),
+        0,
+        &vec![0_usize.into()],
+        &vec![0_usize.into()],
+    );
+}
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn u256_sqrt_big_num() {
+    let program_data = include_bytes!("../../cairo_programs/cairo-1-contracts/u256_sqrt.casm");
+    run_cairo_1_entrypoint(
+        program_data.as_slice(),
+        144,
+        &vec![],
+        &vec![1125899906842624_usize.into()],
+    );
+}
+// Failing due to overflow, it shouldn't!
+// #[test]
+// #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+// fn u256_sqrt_max_num() {
+//     let program_data = include_bytes!("../../cairo_programs/cairo-1-contracts/u256_sqrt.casm");
+//     run_cairo_1_entrypoint(
+//         program_data.as_slice(),
+//         257,
+//         &vec![],
+//         &vec![1.into()],
+//     );
+// }
