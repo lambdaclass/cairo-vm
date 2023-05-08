dc6eb982bc7d657ec8e6301910f76e35f3e5c9a4
juanbono <juanbono94@gmail.com>
2023-05-08 15:40:07 -0300

    add test for widemul128

--- src/tests/cairo_1_run_from_entrypoint_tests.rs
+++ src/tests/cairo_1_run_from_entrypoint_tests.rs
@@ -23,3 +23,15 @@ fn fibonacci_3() {
         &[9_usize.into()],
     );
 }
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn wide_mul_128_test() {
+    let program_data = include_bytes!("../../cairo_programs/cairo-1-contracts/widemul128.casm");
+    run_cairo_1_entrypoint(
+        program_data.as_slice(),
+        0,
+        &[4_usize.into(), 2_usize.into()],
+        &[1_usize.into()],
+    );
+}
