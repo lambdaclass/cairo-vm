47ef7ca8b6c90fa005f2c62ae7b3eb501d99e5c2
Milton <milton.scuderi@lambdaclass.com>
2023-05-08 13:03:45 -0300

    Add feature cairo-1-hint to test

--- src/tests/cairo_1_hints.rs
+++ src/tests/cairo_1_hints.rs
@@ -1,5 +1,7 @@
+#[cfg(feature = "cairo-1-hints")]
 use crate::tests::*;
 
+#[cfg(feature = "cairo-1-hints")]
 #[test]
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
 fn alloc_felt_252_dict() {
