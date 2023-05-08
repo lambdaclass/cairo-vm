46115c176cf9647b7860b71392d1459dc80a683d
Roberto Catalan <rcatalan@itba.edu.ar>
2023-05-08 13:29:10 -0300

    Add rust test for Cairo 1 program

--- src/tests/cairo_1_run_from_entrypoint_tests.rs
+++ src/tests/cairo_1_run_from_entrypoint_tests.rs
@@ -27,12 +27,8 @@ fn fibonacci_3() {
 #[test]
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
 fn random_ec_point() {
-    let program_data = include_bytes!("../../cairo_programs/cairo-1-contracts/random_ec_point.casm");
+    let program_data =
+        include_bytes!("../../cairo_programs/cairo-1-contracts/random_ec_point.casm");
     // No arguments needed or return value expected. It should just not panic, there is an assert in the contract.
-    run_cairo_1_entrypoint(
-        program_data.as_slice(),
-        0,
-        &[],
-        &[],
-    );
+    run_cairo_1_entrypoint(program_data.as_slice(), 0, &[], &[]);
 }
