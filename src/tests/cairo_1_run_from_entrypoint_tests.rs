f2b087e29f789bce0afa687249f2e9b218af83fe
Nacho <amigoigna@gmail.com>
2023-05-10 15:34:12 -0300

    test less than

--- src/tests/cairo_1_run_from_entrypoint_tests.rs
+++ src/tests/cairo_1_run_from_entrypoint_tests.rs
@@ -2,6 +2,32 @@ use crate::tests::*;
 
 #[test]
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn test_less_than_or_equal() {
+    let program_data = include_bytes!("../../cairo_programs/cairo-1-contracts/test_less_than.casm");
+    run_cairo_1_entrypoint(
+        program_data.as_slice(),
+        0,
+        &[8_usize.into()],
+        &[1_u8.into()],
+    );
+
+    run_cairo_1_entrypoint(
+        program_data.as_slice(),
+        0,
+        &[290_usize.into()],
+        &[0_u8.into()],
+    );
+
+    run_cairo_1_entrypoint(
+        program_data.as_slice(),
+        0,
+        &[250_usize.into()],
+        &[1_u8.into()],
+    );
+}
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
 fn fibonacci_1() {
     let program_data = include_bytes!("../../cairo_programs/cairo-1-contracts/fib.casm");
     run_cairo_1_entrypoint(
