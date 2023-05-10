b0d28c66b5f5c7a551592291713bfcfa73c0a251
mmsc2 <88055861+mmsc2@users.noreply.github.com>
2023-05-10 19:47:48 -0300

    Add arena index hint test (#1148)
    * Add arena index hint test
    
    * Modify changelog
    
    * fix changelog
    
    * fix changelog
    
    * Add test tag
    
    ---------
    
    Co-authored-by: Pedro Fontana <fontana.pedro93@gmail.com>

--- src/tests/cairo_1_run_from_entrypoint_tests.rs
+++ src/tests/cairo_1_run_from_entrypoint_tests.rs
@@ -64,6 +64,14 @@ fn divmod_hint_test() {
 
 #[test]
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn get_segment_arena_test() {
+    let program_data =
+        include_bytes!("../../cairo_programs/cairo-1-contracts/get_segment_arena_index.casm");
+    run_cairo_1_entrypoint(program_data.as_slice(), 0, &[], &[1_usize.into()]);
+}
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
 fn boxed_fibonacci() {
     let program_data =
         include_bytes!("../../cairo_programs/cairo-1-contracts/alloc_constant_size.casm");
