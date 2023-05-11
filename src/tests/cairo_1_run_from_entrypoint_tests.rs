64f535b5c60dbdf1398f58401d65c3dc6ccd801b
juanbono <juanbono94@gmail.com>
2023-05-11 16:48:49 -0300

    add impl and test

--- src/tests/cairo_1_run_from_entrypoint_tests.rs
+++ src/tests/cairo_1_run_from_entrypoint_tests.rs
@@ -458,3 +458,20 @@ fn linear_split() {
         &[0.into(), 1000.into()],
     );
 }
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn widelmul128_test() {
+    let program_data = include_bytes!("../../cairo_programs/cairo-1-contracts/widemul128.casm");
+    run_cairo_1_entrypoint(
+        program_data.as_slice(),
+        0,
+        // numbers to multiply:
+        &[4_usize.into(), 2_usize.into()],
+        // it returns: `a * b == 2**128 * res_high + res_low`
+        // that property should be 1 (true) if
+        // the implementation is correct and
+        // false otherwise.
+        &[1_usize.into()],
+    );
+}
