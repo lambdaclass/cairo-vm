9ccafef110789fe978a626e2f2d17560d8913eb0
Federica <fedemoletta@hotmail.com>
2023-05-11 13:44:30 -0300

    Add test

--- src/tests/cairo_1_run_from_entrypoint_tests.rs
+++ src/tests/cairo_1_run_from_entrypoint_tests.rs
@@ -136,3 +136,16 @@ fn linear_split() {
         &[0.into(), 1000.into()],
     );
 }
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn felt252_dict_entry_update() {
+    let program_data =
+        include_bytes!("../../cairo_programs/cairo-1-contracts/felt252_dict_entry_update.casm");
+    run_cairo_1_entrypoint(
+        program_data.as_slice(),
+        0,
+        &[],
+        &[64_usize.into(), 74_usize.into()],
+    );
+}
