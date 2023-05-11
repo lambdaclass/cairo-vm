434acc6a636b54a4ca1223e91a83853ccbc54dea
fmoletta <99273364+fmoletta@users.noreply.github.com>
2023-05-12 01:04:04 +0300

    tests: Add test that runs a contract entrypoint using the hints `Felt252DictEntryUpdate`, `GetCurrentAccessIndex` & `GetCurrentAccessAccessDelta` (#1157)
    * Add test
    
    * Update felt252_dict_entry_update.cairo
    
    * Update cairo_1_run_from_entrypoint_tests.rs
    
    ---------
    
    Co-authored-by: Pedro Fontana <fontana.pedro93@gmail.com>

--- src/tests/cairo_1_run_from_entrypoint_tests.rs
+++ src/tests/cairo_1_run_from_entrypoint_tests.rs
@@ -461,6 +461,19 @@ fn linear_split() {
 
 #[test]
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn felt252_dict_entry_update() {
+    let program_data =
+        include_bytes!("../../cairo_programs/cairo-1-contracts/felt252_dict_entry_update.casm");
+    run_cairo_1_entrypoint(
+        program_data.as_slice(),
+        0,
+        &[],
+        &[64_usize.into(), 75_usize.into()],
+    );
+}
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
 fn widelmul128_test() {
     let program_data = include_bytes!("../../cairo_programs/cairo-1-contracts/widemul128.casm");
     run_cairo_1_entrypoint(
