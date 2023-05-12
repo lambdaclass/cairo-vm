ce17abd8d761778842d1c1e0af73a81701a42522
Pedro Fontana <fontana.pedro93@gmail.com>
2023-05-12 17:43:24 -0300

    cargo clippy

--- src/tests/cairo_1_run_from_entrypoint_tests.rs
+++ src/tests/cairo_1_run_from_entrypoint_tests.rs
@@ -463,7 +463,7 @@ fn linear_split() {
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
 fn alloc_felt_252_dict() {
     let program_data = include_bytes!("../../cairo_programs/cairo-1-contracts/felt_252_dict.casm");
-    run_cairo_1_entrypoint(program_data.as_slice(), 0, &[], &vec![1.into()]);
+    run_cairo_1_entrypoint(program_data.as_slice(), 0, &[], &[1.into()]);
 }
 
 #[test]
