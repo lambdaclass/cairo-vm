2088c186fe366f4b609f2ac85d99984aab193b59
Tomá <47506558+MegaRedHand@users.noreply.github.com>
2023-05-29 16:27:30 -0300

    Patch arbitrary

--- felt/src/arbitrary.rs
+++ felt/src/arbitrary.rs
@@ -1,3 +1,4 @@
+use lambdaworks_math::{field::element::FieldElement, unsigned_integer::element::UnsignedInteger};
 use num_bigint::BigUint;
 use num_traits::Zero;
 use proptest::prelude::*;
@@ -29,7 +30,29 @@ fn any_felt_big_int<const PH: u128, const PL: u128>() -> impl Strategy<Value = F
 
 /// Returns a [`Strategy`] that generates any valid Felt252
 fn any_felt252() -> impl Strategy<Value = Felt252> {
-    any_felt_big_int::<FIELD_HIGH, FIELD_LOW>().prop_map(|value| Felt252 { value })
+    (0..=FIELD_HIGH)
+        // turn range into `impl Strategy`
+        .prop_map(|x| x)
+        // choose second 128-bit limb capped by first one
+        .prop_flat_map(|high| {
+            let low = if high == FIELD_HIGH {
+                (0..FIELD_LOW).prop_map(|x| x).sboxed()
+            } else {
+                any::<u128>().sboxed()
+            };
+            (Just(high), low)
+        })
+        // turn (u128, u128) into limbs array and then into Felt252
+        .prop_map(|(high, low)| {
+            let limbs = [
+                (high >> 64) as u64,
+                (high & ((1 << 64) - 1)) as u64,
+                (low >> 64) as u64,
+                (low & ((1 << 64) - 1)) as u64,
+            ];
+            FieldElement::new(UnsignedInteger::from_limbs(limbs))
+        })
+        .prop_map(|value| Felt252 { value })
 }
 
 /// Returns a [`Strategy`] that generates any nonzero Felt252
