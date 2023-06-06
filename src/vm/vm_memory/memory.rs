cf248df86bffaa62b43a9b36309adbb24d1f2202
Tomá <47506558+MegaRedHand@users.noreply.github.com>
2023-06-06 16:17:35 -0300

    Change approach
    The +1 in resize is used to avoid having to allocate in the case the next push falls outside capacity.
    Instead, we use a replace in the place of the insert.

--- src/vm/vm_memory/memory.rs
+++ src/vm/vm_memory/memory.rs
@@ -73,11 +73,9 @@ impl AddressSet {
 
             let offset = addr.offset;
             if offset >= self.0[segment].len() {
-                self.0[segment].resize(offset, false);
-                self.0[segment].push(true);
-            } else {
-                self.0[segment].insert(offset, true);
+                self.0[segment].resize(offset + 1, false);
             }
+            self.0[segment].replace(offset, true);
         }
     }
 }
