65a7e3318737039d1182d03af42d29defe0d3628
Juanma <juanma@Juanmas-MacBook-Air.local>
2023-06-30 10:59:14 -0300

    Use run until steps

--- fuzzer/src/fuzz_program.rs
+++ fuzzer/src/fuzz_program.rs
@@ -5,11 +5,13 @@ use cairo_vm::{
     hint_processor::builtin_hint_processor::builtin_hint_processor_definition::BuiltinHintProcessor
 };
 
+const STEPS_LIMIT: usize = 1000000;
 fn main() {
     loop {
         fuzz!(|data: (CairoRunConfig, Program)| {
             let (cairo_config, program) = data;
-            let _ = cairo_run_parsed_program(program, &cairo_config, &mut BuiltinHintProcessor::new_empty());
+            let _ = cairo_run_parsed_program(program.clone(), &CairoRunConfig::default(), &mut BuiltinHintProcessor::new_empty(), STEPS_LIMIT);
+            let _ = cairo_run_parsed_program(program, &cairo_config, &mut BuiltinHintProcessor::new_empty(), STEPS_LIMIT);
         });
     }
 }
