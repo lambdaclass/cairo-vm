b20dc3e5255438572cb591312b783888774d643a
Tomá <47506558+MegaRedHand@users.noreply.github.com>
2023-08-02 10:50:07 -0300

    Revert star import change

--- felt/src/lib.rs
+++ felt/src/lib.rs
@@ -15,10 +15,10 @@ mod lib_lambdaworks;
 use core::fmt;
 
 #[cfg(feature = "lambdaworks-felt")]
-pub use lib_lambdaworks::*;
+pub use lib_lambdaworks::Felt252;
 
 #[cfg(not(feature = "lambdaworks-felt"))]
-pub use lib_bigint_felt::*;
+pub use lib_bigint_felt::Felt252;
 
 pub const PRIME_STR: &str = "0x800000000000011000000000000000000000000000000000000000000000001"; // in decimal, this is equal to 3618502788666131213697322783095070105623107215331596699973092056135872020481
 pub const FIELD_HIGH: u128 = (1 << 123) + (17 << 64); // this is equal to 10633823966279327296825105735305134080
