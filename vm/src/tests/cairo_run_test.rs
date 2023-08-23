03abbc67ccef4da1dcecd4edf131eef60762d8fa
fmoletta <99273364+fmoletta@users.noreply.github.com>
2023-08-23 15:45:24 -0300

    Update cairo_run_test.rs

--- vm/src/tests/cairo_run_test.rs
+++ vm/src/tests/cairo_run_test.rs
@@ -1008,7 +1008,6 @@ fn fibonacci_proof_mode_disable_trace_padding() {
 }
 
 #[test]
-#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
 fn cairo_run_overflowing_dict() {
     let program_data =
         include_bytes!("../../../cairo_programs/manually_compiled/overflowing_dict.json");
