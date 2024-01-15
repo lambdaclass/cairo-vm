0d4cff54a76baa9cedb26f9df09c5bd063f3aa3d
Federica <fedemoletta@hotmail.com>
2024-01-15 18:35:07 -0300

    Update test

--- cairo1-run/src/main.rs
+++ cairo1-run/src/main.rs
@@ -1105,6 +1105,6 @@ mod tests {
     #[case(["cairo1-run", "../cairo_programs/cairo-1-programs/struct_span_return.cairo", "--print_output", "--trace_file", "/dev/null", "--memory_file", "/dev/null", "--layout", "all_cairo", "--proof_mode", "--air_public_input", "/dev/null", "--air_private_input", "/dev/null"].as_slice())]
     fn test_struct_span_return(#[case] args: &[&str]) {
         let args = args.iter().cloned().map(String::from);
-        assert_matches!(run(args), Ok(Some(res)) if res == "[[4 3][2 1]]");
+        assert_matches!(run(args), Ok(Some(res)) if res == "[[4 3] [2 1]]");
     }
 }
