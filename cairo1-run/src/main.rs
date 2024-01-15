27d54907912bf9bbdc9fb669f3f62f8a1c7ee451
Federica <fedemoletta@hotmail.com>
2024-01-15 11:04:28 -0300

    Apply input arguments fix from cairo_lang

--- cairo1-run/src/main.rs
+++ cairo1-run/src/main.rs
@@ -193,7 +193,12 @@ enum Error {
     #[error("Failed to extract return values from VM")]
     FailedToExtractReturnValues,
     #[error("Function expects arguments of size {expected} and received {actual} instead.")]
-    ArgumentsSizeMismatch { expected: usize, actual: usize },
+    ArgumentsSizeMismatch { expected: i16, actual: i16 },
+    #[error("Function param {param_index} only partially contains argument {arg_index}.")]
+    ArgumentUnaligned {
+        param_index: usize,
+        arg_index: usize,
+    },
 }
 
 pub struct FileWriter {
@@ -642,7 +647,8 @@ fn create_entry_code(
     }
     let mut array_args_data_iter = array_args_data.iter();
     let after_arrays_data_offset = ap_offset;
-    let mut arg_iter = args.iter().peekable();
+    let mut arg_iter = args.iter().enumerate();
+    let mut param_index = 0;
     let mut expected_arguments_size = 0;
     if func.signature.param_types.iter().any(|ty| {
         get_info(sierra_program_registry, ty)
@@ -666,7 +672,6 @@ fn create_entry_code(
     for ty in func.signature.param_types.iter() {
         let info = get_info(sierra_program_registry, ty)
             .ok_or_else(|| Error::NoInfoForType(ty.clone()))?;
-        let ty_size = type_sizes[ty];
         let generic_ty = &info.long_id.generic_id;
         if let Some(offset) = builtin_offset.get(generic_ty) {
             let mut offset = *offset;
@@ -677,46 +682,69 @@ fn create_entry_code(
             casm_extend! {ctx,
                 [ap + 0] = [fp - offset], ap++;
             }
+            ap_offset += 1;
         } else if generic_ty == &SystemType::ID {
             casm_extend! {ctx,
                 %{ memory[ap + 0] = segments.add() %}
                 ap += 1;
             }
+            ap_offset += 1;
         } else if generic_ty == &GasBuiltinType::ID {
             casm_extend! {ctx,
                 [ap + 0] = initial_gas, ap++;
             }
+            ap_offset += 1;
         } else if generic_ty == &SegmentArenaType::ID {
             let offset = -ap_offset + after_arrays_data_offset;
             casm_extend! {ctx,
                 [ap + 0] = [ap + offset] + 3, ap++;
             }
-        } else if let Some(FuncArg::Array(_)) = arg_iter.peek() {
-            let values = extract_matches!(arg_iter.next().unwrap(), FuncArg::Array);
-            let offset = -ap_offset + array_args_data_iter.next().unwrap();
-            expected_arguments_size += 1;
-            casm_extend! {ctx,
-                [ap + 0] = [ap + (offset)], ap++;
-                [ap + 0] = [ap - 1] + (values.len()), ap++;
-            }
+            ap_offset += 1;
         } else {
-            let arg_size = ty_size;
-            expected_arguments_size += arg_size as usize;
-            for _ in 0..arg_size {
-                if let Some(value) = arg_iter.next() {
-                    let value = extract_matches!(value, FuncArg::Single);
-                    casm_extend! {ctx,
-                        [ap + 0] = (value.to_bigint()), ap++;
+            let ty_size = type_sizes[ty];
+            let param_ap_offset_end = ap_offset + ty_size;
+            expected_arguments_size += ty_size;
+            while ap_offset < param_ap_offset_end {
+                let Some((arg_index, arg)) = arg_iter.next() else {
+                    break;
+                };
+                match arg {
+                    FuncArg::Single(value) => {
+                        casm_extend! {ctx,
+                            [ap + 0] = (value.to_bigint()), ap++;
+                        }
+                        ap_offset += 1;
+                    }
+                    FuncArg::Array(values) => {
+                        let offset = -ap_offset + array_args_data_iter.next().unwrap();
+                        casm_extend! {ctx,
+                            [ap + 0] = [ap + (offset)], ap++;
+                            [ap + 0] = [ap - 1] + (values.len()), ap++;
+                        }
+                        ap_offset += 2;
+                        if ap_offset > param_ap_offset_end {
+                            return Err(Error::ArgumentUnaligned {
+                                param_index,
+                                arg_index,
+                            });
+                        }
                     }
                 }
             }
+            param_index += 1;
         };
-        ap_offset += ty_size;
     }
-    if expected_arguments_size != args.len() {
+    let actual_args_size = args
+        .iter()
+        .map(|arg| match arg {
+            FuncArg::Single(_) => 1,
+            FuncArg::Array(_) => 2,
+        })
+        .sum::<i16>();
+    if expected_arguments_size != actual_args_size {
         return Err(Error::ArgumentsSizeMismatch {
             expected: expected_arguments_size,
-            actual: args.len(),
+            actual: actual_args_size,
         });
     }
 
