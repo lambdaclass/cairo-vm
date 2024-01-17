3d17276871a5b004e8dba788065f89b97d71a50a
Federica <fedemoletta@hotmail.com>
2024-01-17 16:43:23 -0300

    Expand comparison script

--- vm/src/tests/cairo_pie_comparator.py
+++ vm/src/tests/cairo_pie_comparator.py
@@ -8,13 +8,16 @@ from zipfile import ZipFile
 filename1 = sys.argv[1]
 filename2 = sys.argv[2]
 
-with ZipFile(filename1) as cairo_lang_pie_zip, ZipFile(filename2) as cairo_vm_pie_zip:
-    with cairo_lang_pie_zip.open('version.json') as cl_verison_file, cairo_vm_pie_zip.open('version.json') as cv_verison_file:
-        cl_version = json.load(cl_verison_file)
-        cv_version = json.load(cv_verison_file)
-        if cl_version == cv_verison_file:
-            print(f"Comparison succesful for {filename1}/version.json vs {filename2}/version.json")
-        else:
-            print(f"Comparison unsuccesful for {filename1}/version.json  vs {filename2}/version.json")
-            exit(1)
+strict_comparison_files = ["version.json", "metadata.json", "execution_resources.json", "additional_data.json"]
 
+with ZipFile(filename1) as cairo_lang_pie_zip, ZipFile(filename2) as cairo_vm_pie_zip:
+    # Compare json files
+    for file in strict_comparison_files:
+        with cairo_lang_pie_zip.open(file) as cairo_lang_file, cairo_vm_pie_zip.open(file) as cairo_vm_file:
+            cl_content = json.load(cairo_lang_file)
+            cv_content = json.load(cairo_vm_file)
+            if cl_content == cv_content:
+                print(f"Comparison succesful for {filename1}/{file} vs {filename2}/{file}")
+            else:
+                print(f"Comparison unsuccesful for {filename1}/{file}  vs {filename2}/{file}")
+                exit(1)
