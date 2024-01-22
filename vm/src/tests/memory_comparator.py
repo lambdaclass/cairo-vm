2f715c43308f2e0b5d61088883b6f3f4571926c2
fmoletta <99273364+fmoletta@users.noreply.github.com>
2024-01-22 22:56:34 +0200

    Add `cairo_pie_output` flag to `cairo_vm_cli` (#1578)
    * First draft
    
    * Add special serialization for ecdsa builtin additional data
    
    * Start comparison script
    
    * Add compare_all_no_proof target
    
    * Update file permissions
    
    * Fix makefile
    
    * Add special serialization for pedersen builtin additional data
    
    * Fix prev commit
    
    * Serialize ecdsa additional data as sequence
    
    * Expand comparison script
    
    * Skip bugged case + Reduce amount of prints in comparison script
    
    * Improve skip
    
    * Add comparison to CI
    
    * Fix
    
    * Fix pie memory serialization
    
    * Fix constant value
    
    * Save temp changes
    
    * Revert "Fix constant value"
    
    This reverts commit 08e037eca1c0e4376f748a7bdca10462f19f586f.
    
    * Use to_bytes instead of serialize impl + simplify scripts
    
    * Cleanup
    
    * Fix test
    
    * Clean interface + remove unwraps
    
    * Fixes
    
    * clippy
    
    * Move arg validation to a separate func
    
    * Move mutually exclusive argument validation to clap
    
    * rename var
    
    * Add Chaneglog entry
    
    * Make zip dep optional
    
    * Fix wasm imports
    
    * Add file extension
    
    * Add file extension
    
    * Remove unwrap + improve coverage
    
    * quick fix
    
    * Clean solution
    
    * Update README
    
    * Update makefile targets
    
    ---------
    
    Co-authored-by: Pedro Fontana <fontana.pedro93@gmail.com>
    Co-authored-by: Mario Rugiero <mario.rugiero@lambdaclass.com>

--- vm/src/tests/memory_comparator.py
+++ vm/src/tests/memory_comparator.py
@@ -5,45 +5,28 @@ import sys
 def main():
     filename1 = sys.argv[1]
     filename2 = sys.argv[2]
-    cairo_mem = {}
-    cairo_rs_mem = {}
 
-    with open(filename1, 'rb') as f:
-        cairo_raw = f.read()
-        assert len(cairo_raw) % 40 == 0, f'{filename1}: malformed memory file from Cairo VM'
-        chunks = len(cairo_raw) // 40
-        for i in range(0, chunks):
-            chunk = cairo_raw[i*40:(i+1)*40]
-            k, v = int.from_bytes(chunk[:8], 'little'), int.from_bytes(chunk[8:], 'little')
-            assert k not in cairo_mem, f'{filename1}: address {k} has two values'
-            cairo_mem[k] = v
-        assert len(cairo_mem) * 40 == len(cairo_raw), f'{filename1}: {len(cairo_mem) * 40} != {len(cairo_raw)}'
+    with open(filename1, 'rb') as f1, open(filename2, 'rb') as f2:
+        compare_memory_file_contents(f1.read(), f2.read())
 
-    with open(filename2, 'rb') as f:
-        cairo_rs_raw = f.read()
-        assert len(cairo_rs_raw) % 40 == 0, f'{filename2}: malformed memory file from cairo-vm'
-        chunks = len(cairo_rs_raw) // 40
-        for i in range(0, chunks):
-            chunk = cairo_rs_raw[i*40:(i+1)*40]
-            k, v = int.from_bytes(chunk[:8], 'little'), int.from_bytes(chunk[8:], 'little')
-            assert k not in cairo_rs_mem, f'{filename2}: address {k} has two values'
-            cairo_rs_mem[k] = v
-        assert len(cairo_rs_mem) * 40 == len(cairo_rs_raw), f'{filename2}: {len(cairo_rs_mem) * 40} != {len(cairo_rs_raw)}'
+def compare_memory_file_contents(cairo_raw_mem, cairo_rs_raw_mem):
+    cairo_mem = read_memory_file_contents(cairo_raw_mem)
+    cairo_rs_mem = read_memory_file_contents(cairo_rs_raw_mem)
 
-    assert len(cairo_mem) == len(cairo_rs_mem), f'{filename2}: len(cairo_mem)={len(cairo_mem)} len(cairo_mem)={len(cairo_rs_mem)}'
+    assert len(cairo_mem) == len(cairo_rs_mem), f'len(cairo_mem)={len(cairo_mem)} len(cairo_mem)={len(cairo_rs_mem)}'
     if cairo_mem != cairo_rs_mem:
-        print(f'Mismatch between {filename1} (Cairo) and {filename2} (cairo_rs)')
-        print('keys in Cairo but not cairo-vm:')
+        print(f'Mismatch between cairo_lang and cairo-vm')
+        print('keys in cairo_lang but not cairo-vm:')
         for k in cairo_mem:
             if k in cairo_rs_mem:
                 continue
-            print(f'{k}:{v}')
-        print('keys in cairo_rs but not Cairo:')
+        print(f'{k}:{cairo_mem[k]}')
+        print('keys in cairo-vm but not cairo_lang:')
         for k in cairo_rs_mem:
             if k in cairo_mem:
                 continue
-            print(f'{k}:{v}')
-        print('mismatched values (Cairo <-> cairo_rs)):')
+            print(f'{k}:{cairo_rs_mem[k]}')
+        print('mismatched values (cairo_lang <-> cairo-vm)):')
         for k in cairo_rs_mem:
             if k not in cairo_mem:
                 continue
@@ -52,6 +35,17 @@ def main():
             print(f'{k}:({cairo_mem[k]} <-> {cairo_rs_mem[k]})')
         exit(1)
 
+def read_memory_file_contents(raw_mem_content) -> {}:
+        mem = {}
+        assert len(raw_mem_content) % 40 == 0, f'Malformed memory file'
+        chunks = len(raw_mem_content) // 40
+        for i in range(0, chunks):
+            chunk = raw_mem_content[i*40:(i+1)*40]
+            k, v = int.from_bytes(chunk[:8], 'little'), int.from_bytes(chunk[8:], 'little')
+            assert k not in mem, f'Address {k} has two values'
+            mem[k] = v
+        assert len(mem) * 40 == len(raw_mem_content), f'Malformed memory file'
+        return mem
 
 if __name__ == '__main__':
     main()
