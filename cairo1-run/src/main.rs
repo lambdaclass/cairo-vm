69e244ef964e867caf4853d5a1c7d2101a3c527f
Federica <fedemoletta@hotmail.com>
2024-03-18 12:32:30 -0300

    Fix test value

--- cairo1-run/src/main.rs
+++ cairo1-run/src/main.rs
@@ -596,7 +596,7 @@ mod tests {
     #[case(["cairo1-run", "../cairo_programs/cairo-1-programs/tensor_new.cairo", "--print_output", "--trace_file", "/dev/null", "--memory_file", "/dev/null", "--layout", "all_cairo", "--proof_mode", "--air_public_input", "/dev/null", "--air_private_input", "/dev/null"].as_slice())]
     fn test_run_tensor_new(#[case] args: &[&str]) {
         let args = args.iter().cloned().map(String::from);
-        let expected_output = "[1 2] [1 false 2 true]";
+        let expected_output = "[1 2] [1 false 1 true]";
         assert_matches!(run(args), Ok(Some(res)) if res == expected_output);
     }
 }
