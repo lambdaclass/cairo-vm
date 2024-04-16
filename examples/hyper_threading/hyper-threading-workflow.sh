11b0c55754a0ddbcc818e758f5f7d4de7f9ee677
Pedro Fontana <peter@Pedros-MacBook-Pro.local>
2024-04-16 18:00:59 -0300

    use echo -e

--- examples/hyper_threading/hyper-threading-workflow.sh
+++ examples/hyper_threading/hyper-threading-workflow.sh
@@ -6,8 +6,8 @@ thread_counts=(1 2 4 6 8 16 )
 # Define binary names
 binaries=("hyper_threading_main" "hyper_threading_pr")
 
-echo "**Hyper Thereading Benchmark results**" >> result.md
-echo "\n \n \n " >> result.md
+echo -e "**Hyper Thereading Benchmark results**" >> result.md
+echo -e "\n \n \n " >> result.md
 
 # Iter over thread_counts
 for threads in "${thread_counts[@]}"; do
@@ -21,10 +21,10 @@ for threads in "${thread_counts[@]}"; do
     
     # Execute 
     echo "Running benchmark for ${threads} threads"
-    echo "\n \n \n " >> result.md
-    echo $cmd >> result.md 
-    eval $cmd >> result.md
-    echo "\n \n \n " >> result.md
+    echo -e "\n \n \n " >> result.md
+    echo -e $cmd >> result.md 
+    eval -e $cmd >> result.md
+    echo -e "\n \n \n " >> result.md
 done
 
 {
