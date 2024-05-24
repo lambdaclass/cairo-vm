e6e0426aa513381da937b71951053d2743c211b6
Federica <fedemoletta@hotmail.com>
2024-05-24 11:52:24 -0300

    Add MarginParams

--- vm/src/hint_processor/builtin_hint_processor/excess_balance.rs
+++ vm/src/hint_processor/builtin_hint_processor/excess_balance.rs
@@ -4,12 +4,21 @@ use num_traits::Pow;
 
 use crate::{math_utils::signed_felt, types::relocatable::Relocatable, vm::{errors::vm_errors::VirtualMachineError, vm_memory::memory::Memory}};
 
-#[derive(Default, Debug, PartialEq)]
+#[derive(Debug, PartialEq)]
 struct Position {
     market: String,
     amount: Decimal,
     cost: Decimal,
     cached_funding: Decimal,
+}
+
+#[derive(Debug, PartialEq)]
+struct MarginParams {
+    market: String,
+    imf_base: Decimal,
+    imf_factor: Decimal,
+    mmf_factor: Decimal,
+    imf_shift: Decimal,
 }
 
 impl Position {
@@ -19,10 +28,26 @@ impl Position {
             d * Decimal::from(10).pow(-8_i64)
         };
         Ok(Position {
-            market: core::str::from_utf8(&memory.get_integer(read_ptr)?.to_bytes_be()).unwrap().trim_start_matches("\0").to_string(),
+            market: core::str::from_utf8(&memory.get_integer(read_ptr)?.to_bytes_be()).unwrap_or_default().trim_start_matches("\0").to_string(),
             amount: felt_to_scaled_decimal(memory.get_integer((read_ptr + 1)?)?.into_owned()),
             cost: felt_to_scaled_decimal(memory.get_integer((read_ptr + 2)?)?.into_owned()),
             cached_funding: felt_to_scaled_decimal(memory.get_integer((read_ptr + 3)?)?.into_owned()),
+        })
+    }
+}
+
+impl MarginParams {
+    fn read_from_memory(memory: &Memory, read_ptr: Relocatable) -> Result<Self, VirtualMachineError> {
+        let felt_to_scaled_decimal = |f: Felt252| -> Decimal {
+            let d = Decimal::from_str_radix(&signed_felt(f).to_string(), 10).unwrap_or_default();
+            d * Decimal::from(10).pow(-8_i64)
+        };
+        Ok(MarginParams {
+            market: core::str::from_utf8(&memory.get_integer(read_ptr)?.to_bytes_be()).unwrap_or_default().trim_start_matches("\0").to_string(),
+            imf_base: felt_to_scaled_decimal(memory.get_integer((read_ptr + 4)?)?.into_owned()),
+            imf_factor: felt_to_scaled_decimal(memory.get_integer((read_ptr + 5)?)?.into_owned()),
+            mmf_factor: felt_to_scaled_decimal(memory.get_integer((read_ptr + 6)?)?.into_owned()),
+            imf_shift: felt_to_scaled_decimal(memory.get_integer((read_ptr + 7)?)?.into_owned()),
         })
     }
 }
@@ -49,6 +74,25 @@ mod tests {
             cached_funding: Decimal::from_scientific("0e-8").unwrap()
         };
         assert_eq!(expected_position, Position::read_from_memory(&memory, (0, 0).into()).unwrap())
+    }
+
+    #[test]
+    fn test_read_margin_params() {
+        let memory = memory![
+           ((0, 0), ("20527877651862571847371805264", 10)),
+           ((0, 4), 5000000),
+           ((0, 5), 20000),
+           ((0, 6), 50000000),
+           ((0, 7), 20000000000000)
+        ];
+        let expected_position = MarginParams {
+            market: String::from("BTC-USD-PERP"),
+            imf_base: Decimal::from_str("0.05000000").unwrap(),
+            imf_factor: Decimal::from_str("0.00020000").unwrap(),
+            mmf_factor: Decimal::from_str("0.50000000").unwrap(),
+            imf_shift: Decimal::from_str("200000.00000000").unwrap()
+        };
+        assert_eq!(expected_position, MarginParams::read_from_memory(&memory, (0, 0).into()).unwrap())
     }
 }
 
