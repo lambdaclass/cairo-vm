5960a2b8d2e23009a848befdce0614b5c6c72125
Federica <fedemoletta@hotmail.com>
2024-05-27 15:40:46 -0300

    refactor + handle errors

--- vm/src/hint_processor/builtin_hint_processor/excess_balance.rs
+++ vm/src/hint_processor/builtin_hint_processor/excess_balance.rs
@@ -2,6 +2,7 @@ use crate::{
     hint_processor::hint_processor_definition::HintReference,
     serde::deserialize_program::ApTracking,
     stdlib::collections::HashMap,
+    types::relocatable::MaybeRelocatable,
     vm::{errors::hint_errors::HintError, vm_core::VirtualMachine},
 };
 use core::{cmp::max, str::FromStr};
@@ -19,6 +20,25 @@ use crate::{
 use lazy_static::lazy_static;
 
 use super::{dict_manager::DictManager, hint_utils::get_ptr_from_var_name};
+
+// General helper functions
+
+fn felt_to_scaled_decimal(f: &Felt252) -> Option<Decimal> {
+    let mut d = Decimal::from_str_radix(&signed_felt(*f).to_string(), 10).ok()?;
+    d.set_scale(8).ok();
+    Some(d)
+}
+
+fn felt_to_trimmed_str(f: &Felt252) -> Option<String> {
+    Some(
+        core::str::from_utf8(&f.to_bytes_be())
+            .ok()?
+            .trim_start_matches('\0')
+            .to_string(),
+    )
+}
+
+// Internal Data types
 
 #[derive(Debug, PartialEq, Eq, Hash)]
 struct Position {
@@ -38,152 +58,211 @@ struct MarginParams {
 }
 
 impl Position {
-    fn read_from_memory(
-        // TODO: Make this receive a MaybeRelocatable so we dont need to zip twice on iterator
-        memory: &Memory,
-        read_ptr: Relocatable,
-    ) -> Result<Self, VirtualMachineError> {
-        Ok(Position {
-            market: core::str::from_utf8(&memory.get_integer(read_ptr)?.to_bytes_be())
-                .unwrap_or_default()
-                .trim_start_matches("\0")
-                .to_string(),
-            amount: felt_to_scaled_decimal(memory.get_integer((read_ptr + 1)?)?.as_ref()),
-            cost: felt_to_scaled_decimal(memory.get_integer((read_ptr + 2)?)?.as_ref()),
-            cached_funding: felt_to_scaled_decimal(memory.get_integer((read_ptr + 3)?)?.as_ref()),
+    fn read_from_memory(memory: &Memory, read_ptr: Relocatable) -> Option<Self> {
+        Some(Position {
+            market: felt_to_trimmed_str(memory.get_integer(read_ptr).ok()?.as_ref())?,
+            amount: felt_to_scaled_decimal(
+                memory.get_integer((read_ptr + 1_u32).ok()?).ok()?.as_ref(),
+            )?,
+            cost: felt_to_scaled_decimal(
+                memory.get_integer((read_ptr + 2_u32).ok()?).ok()?.as_ref(),
+            )?,
+            cached_funding: felt_to_scaled_decimal(
+                memory.get_integer((read_ptr + 3_u32).ok()?).ok()?.as_ref(),
+            )?,
         })
     }
 }
 
 impl MarginParams {
-    fn read_from_memory(
-        memory: &Memory,
-        read_ptr: Relocatable,
-    ) -> Result<Self, VirtualMachineError> {
-        Ok(MarginParams {
-            market: felt_to_trimmed_str(memory.get_integer(read_ptr)?.as_ref()),
-            imf_base: felt_to_scaled_decimal(memory.get_integer((read_ptr + 4)?)?.as_ref()),
-            imf_factor: felt_to_scaled_decimal(memory.get_integer((read_ptr + 5)?)?.as_ref()),
-            mmf_factor: felt_to_scaled_decimal(memory.get_integer((read_ptr + 6)?)?.as_ref()),
-            imf_shift: felt_to_scaled_decimal(memory.get_integer((read_ptr + 7)?)?.as_ref()),
+    fn read_from_memory(memory: &Memory, read_ptr: Relocatable) -> Option<Self> {
+        Some(MarginParams {
+            market: felt_to_trimmed_str(memory.get_integer(read_ptr).ok()?.as_ref())?,
+            imf_base: felt_to_scaled_decimal(
+                memory.get_integer((read_ptr + 4_u32).ok()?).ok()?.as_ref(),
+            )?,
+            imf_factor: felt_to_scaled_decimal(
+                memory.get_integer((read_ptr + 5_u32).ok()?).ok()?.as_ref(),
+            )?,
+            mmf_factor: felt_to_scaled_decimal(
+                memory.get_integer((read_ptr + 6_u32).ok()?).ok()?.as_ref(),
+            )?,
+            imf_shift: felt_to_scaled_decimal(
+                memory.get_integer((read_ptr + 7_u32).ok()?).ok()?.as_ref(),
+            )?,
         })
     }
 
-    fn imf(&self, abs_value: Decimal) -> Decimal {
-        let mut diff = (abs_value - self.imf_shift).trunc_with_scale(8);
-        diff.set_scale(8);
-        diff = diff.trunc();
-        let max = BigUint::from_str(&Decimal::ZERO.max(diff).to_string()).unwrap();
-        let part_sqrt = isqrt(&max).unwrap();
-        let mut part_sqrt = Decimal::from_str(&part_sqrt.to_string()).unwrap();
-        part_sqrt.set_scale(4);
-        self.imf_base.max(self.imf_factor * part_sqrt)
+    fn imf(&self, abs_value: Decimal) -> Option<Decimal> {
+        let mut diff = (abs_value - self.imf_shift);
+        diff.set_scale(8).ok()?;
+        let max = BigUint::from_str(&Decimal::ZERO.max(diff.trunc()).to_string()).ok()?;
+        let part_sqrt = isqrt(&max).ok()?;
+        let mut part_sqrt = Decimal::from_str(&part_sqrt.to_string()).ok()?;
+        part_sqrt.set_scale(4).ok()?;
+        Some(self.imf_base.max(self.imf_factor * part_sqrt))
     }
 
-    fn mmf(self, abs_value: Decimal) -> Decimal {
-        self.mmf_factor * self.imf(abs_value)
+    fn mmf(self, abs_value: Decimal) -> Option<Decimal> {
+        Some(self.mmf_factor * self.imf(abs_value)?)
     }
 }
 
-fn felt_to_scaled_decimal(f: &Felt252) -> Decimal {
-    let mut d = Decimal::from_str_radix(&signed_felt(*f).to_string(), 10).unwrap_or_default();
-    d.rescale(8);
-    d
+// Excess Balance helpers
+
+fn dict_ref_from_var_name<'a>(
+    var_name: &'a str,
+    vm: &'a VirtualMachine,
+    dict_manager: &'a DictManager,
+    ids_data: &'a HashMap<String, HintReference>,
+    ap_tracking: &'a ApTracking,
+) -> Option<&'a HashMap<MaybeRelocatable, MaybeRelocatable>> {
+    let prices_cache_ptr = get_ptr_from_var_name(var_name, vm, ids_data, ap_tracking).ok()?;
+    Some(
+        dict_manager
+            .get_tracker(prices_cache_ptr)
+            .ok()?
+            .get_dictionary_ref(),
+    )
 }
 
-fn felt_to_trimmed_str(f: &Felt252) -> String {
-    core::str::from_utf8(&f.to_bytes_be())
-        .unwrap_or_default()
-        .trim_start_matches("\0")
-        .to_string()
+fn prices_dict(
+    vm: &VirtualMachine,
+    dict_manager: &DictManager,
+    ids_data: &HashMap<String, HintReference>,
+    ap_tracking: &ApTracking,
+) -> Option<HashMap<String, Decimal>> {
+    // Fetch dictionary
+    let prices = dict_ref_from_var_name("prices_dict", vm, dict_manager, ids_data, ap_tracking)?;
+
+    // Apply data type conversions
+    let apply_conversion =
+        |k: &MaybeRelocatable, v: &MaybeRelocatable| -> Option<(String, Decimal)> {
+            Some((
+                felt_to_trimmed_str(k.get_int_ref()?)?,
+                felt_to_scaled_decimal(v.get_int_ref()?)?,
+            ))
+        };
+
+    prices
+        .iter()
+        .map(|(k, v)| apply_conversion(k, v))
+        .collect::<Option<_>>()
+}
+
+fn indices_dict(
+    vm: &VirtualMachine,
+    dict_manager: &DictManager,
+    ids_data: &HashMap<String, HintReference>,
+    ap_tracking: &ApTracking,
+) -> Option<HashMap<String, Decimal>> {
+    // Fetch dictionary
+    let indices = dict_ref_from_var_name("indices_dict", vm, dict_manager, ids_data, ap_tracking)?;
+
+    // Apply data type conversions
+    let apply_conversion =
+        |k: &MaybeRelocatable, v: &MaybeRelocatable| -> Option<(String, Decimal)> {
+            Some((
+                felt_to_trimmed_str(k.get_int_ref()?)?,
+                felt_to_scaled_decimal(v.get_int_ref()?)?,
+            ))
+        };
+
+    indices
+        .iter()
+        .map(|(k, v)| apply_conversion(k, v))
+        .collect::<Option<_>>()
+}
+
+fn perps_dict(
+    vm: &VirtualMachine,
+    dict_manager: &DictManager,
+    ids_data: &HashMap<String, HintReference>,
+    ap_tracking: &ApTracking,
+) -> Option<HashMap<String, MarginParams>> {
+    // Fetch dictionary
+    let perps = dict_ref_from_var_name("perps_cache_ptr", vm, dict_manager, ids_data, ap_tracking)?;
+
+    // Apply data type conversions
+    let apply_conversion =
+        |k: &MaybeRelocatable, v: &MaybeRelocatable| -> Option<(String, MarginParams)> {
+            Some((
+                felt_to_trimmed_str(k.get_int_ref()?)?,
+                MarginParams::read_from_memory(&vm.segments.memory, v.get_relocatable()?)?,
+            ))
+        };
+
+    perps
+        .iter()
+        .map(|(k, v)| apply_conversion(k, v))
+        .collect::<Option<_>>()
+}
+
+fn fees_dict(
+    vm: &VirtualMachine,
+    dict_manager: &DictManager,
+    ids_data: &HashMap<String, HintReference>,
+    ap_tracking: &ApTracking,
+) -> Option<HashMap<Decimal, Decimal>> {
+    // Fetch dictionary
+    let fees = dict_ref_from_var_name("fees_dict", vm, dict_manager, ids_data, ap_tracking)?;
+
+    // Apply data type conversions
+    let apply_conversion =
+        |k: &MaybeRelocatable, v: &MaybeRelocatable| -> Option<(Decimal, Decimal)> {
+            Some((
+                felt_to_scaled_decimal(k.get_int_ref()?)?,
+                felt_to_scaled_decimal(v.get_int_ref()?)?,
+            ))
+        };
+
+    fees.iter()
+        .map(|(k, v)| apply_conversion(k, v))
+        .collect::<Option<_>>()
+}
+
+fn balances_dict(
+    vm: &VirtualMachine,
+    dict_manager: &DictManager,
+    ids_data: &HashMap<String, HintReference>,
+    ap_tracking: &ApTracking,
+) -> Option<HashMap<Position, Position>> {
+    // Fetch dictionary
+    let balances =
+        dict_ref_from_var_name("balances_dict", vm, dict_manager, ids_data, ap_tracking)?;
+
+    // Apply data type conversions
+    let apply_conversion =
+        |k: &MaybeRelocatable, v: &MaybeRelocatable| -> Option<(Position, Position)> {
+            Some((
+                Position::read_from_memory(&vm.segments.memory, k.get_relocatable()?)?,
+                Position::read_from_memory(&vm.segments.memory, v.get_relocatable()?)?,
+            ))
+        };
+
+    balances
+        .iter()
+        .map(|(k, v)| apply_conversion(k, v))
+        .collect::<Option<_>>()
 }
 
 fn excess_balance_func(
     vm: &VirtualMachine,
-    dict_manager: DictManager,
+    dict_manager: &DictManager,
     ids_data: &HashMap<String, HintReference>,
     ap_tracking: &ApTracking,
 ) -> Result<(), HintError> {
-    // Fetch dictionaries from memory
-    let prices_cache_ptr = get_ptr_from_var_name("prices_cache_ptr", vm, ids_data, ap_tracking)?;
-    let indices_cache_ptr = get_ptr_from_var_name("indices_cache_ptr", vm, ids_data, ap_tracking)?;
-    let perps_cache_ptr = get_ptr_from_var_name("perps_cache_ptr", vm, ids_data, ap_tracking)?;
-    let fees_cache_ptr = get_ptr_from_var_name("fees_cache_ptr", vm, ids_data, ap_tracking)?;
-    let balances_cache_ptr =
-        get_ptr_from_var_name("balances_cache_ptr", vm, ids_data, ap_tracking)?;
-
-    let prices = dict_manager
-        .get_tracker(prices_cache_ptr)?
-        .get_dictionary_ref();
-    let indices = dict_manager
-        .get_tracker(indices_cache_ptr)?
-        .get_dictionary_ref();
-    let perps = dict_manager
-        .get_tracker(perps_cache_ptr)?
-        .get_dictionary_ref();
-    let fees = dict_manager
-        .get_tracker(fees_cache_ptr)?
-        .get_dictionary_ref();
-    let balances = dict_manager
-        .get_tracker(balances_cache_ptr)?
-        .get_dictionary_ref();
-
-    // Convert dictionaries to the representation used by the this algorithm
-    let prices: HashMap<String, Decimal> = prices
-        .iter()
-        .map(|(k, v)| {
-            (k.get_int_ref()
-                .zip(v.get_int_ref())
-                .map(|(k, v)| (felt_to_trimmed_str(k), felt_to_scaled_decimal(v))))
-        })
-        .collect::<Option<_>>()
-        .ok_or_else(|| HintError::ExcessBalanceUnexpectedTypeInDict("prices".into()))?;
-
-    let indices: HashMap<String, Decimal> = indices
-        .iter()
-        .map(|(k, v)| {
-            (k.get_int_ref()
-                .zip(v.get_int_ref())
-                .map(|(k, v)| (felt_to_trimmed_str(k), felt_to_scaled_decimal(v))))
-        })
-        .collect::<Option<_>>()
-        .ok_or_else(|| HintError::ExcessBalanceUnexpectedTypeInDict("indices".into()))?;
-
-    let perps: HashMap<String, MarginParams> = perps
-        .iter()
-        .map(|(k, v)| {
-            (k.get_int_ref().zip(v.get_relocatable()).and_then(|(k, v)| {
-                MarginParams::read_from_memory(&vm.segments.memory, v)
-                    .ok()
-                    .map(|v| (felt_to_trimmed_str(k), v))
-            }))
-        })
-        .collect::<Option<_>>()
-        .ok_or_else(|| HintError::ExcessBalanceUnexpectedTypeInDict("fees".into()))?;
-
-    let fees: HashMap<Decimal, Decimal> = fees
-        .iter()
-        .map(|(k, v)| {
-            (k.get_int_ref()
-                .zip(v.get_int_ref())
-                .map(|(k, v)| (felt_to_scaled_decimal(k), felt_to_scaled_decimal(v))))
-        })
-        .collect::<Option<_>>()
-        .ok_or_else(|| HintError::ExcessBalanceUnexpectedTypeInDict("fees".into()))?;
-
-    let balances: HashMap<Position, Position> = balances
-        .iter()
-        .map(|(k, v)| {
-            (k.get_relocatable()
-                .zip(v.get_relocatable())
-                .and_then(|(k, v)| {
-                    Position::read_from_memory(&vm.segments.memory, k)
-                        .ok()
-                        .zip(Position::read_from_memory(&vm.segments.memory, v).ok())
-                }))
-        })
-        .collect::<Option<_>>()
-        .ok_or_else(|| HintError::ExcessBalanceUnexpectedTypeInDict("balances".into()))?;
+    // Fetch dictionaries
+    let prices = prices_dict(vm, dict_manager, ids_data, ap_tracking)
+        .ok_or_else(|| HintError::ExcessBalanceFailedToFecthDict("prices".into()))?;
+    let indices = indices_dict(vm, dict_manager, ids_data, ap_tracking)
+        .ok_or_else(|| HintError::ExcessBalanceFailedToFecthDict("indices".into()))?;
+    let perps = perps_dict(vm, dict_manager, ids_data, ap_tracking)
+        .ok_or_else(|| HintError::ExcessBalanceFailedToFecthDict("perps".into()))?;
+    let fees = fees_dict(vm, dict_manager, ids_data, ap_tracking)
+        .ok_or_else(|| HintError::ExcessBalanceFailedToFecthDict("fees".into()))?;
+    let balances = balances_dict(vm, dict_manager, ids_data, ap_tracking)
+        .ok_or_else(|| HintError::ExcessBalanceFailedToFecthDict("balances".into()))?;
 
     // Fetch settelement price
     let settlement_asset = String::from("USDC-USD");
@@ -252,6 +331,6 @@ mod tests {
             imf_shift: Decimal::from_str("200000.00000000").unwrap(),
         };
         let expected_res = Decimal::from_str("0.101784080000").unwrap();
-        assert_eq!(expected_res, margin_params.imf(abs_value));
+        assert_eq!(expected_res, margin_params.imf(abs_value).unwrap());
     }
 }
