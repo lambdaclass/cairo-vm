698b35ce1d8aae91c1fab7803f6b3c4daa09c3f7
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-08-30 12:11:55 -0300

    Fix zero segment location

--- vm/src/vm/vm_memory/memory_segments.rs
+++ vm/src/vm/vm_memory/memory_segments.rs
@@ -217,7 +217,7 @@ impl MemorySegmentManager {
             }
             let accessed_amount =
                 // Instead of marking the values in the zero segment until zero_segment_size as accessed we use zero_segment_size as accessed_amount
-                if !self.zero_segment_index.is_zero() && i == self.zero_segment_index {
+                if self.has_zero_segment() && i == self.zero_segment_index {
                     self.zero_segment_size
                 } else {
                     match self.memory.get_amount_of_accessed_addresses_for_segment(i) {
@@ -236,6 +236,10 @@ impl MemorySegmentManager {
             memory_holes += segment_size - accessed_amount;
         }
         Ok(memory_holes)
+    }
+
+    pub fn has_zero_segment(&self) -> bool {
+        !self.zero_segment_index.is_zero()
     }
 
     /// Returns a list of addresses of memory cells that constitute the public memory.
@@ -282,7 +286,7 @@ impl MemorySegmentManager {
     // Fills the segment with the value 0 until size is reached
     // Returns the index of the zero segment
     pub(crate) fn add_zero_segment(&mut self, size: usize) -> usize {
-        if self.zero_segment_index.is_zero() {
+        if !self.has_zero_segment() {
             self.zero_segment_index = self.add().segment_index as usize;
         }
 
@@ -298,7 +302,7 @@ impl MemorySegmentManager {
 
     // Finalizes the zero segment and clears it's tracking data from the manager
     pub(crate) fn finalize_zero_segment(&mut self) {
-        if !self.zero_segment_index.is_zero() {
+        if self.has_zero_segment() {
             self.finalize(Some(self.zero_segment_size), self.zero_segment_index, None);
             self.zero_segment_index = 0;
             self.zero_segment_size = 0;
