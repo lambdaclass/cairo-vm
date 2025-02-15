9421415d16e5a19b78a0daaa7b0d964209a8b23d
Maxim Evtush <154841002+maximevtush@users.noreply.github.com>
2025-02-15 22:39:01 +0100

    Update math_utils.rs

--- vm/src/hint_processor/builtin_hint_processor/math_utils.rs
+++ vm/src/hint_processor/builtin_hint_processor/math_utils.rs
@@ -1684,7 +1684,7 @@ mod tests {
 
     #[test]
     #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
-    fn unsigned_div_rem_inconsitent_memory() {
+    fn unsigned_div_rem_inconsistent_memory() {
         let hint_code = "from starkware.cairo.common.math_utils import assert_integer\nassert_integer(ids.div)\nassert 0 < ids.div <= PRIME // range_check_builtin.bound, \\\n    f'div={hex(ids.div)} is out of the valid range.'\nids.q, ids.r = divmod(ids.value, ids.div)";
         let mut vm = vm_with_range_check!();
         //Initialize fp
@@ -1816,7 +1816,7 @@ mod tests {
 
     #[test]
     #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
-    fn signed_div_rem_inconsitent_memory() {
+    fn signed_div_rem_inconsistent_memory() {
         let hint_code = "from starkware.cairo.common.math_utils import as_int, assert_integer\n\nassert_integer(ids.div)\nassert 0 < ids.div <= PRIME // range_check_builtin.bound, \\\n    f'div={hex(ids.div)} is out of the valid range.'\n\nassert_integer(ids.bound)\nassert ids.bound <= range_check_builtin.bound // 2, \\\n    f'bound={hex(ids.bound)} is out of the valid range.'\n\nint_value = as_int(ids.value, PRIME)\nq, ids.r = divmod(int_value, ids.div)\n\nassert -ids.bound <= q < ids.bound, \\\n    f'{int_value} / {ids.div} = {q} is out of the range [{-ids.bound}, {ids.bound}).'\n\nids.biased_q = q + ids.bound";
         let mut vm = vm_with_range_check!();
         //Initialize fp
