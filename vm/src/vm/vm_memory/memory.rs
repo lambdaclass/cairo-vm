98adca486ec4c0fe33b40fbb8eb460e4bc7f92c5
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-03-14 17:20:15 -0300

    Add comment on temporary segments

--- vm/src/vm/vm_memory/memory.rs
+++ vm/src/vm/vm_memory/memory.rs
@@ -158,6 +158,11 @@ impl AddressSet {
 
 pub struct Memory {
     pub(crate) data: Vec<Vec<MemoryCell>>,
+    /// Temporary segments are used when its necessary to write data, but we
+    /// don't know yet where it will be located. These segments are eventually
+    /// relocated to the main memory acording to `relocation_rules`. For
+    /// example, dictionaries required to be contiguous, so each is stored in a
+    /// temporary and eventually relocated to a single segment.
     pub(crate) temp_data: Vec<Vec<MemoryCell>>,
     // relocation_rules's keys map to temp_data's indices and therefore begin at
     // zero; that is, segment_index = -1 maps to key 0, -2 to key 1...
