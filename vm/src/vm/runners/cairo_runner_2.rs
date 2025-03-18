8ee22adbba25ab1c3433736411add8205fdffa76
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-03-18 17:25:11 -0300

    Implement builtin initialization

--- vm/src/vm/runners/cairo_runner_2.rs
+++ vm/src/vm/runners/cairo_runner_2.rs
@@ -1,13 +1,19 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
-use cairo_lang_executable::executable::{EntryPointKind, Executable};
+use cairo_lang_executable::executable::{EntryPointKind, Executable, ExecutableEntryPoint};
 
 use crate::{
     hint_processor::hint_processor_definition::HintReference,
     serde::deserialize_program::{Attribute, Identifier, InstructionLocation},
-    types::layout::CairoLayout,
+    types::{builtin_name::BuiltinName, layout::CairoLayout},
+    utils::is_subsequence,
     vm::{
-        errors::vm_errors::VirtualMachineError,
+        errors::{runner_errors::RunnerError, vm_errors::VirtualMachineError},
+        runners::builtin_runner::{
+            BitwiseBuiltinRunner, BuiltinRunner, EcOpBuiltinRunner, HashBuiltinRunner,
+            KeccakBuiltinRunner, ModBuiltinRunner, OutputBuiltinRunner, PoseidonBuiltinRunner,
+            RangeCheckBuiltinRunner, SignatureBuiltinRunner, RC_N_PARTS_96, RC_N_PARTS_STANDARD,
+        },
         vm_core::{VirtualMachine, VirtualMachineBuilder},
     },
     Felt252,
@@ -38,10 +44,28 @@ impl CairoRunner2 {
         instruction_locations: Option<HashMap<usize, InstructionLocation>>,
         identifiers: HashMap<String, Identifier>,
         reference_manager: Vec<HintReference>,
-    ) -> Self {
-        let virtual_machine = VirtualMachineBuilder::default().build();
+    ) -> Result<Self, RunnerError> {
+        let entrypoint = executable
+            .entrypoints
+            .iter()
+            .find(|entrypoint| {
+                // TODO: Use `Eq` once implemented on `EntryPointKind`.
+                std::mem::discriminant(&entrypoint.kind) == std::mem::discriminant(&entrypoint_kind)
+            })
+            .expect("executable had no entrypoint of required kind");
 
-        Self {
+        let builtins = get_entrypoint_builtins(entrypoint);
+
+        check_builtin_order(&builtins)?;
+
+        let builtins_set: HashSet<BuiltinName> = builtins.clone().into_iter().collect();
+        let builtin_runners = initialize_builtin_runners(&layout, builtins_set, true, true)?;
+
+        let virtual_machine = VirtualMachineBuilder::default()
+            .builtin_runners(builtin_runners)
+            .build();
+
+        Ok(Self {
             executable,
             virtual_machine,
             entrypoint_kind,
@@ -52,12 +76,153 @@ impl CairoRunner2 {
             instruction_locations,
             identifiers,
             reference_manager,
-        }
+        })
     }
 
     pub fn run(&mut self) -> Result<(), VirtualMachineError> {
         Ok(())
     }
+}
+
+// TODO: Remove this once cyclic dependency is fixed.
+// It should not be necessary, but cargo treats executable BuiltinName as a separate type
+// which is why I had to create this adapter function.
+pub fn get_entrypoint_builtins(entrypoint: &ExecutableEntryPoint) -> Vec<BuiltinName> {
+    let mut builtins = Vec::with_capacity(entrypoint.builtins.len());
+
+    for builtin in &entrypoint.builtins {
+        let adapted_builtin = BuiltinName::from_str(builtin.to_str())
+            .expect("should never fail under the same implementation");
+        builtins.push(adapted_builtin);
+    }
+
+    builtins
+}
+
+pub fn check_builtin_order(builtins: &[BuiltinName]) -> Result<(), RunnerError> {
+    let ordered_builtins = vec![
+        BuiltinName::output,
+        BuiltinName::pedersen,
+        BuiltinName::range_check,
+        BuiltinName::ecdsa,
+        BuiltinName::bitwise,
+        BuiltinName::ec_op,
+        BuiltinName::keccak,
+        BuiltinName::poseidon,
+        BuiltinName::range_check96,
+        BuiltinName::add_mod,
+        BuiltinName::mul_mod,
+    ];
+    if !is_subsequence(builtins, &ordered_builtins) {
+        return Err(RunnerError::DisorderedBuiltins);
+    };
+
+    Ok(())
+}
+
+pub fn initialize_builtin_runners(
+    layout: &CairoLayout,
+    mut builtins: HashSet<BuiltinName>,
+    allow_missing_builtins: bool,
+    create_all_builtins: bool,
+) -> Result<Vec<BuiltinRunner>, RunnerError> {
+    let mut builtin_runners = Vec::new();
+
+    if layout.builtins.output {
+        let included = builtins.remove(&BuiltinName::output);
+        if included || create_all_builtins {
+            builtin_runners.push(OutputBuiltinRunner::new(included).into());
+        }
+    }
+
+    if let Some(instance_def) = layout.builtins.pedersen.as_ref() {
+        let included = builtins.remove(&BuiltinName::pedersen);
+        if included || create_all_builtins {
+            builtin_runners.push(HashBuiltinRunner::new(instance_def.ratio, included).into());
+        }
+    }
+
+    if let Some(instance_def) = layout.builtins.range_check.as_ref() {
+        let included = builtins.remove(&BuiltinName::range_check);
+        if included || create_all_builtins {
+            builtin_runners.push(
+                RangeCheckBuiltinRunner::<RC_N_PARTS_STANDARD>::new_with_low_ratio(
+                    instance_def.ratio,
+                    included,
+                )
+                .into(),
+            );
+        }
+    }
+
+    if let Some(instance_def) = layout.builtins.ecdsa.as_ref() {
+        let included = builtins.remove(&BuiltinName::ecdsa);
+        if included || create_all_builtins {
+            builtin_runners.push(SignatureBuiltinRunner::new(instance_def.ratio, included).into());
+        }
+    }
+
+    if let Some(instance_def) = layout.builtins.bitwise.as_ref() {
+        let included = builtins.remove(&BuiltinName::bitwise);
+        if included || create_all_builtins {
+            builtin_runners.push(BitwiseBuiltinRunner::new(instance_def.ratio, included).into());
+        }
+    }
+
+    if let Some(instance_def) = layout.builtins.ec_op.as_ref() {
+        let included = builtins.remove(&BuiltinName::ec_op);
+        if included || create_all_builtins {
+            builtin_runners.push(EcOpBuiltinRunner::new(instance_def.ratio, included).into());
+        }
+    }
+
+    if let Some(instance_def) = layout.builtins.keccak.as_ref() {
+        let included = builtins.remove(&BuiltinName::keccak);
+        if included || create_all_builtins {
+            builtin_runners.push(KeccakBuiltinRunner::new(instance_def.ratio, included).into());
+        }
+    }
+
+    if let Some(instance_def) = layout.builtins.poseidon.as_ref() {
+        let included = builtins.remove(&BuiltinName::poseidon);
+        if included || create_all_builtins {
+            builtin_runners.push(PoseidonBuiltinRunner::new(instance_def.ratio, included).into());
+        }
+    }
+
+    if let Some(instance_def) = layout.builtins.range_check96.as_ref() {
+        let included = builtins.remove(&BuiltinName::range_check96);
+        if included || create_all_builtins {
+            builtin_runners.push(
+                RangeCheckBuiltinRunner::<RC_N_PARTS_96>::new_with_low_ratio(
+                    instance_def.ratio,
+                    included,
+                )
+                .into(),
+            );
+        }
+    }
+    if let Some(instance_def) = layout.builtins.add_mod.as_ref() {
+        let included = builtins.remove(&BuiltinName::add_mod);
+        if included || create_all_builtins {
+            builtin_runners.push(ModBuiltinRunner::new_add_mod(instance_def, included).into());
+        }
+    }
+    if let Some(instance_def) = layout.builtins.mul_mod.as_ref() {
+        let included = builtins.remove(&BuiltinName::mul_mod);
+        if included || create_all_builtins {
+            builtin_runners.push(ModBuiltinRunner::new_mul_mod(instance_def, included).into());
+        }
+    }
+
+    if !builtins.is_empty() && !allow_missing_builtins {
+        return Err(RunnerError::NoBuiltinForInstance(Box::new((
+            builtins,
+            layout.name,
+        ))));
+    }
+
+    Ok(builtin_runners)
 }
 
 #[cfg(test)]
@@ -88,7 +253,8 @@ mod tests {
             None,
             HashMap::default(),
             Vec::default(),
-        );
+        )
+        .expect("failed to create runner");
 
         runner.run().expect("failed to run program");
     }
