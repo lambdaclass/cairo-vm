c3523137b320a9d38d31a1984fbcbe75c1692566
enitrat <msaug@protonmail.com>
2025-04-01 11:52:21 +0100

    dev: make Memory::get `pub`

--- vm/src/vm/vm_memory/memory.rs
+++ vm/src/vm/vm_memory/memory.rs
@@ -238,7 +238,7 @@ impl Memory {
     }
 
     /// Retrieve a value from memory (either normal or temporary) and apply relocation rules
-    pub(crate) fn get<'a, 'b: 'a, K: 'a>(&'b self, key: &'a K) -> Option<Cow<'b, MaybeRelocatable>>
+    pub fn get<'a, 'b: 'a, K: 'a>(&'b self, key: &'a K) -> Option<Cow<'b, MaybeRelocatable>>
     where
         Relocatable: TryFrom<&'a K>,
     {
