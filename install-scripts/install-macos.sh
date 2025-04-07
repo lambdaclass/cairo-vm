eaabbf4e4efeb7012567e9635640acca7ae6558b
FrancoGiachetta <francogiachetta27@gmail.com>
2025-04-07 17:33:57 -0300

    avoid installing gmp if it is already installed

--- install-scripts/install-macos.sh
+++ install-scripts/install-macos.sh
@@ -9,7 +9,10 @@ source "$HOME/.cargo/env"
 rustc --version
 
 # Install pyenv
-brew install pyenv gmp
+brew install pyenv
+
+ifeq (, $(shell which gmp))
+    brew install gmp
 
 # Make sure pyenv has been installed correctly
 pyenv -v
