5b833000a7e3531aabd6370f025f0622d7d8320f
Diego Civini <diego.civini@lambdaclass.com>
2025-10-08 16:32:36 -0300

    Remove debugging prints

--- vm/src/hint_processor/builtin_hint_processor/builtin_hint_processor_definition.rs
+++ vm/src/hint_processor/builtin_hint_processor/builtin_hint_processor_definition.rs
@@ -233,8 +233,6 @@ impl HintProcessorLogic for BuiltinHintProcessor {
             );
         }
 
-        println!("{}", hint_data.code);
-
         let hint_func = hint_data.f.unwrap(); // TODO: Remove unwrap
 
         hint_func(
@@ -524,7 +522,6 @@ impl HintProcessorLogic for BuiltinHintProcessor {
                 super::simulated_builtins::get_simulated_builtin_base
             }
             code => {
-                println!("#################################");
                 return Err(
                     crate::vm::errors::vm_errors::VirtualMachineError::CompileHintFail(
                         code.to_string().into_boxed_str(),
