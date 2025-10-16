accc0760d1e23c985794296e785d45036cb9cf13
FrancoGiachetta <francogiachetta27@gmail.com>
2025-10-16 12:45:06 -0300

    simplify things

--- vm/src/hint_processor/hint_processor_definition.rs
+++ vm/src/hint_processor/hint_processor_definition.rs
@@ -76,7 +76,7 @@ pub type HintExtension = HashMap<Relocatable, Vec<Box<dyn Any>>>;
 pub trait HintProcessor: HintProcessorLogic + ResourceTracker {}
 impl<T> HintProcessor for T where T: HintProcessorLogic + ResourceTracker {}
 
-fn get_ids_data(
+pub(crate) fn get_ids_data(
     reference_ids: &HashMap<String, usize>,
     references: &[HintReference],
 ) -> Result<HashMap<String, HintReference>, VirtualMachineError> {
