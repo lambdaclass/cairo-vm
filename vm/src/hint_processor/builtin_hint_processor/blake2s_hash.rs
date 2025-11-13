b2be8c4159dec339f46fe90ce35700512baeb92e
gabrielbosio <gabrielbosio95@gmail.com>
2025-11-13 16:13:31 -0300

    Make blake2s modules internal instead

--- vm/src/hint_processor/builtin_hint_processor/blake2s_hash.rs
+++ vm/src/hint_processor/builtin_hint_processor/blake2s_hash.rs
@@ -101,7 +101,7 @@ fn blake_round(mut state: Vec<u32>, message: &[u32; 16], sigma: [usize; 16]) -> 
     state
 }
 
-pub(crate) fn blake2s_compress(
+pub fn blake2s_compress(
     h: &[u32; 8],
     message: &[u32; 16],
     t0: u32,
